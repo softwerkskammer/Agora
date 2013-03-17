c0c7f127287b2d3f4f9570a1f353bbdfe6752587
Nicole Rauch <nicole.m@gmx.de>
2013-03-17 21:29:22 +0100

    Fixed the wrong port number

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hallo Softwerkskammer :-)\n');
-    }).listen(171024, "127.0.0.1");
+	res.end('Hallo Softwerkskammer! :-)\n');
+    }).listen(17124, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:171024/');
