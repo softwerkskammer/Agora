1af1b22da73112fd6b20364fcfbc3625a9b83207
Gregor Gramlich <gramlich@eosc.de>
2013-03-17 21:52:01 +0100

    refactored start.js and made it pass jshint

--- start.js
+++ start.js
@@ -1,6 +1,9 @@
+"use strict";
 var http = require('http');
+var ip = "127.0.0.1", port = 17124;
+
 http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hallo Softwerkskammer! :-)\n');
-    }).listen(17124, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:171024/');
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hallo Softwerkskammer! :-)\n');
+}).listen(port, ip);
+console.log('Server running at http://' + ip + ':' + port);
