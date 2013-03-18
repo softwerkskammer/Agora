a626e6e72f39b032164f58f5402fad6d1c2b3208
holgergp <holgergp@gmail.com>
2013-03-18 22:02:37 +0100

    Erste Gehversuche mit node-soap

--- start.js
+++ start.js
@@ -2,8 +2,25 @@
 var http = require('http');
 var ip = "127.0.0.1", port = 17124;
 
+var soap = require('soap');
+var url = 'http://swk.monoceres.uberspace.de/fcgi-bin/sympa.fcgi/wsdl';
+var args = {listname: 'craftsmanswap'};
+soap.createClient(url, function(err, client) {
+    client.info(args, function(err, result) {
+        console.log(result);
+    });
+});
+
+
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hallo Softwerkskammer! :-)\n');
+
+    soap.createClient(url, function(err, client) {
+        client.info(args, function(err, result) {
+            res.end('Hallo Softwerkskammer! :-)\n');
+        });
+    });
+
+
 }).listen(port, ip);
 console.log('Server running at http://' + ip + ':' + port);
