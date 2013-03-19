d8d6f37c8127226c0bae90095db6c4c16eae8bf3
Sergey Shishkin <sergei.shishkin@gmail.com>
2013-03-19 09:55:57 +0100

    Splitted the app into sub-apps

--- start.js
+++ start.js
@@ -1,12 +1,13 @@
 "use strict";
 
 var express = require('express');
+var site = require('./lib/site');
+var events = require('./lib/events');
 var app = express();
 var port = 17124;
 
-app.get('/', function(req, res){
-  res.send('Hallo Softwerkskammer! :-)');
-});
+app.use('/', site);
+app.use('/events', events);
 
 app.listen(port);
 console.log('Server running at port ' + port);
