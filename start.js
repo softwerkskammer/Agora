ae02c3c14d49a6f2574b4833d5f9d41154e16696
holgergp <holgergp@gmail.com>
2013-03-19 08:10:12 +0100

    Modul für den SWK-Sympa-Client

--- start.js
+++ start.js
@@ -2,23 +2,27 @@
 var http = require('http');
 var ip = "127.0.0.1", port = 17124;
 
-var soap = require('soap');
-var url = 'http://swk.monoceres.uberspace.de/fcgi-bin/sympa.fcgi/wsdl';
-var args = {listname: 'craftsmanswap'};
+
+var swkSympaClient=require('./swkSympaClient/client');
+
 
 
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
+    var responseCallback= function (result){
+        var myResponse="Hallo Softwerkskammer :-)" +
+            "\n" +
+            "\n" +
+            "Response auf den Info Request:" +
+            "\n" +
+            "\n" +
+            result;
+        res.end(myResponse);
+    }
+    swkSympaClient.getInfoRequest(responseCallback);
 
-    soap.createClient(url, function(err, client) {
-        client.info(args, function(err, result) {
-            res.end('Hallo Softwerkskammer! :-)\n'+result.body);
 
-
-
-        });
-    });
 
 
 }).listen(port, ip);
---
+++ swkSympaClient/client.js
@@ -0,0 +1,26 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: flunky
+ * Date: 19.03.13
+ * Time: 07:50
+ * To change this template use File | Settings | File Templates.
+ */
+var soap = require('soap');
+var url = 'http://swk.monoceres.uberspace.de/fcgi-bin/sympa.fcgi/wsdl';
+var args = {listname: 'craftsmanswap'};
+
+client = function(){}
+
+var getInfoRequest = function(callback) {
+    soap.createClient(url, function(err, client) {
+        client.info(args, function(err, result) {
+            callback(result.body)
+        })
+    });
+
+
+}
+
+client.getInfoRequest=getInfoRequest;
+
+module.exports=client;
