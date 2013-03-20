be174266ce04eeae5bd4ee2ea1c419d843134f4b
Gregor Gramlich <gramlich@eosc.de>
2013-03-20 18:12:54 +0100

    Added gruppen resource

--- app.js
+++ app.js
@@ -21,6 +21,13 @@ app.configure('development', function () {
 app.use('/', require('./lib/site'));
 app.use('/events', require('./lib/events'));
 app.use('/gruppenverwaltung', require('./lib/gruppenverwaltung'));
+var gruppenApp = require('./lib/gruppen');
+/* This is needed in the gruppen.jade view, to produce reasonable hrefs */
+gruppenApp.locals({
+  gruppen_route: 'gruppen'
+});
+
+app.use('/gruppen', gruppenApp);
 
 var http = require('http');
 var server = http.createServer(app);
