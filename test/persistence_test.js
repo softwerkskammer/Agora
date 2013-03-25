adf5a3896920c915ad9d8170811c7a3f2b45b962
Gregor Gramlich <gramlich@eosc.de>
2013-03-25 10:09:39 +0100

    New test for non-existing id

--- test/persistence_test.js
+++ test/persistence_test.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var fs = require('fs');
-
+var should = require('chai').should();
 var createTeststore = function () {
   return require('../lib/persistence/persistence')('teststore');
 };
@@ -16,23 +16,33 @@ if (fs.existsSync(storeDir)) {
 }
 
 describe('The store can persist objects and retrieve them', function () {
+  var persistence;
+
+  beforeEach(function () {
+    persistence = createTeststore();
+  });
 
   it('stores to the teststore', function () {
     var toPersist = {id: 'toPersist', name: 'Heinz'};
-    var persistence = createTeststore();
     persistence.save(toPersist);
   });
 
-  it('retrieves one from the teststore', function () {
-    var persistence = createTeststore();
+  it('retrieves none for non-existing id', function (done) {
+    persistence.getById('non-existing-id', function (result) {
+      should.not.exist(result);
+      done();
+    });
+  });
+
+  it('retrieves one from the teststore', function (done) {
     persistence.getById('toPersist', function (result) {
       result.id.should.equal('toPersist');
       result.name.should.equal('Heinz');
+      done();
     });
   });
 
   it('retrieves all from the teststore', function () {
-    var persistence = createTeststore();
     persistence.list(function (result) {
       result.length.should.equal(1);
       result[0].name.should.equal('Heinz');
