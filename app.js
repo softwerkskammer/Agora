b2d6d3259ca8d8aa7f67d05abef7196ee5ca7ec6
Markus M. May <mmay@javafreedom.org>
2013-03-29 20:50:35 +0100

    Remove console.log

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ var path = require('path');
 var app = express();
 
 function useApp(parent, url, conf, factory) {
-  console.log('confb: ', conf.get('port'));
   var child = factory(express(), conf);
-  console.log('confa: ', conf.get('port'));
   child.locals({ baseUrl: url });
   parent.use('/' + url, child);
   return child;
@@ -34,13 +32,9 @@ module.exports = function (conf) {
   });
 
   app.use('/', require('./lib/site'));
-  console.log('1: ', conf.get('port'));
   useApp(app, 'events', conf, require('./lib/events'));
-  console.log('2: ', conf.get('port'));  
   useApp(app, 'members', conf, require('./lib/members'));
-  console.log('3: ', conf.get('port'));
   useApp(app, 'groups', conf, require('./lib/groups'));
-  console.log('4: ', conf.get('port'));
 
   var http = require('http');
   var server = http.createServer(app);
@@ -67,8 +61,6 @@ module.exports = function (conf) {
 
   localApp.start = start;
   localApp.stop = stop;
-
-  console.log('localApp: ', localApp.conf.get('port'));
 
   return localApp;
 };
