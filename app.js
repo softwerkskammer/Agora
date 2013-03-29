ddd138ddb9882f561f1a236bad351aa4875ec23f
Gregor Gramlich <gramlich@eosc.de>
2013-03-29 19:42:51 +0100

    restructured app.js with less global variables

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 "use strict";
 
-var express = require('express');
-var path = require('path');
-var app = express();
-var conf = {};
+var express = require('express'),
+  http = require('http'),
+  path = require('path');
+
+var server;
 
 function useApp(parent, url, conf, factory) {
   var child = factory(express());
@@ -13,31 +14,34 @@ function useApp(parent, url, conf, factory) {
   return child;
 }
 
-app.configure(function () {
-  app.set('view engine', 'jade');
-  app.set('views', path.join(__dirname, 'views'));
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
-});
+function initApp(app, conf) {
+  app.use('/', require('./lib/site'));
+  useApp(app, 'events', conf, require('./lib/events'));
+  useApp(app, 'members', conf, require('./lib/members'));
+  useApp(app, 'groups', conf, require('./lib/groups'));
 
-app.configure('development', function () {
-  app.use(express.errorHandler());
-});
+  app.configure(function () {
+    app.set('view engine', 'jade');
+    app.set('views', path.join(__dirname, 'views'));
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
+  });
 
-app.use('/', require('./lib/site'));
-useApp(app, 'events', conf, require('./lib/events'));
-useApp(app, 'members', conf, require('./lib/members'));
-useApp(app, 'groups', conf, require('./lib/groups'));
+  app.configure('development', function () {
+    app.use(express.errorHandler());
+  });
+}
 
-var http = require('http');
-var server = http.createServer(app);
 
 exports.start = function (conf, done) {
   var port = conf.get('port');
+  var app = express();
+  initApp(app, conf);
+  server = http.createServer(app);
   server.listen(port, function () {
     console.log('Server running at port ' + port);
     if (done) {
