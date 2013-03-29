96aee2b5f3f5f4bdd249908a6c5b86bcf6ba23dd
leider <derleider@web.de>
2013-03-29 21:52:14 +0100

    added authentication for mongodb

--- start.sh
+++ start.sh
@@ -4,4 +4,9 @@ export REMOTE_APP_NAME="dummyapp"
 export REMOTE_APP_PWD="dummypwd"
 export REMOTE_APP_USER="dummyuser"
 
+export MONGO_PORT="localhost"
+export MONGO_PORT="27017"
+export MONGO_USER="leider"
+export MONGO_PASS="4uonlytoo"
+
 node start.js
