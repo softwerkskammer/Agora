0eb233e4ce451224b9df10c7544f9f984a9da9d1
Nicole Rauch <nicole.m@gmx.de>
2013-03-30 12:18:09 +0100

    Fixed environment variable name (MONGO_HOST).

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ export swkTrustedAppName="dummyapp"
 export swkTrustedAppPwd="dummypwd"
 export swkRemoteAppUser="dummyuser"
 
-export MONGO_PORT="localhost"
+export MONGO_HOST="localhost"
 export MONGO_PORT="27017"
 export MONGO_USER="leider"
 export MONGO_PASS="4uonlytoo"
