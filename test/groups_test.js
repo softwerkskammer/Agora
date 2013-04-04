12a9e7328248b0002077c08abc9e96de4228f02d
Nicole Rauch <nicole.m@gmx.de>
2013-04-04 22:25:12 +0200

    Now the group test uses stubs for the required APIs. Added tests for all /groups/... URLs.

--- test/groups_test.js
+++ test/groups_test.js
@@ -2,24 +2,112 @@
 "use strict";
 var request = require('supertest'),
   express = require('express'),
-  MongoConf = require('./mongoConf'),
-  conf = new MongoConf();
+  proxyquire = require('proxyquire');
 
-conf.set('port', '17124');
+var Group = require('../lib/groups/group');
+var Member = require('../lib/members/member');
 
-var groups = require('../lib/groups');
-var app = groups(express(), conf);
+var GroupA = new Group('GroupA', 'Gruppe A', 'Dies ist Gruppe A.', 'Themengruppe');
+var MemberA = new Member('hada', 'hada', 'Hans', 'Dampf', 'hans.dampf@gmail.com', '@hada', 'Süden', 'Entwickler', 'ada', 'http://my.blog', 'beim Bier');
+
+var groupsAPIStub = {
+  getAllAvailableGroups: function () {},
+  getGroup: function () {},
+  groupFromObject: function () {},
+  createOrSaveGroup: function (group, callback) { callback(); }
+};
+
+var groupsAndMembersAPIStub = {
+  getGroupAndUsersOfList: function () {}
+};
+
+var groupsApp = proxyquire('../lib/groups', {
+  './groupsAPI'                          : function () {
+    return groupsAPIStub;
+  },
+  '../groupsAndMembers/groupsAndMembersAPI': function () {
+    return groupsAndMembersAPIStub;
+  }
+});
+
+var app = groupsApp(express(), { get: function () {
+  return null;
+} });   // empty config
+
 
 app.locals({
   baseUrl: 'groups'
 });
 
 describe('Groups application', function () {
+  groupsAPIStub.getAllAvailableGroups = function (callback) { callback(null, [GroupA]); };
 
   it('shows all available lists', function (done) {
     request(app)
       .get('/')
       .expect(200)
-      .expect(/Alle Gruppen/, done);
+      .expect('Content-Type', /text\/html/)
+      .expect(/Alle Gruppen/)
+      .expect(/Gruppe A/, done);
+  });
+
+  it('allows to create a new group', function (done) {
+
+    request(app)
+      .get('/new')
+      .expect(200)
+      .expect('Content-Type', /text\/html/)
+      .expect(/<h1>Gruppe anlegen<\/h1>/)
+      .expect(/<input type="submit" value="Anlegen">/, done);
+  });
+
+  it('allows to edit an existing group', function (done) {
+    groupsAPIStub.getGroup = function (groupname, callback) {
+      callback(null, GroupA);
+    };
+
+    request(app)
+      .get('/edit/GroupA')
+      .expect(200)
+      .expect('Content-Type', /text\/html/)
+      .expect(/<h1>Gruppe bearbeiten<\/h1>/)
+      .expect(/<input type="submit" value="Speichern">/, done);
+  });
+
+  it('displays an existing group and its members', function (done) {
+    groupsAndMembersAPIStub.getGroupAndUsersOfList = function (groupname, callback) {
+      callback(null, GroupA, [MemberA]);
+    };
+
+    request(app)
+      .get('/GroupA')
+      .expect(200)
+      .expect('Content-Type', /text\/html/)
+      .expect(/<title>Gruppe A<\/title>/)
+      .expect(/Dies ist Gruppe A./)
+      .expect(/Themengruppe/)
+      .expect(/Mitglieder der Gruppe:/)
+      .expect(/<ul><li>Hans Dampf<\/li><\/ul>/)
+      .end(done);
+  });
+
+  it('saves a newly created Group', function (done) {
+    groupsAPIStub.groupFromObject = function () { return GroupA; };
+
+    request(app)
+      .post('/submit')
+      .expect(302)
+      .expect('Content-Type', /text\/plain/)
+      .expect('Moved Temporarily. Redirecting to /null/groups/GroupA', done);
+  });
+
+  it('saves an already existing Group', function (done) {
+    groupsAPIStub.groupFromObject = function () { return GroupA; };
+
+    request(app)
+      .post('/edit/submit')
+      .expect(302)
+      .expect('Content-Type', /text\/plain/)
+      .expect('Moved Temporarily. Redirecting to /null/groups/GroupA', done);
   });
 });
