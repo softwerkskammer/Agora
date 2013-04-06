4634d232dffbe8377fae9991bdec2de673121dbc
Nicole Rauch <nicole.m@gmx.de>
2013-04-06 22:53:20 +0200

    Now the user is subscribed and unsubscribed according to his selection in his profile.

--- test/groupsAPI_subscription_test.js
+++ test/groupsAPI_subscription_test.js
@@ -11,8 +11,8 @@ var groupstoreStub = {
 
 var sympaStub = {
   getSubscribedListsForUser: function () {},
-  addUserToList: function () {},
-  removeUserFromList: function () {}
+  addUserToList: function (email, list, callback) { callback(); },
+  removeUserFromList: function (email, list, callback) { callback(); }
 };
 
 
@@ -44,6 +44,24 @@ describe('Groups API (Subscriptions)', function () {
     });
   });
 
+  it('subscribes and unsubscribes no lists if old list contains one element and new subscription is the same element (not list)', function (done) {
+    sympaStub.getSubscribedListsForUser = function (email, callback) { callback(null, ['list1']); };
+    var subscribeSpy = sinon.spy(sympaStub, 'addUserToList');
+    var unsubscribeSpy = sinon.spy(sympaStub, 'removeUserFromList');
+
+
+    systemUnderTest.updateSubscriptions('user@mail.com', 'list1', function (err) {
+
+      expect(subscribeSpy.called, 'subscribe is called').to.be.false;
+      expect(unsubscribeSpy.called, 'unsubscribe is called').to.be.false;
+
+      sympaStub.addUserToList.restore();
+      sympaStub.removeUserFromList.restore();
+
+      done(err);
+    });
+  });
+
   it('subscribes and unsubscribes no lists if old and new subscription lists contain the same lists', function (done) {
     sympaStub.getSubscribedListsForUser = function (email, callback) { callback(null, ['list1', 'list2']); };
     var subscribeSpy = sinon.spy(sympaStub, 'addUserToList');
@@ -53,6 +71,25 @@ describe('Groups API (Subscriptions)', function () {
     systemUnderTest.updateSubscriptions('user@mail.com', ['list1', 'list2'], function (err) {
 
       expect(subscribeSpy.called, 'subscribe is called').to.be.false;
+      expect(unsubscribeSpy.called, 'unsubscribe is called').to.be.false;
+
+      sympaStub.addUserToList.restore();
+      sympaStub.removeUserFromList.restore();
+
+      done(err);
+    });
+  });
+
+  it('subscribes one list if old subscriptions are empty and new ones contain one element (not list)', function (done) {
+    sympaStub.getSubscribedListsForUser = function (email, callback) { callback(null, []); };
+    var subscribeSpy = sinon.spy(sympaStub, 'addUserToList');
+    var unsubscribeSpy = sinon.spy(sympaStub, 'removeUserFromList');
+
+
+    systemUnderTest.updateSubscriptions('user@mail.com', 'list1', function (err) {
+
+      expect(subscribeSpy.calledOnce, 'subscribe is called once').to.be.true;
+      expect(subscribeSpy.calledWith('user@mail.com', 'list1')).to.be.true;
       expect(unsubscribeSpy.called, 'unsubscribe is called').to.be.false;
 
       sympaStub.addUserToList.restore();
@@ -73,6 +110,66 @@ describe('Groups API (Subscriptions)', function () {
       expect(subscribeSpy.calledOnce, 'subscribe is called once').to.be.true;
       expect(subscribeSpy.calledWith('user@mail.com', 'list1')).to.be.true;
       expect(unsubscribeSpy.called, 'unsubscribe is called').to.be.false;
+
+      sympaStub.addUserToList.restore();
+      sympaStub.removeUserFromList.restore();
+
+      done(err);
+    });
+  });
+
+  it('unsubscribes one list if old subscriptions contain a list and new ones are undefined', function (done) {
+    sympaStub.getSubscribedListsForUser = function (email, callback) { callback(null, ['list1']); };
+    var subscribeSpy = sinon.spy(sympaStub, 'addUserToList');
+    var unsubscribeSpy = sinon.spy(sympaStub, 'removeUserFromList');
+
+
+    systemUnderTest.updateSubscriptions('user@mail.com', undefined, function (err) {
+
+      expect(subscribeSpy.called, 'subscribe is called').to.be.false;
+      expect(unsubscribeSpy.calledOnce, 'unsubscribe is called once').to.be.true;
+      expect(unsubscribeSpy.calledWith('user@mail.com', 'list1')).to.be.true;
+
+      sympaStub.addUserToList.restore();
+      sympaStub.removeUserFromList.restore();
+
+      done(err);
+    });
+  });
+
+  it('unsubscribes one list if old subscriptions contain a list and new ones are an empty array', function (done) {
+    sympaStub.getSubscribedListsForUser = function (email, callback) { callback(null, ['list1']); };
+    var subscribeSpy = sinon.spy(sympaStub, 'addUserToList');
+    var unsubscribeSpy = sinon.spy(sympaStub, 'removeUserFromList');
+
+
+    systemUnderTest.updateSubscriptions('user@mail.com', [], function (err) {
+
+      expect(subscribeSpy.called, 'subscribe is called').to.be.false;
+      expect(unsubscribeSpy.calledOnce, 'unsubscribe is called once').to.be.true;
+      expect(unsubscribeSpy.calledWith('user@mail.com', 'list1')).to.be.true;
+
+      sympaStub.addUserToList.restore();
+      sympaStub.removeUserFromList.restore();
+
+      done(err);
+    });
+  });
+
+  it('subscribes and unsubscribes appropriately if there are many changes', function (done) {
+    sympaStub.getSubscribedListsForUser = function (email, callback) { callback(null, ['list1', 'list2', 'list3']); };
+    var subscribeSpy = sinon.spy(sympaStub, 'addUserToList');
+    var unsubscribeSpy = sinon.spy(sympaStub, 'removeUserFromList');
+
+
+    systemUnderTest.updateSubscriptions('user@mail.com', ['list2', 'list4', 'list5'], function (err) {
+
+      expect(subscribeSpy.calledTwice, 'subscribe is called twice').to.be.true;
+      expect(unsubscribeSpy.calledTwice, 'unsubscribe is called twice').to.be.true;
+      expect(unsubscribeSpy.calledWith('user@mail.com', 'list1')).to.be.true;
+      expect(unsubscribeSpy.calledWith('user@mail.com', 'list3')).to.be.true;
+      expect(subscribeSpy.calledWith('user@mail.com', 'list4')).to.be.true;
+      expect(subscribeSpy.calledWith('user@mail.com', 'list5')).to.be.true;
 
       sympaStub.addUserToList.restore();
       sympaStub.removeUserFromList.restore();
