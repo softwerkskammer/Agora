aa4c5dea89eaf1adc81f50ae76b67c0769b0dfa4
Markus M. May <mmay@javafreedom.org>
2013-04-07 21:45:53 +0200

    Move events to activities

---
+++ test/activities_test.js
@@ -0,0 +1,51 @@
+/*global describe, it */
+"use strict";
+var request = require('supertest'),
+  express = require('express'),
+  sinon = require('sinon'),
+  proxyquire = require('proxyquire');
+
+require('chai').should();
+
+var Activity = require('../lib/activities/activity');
+
+var dummyActivity = new Activity('id', 'title', 'description', 'assignedGroup', 'location', 'direction', 'activityDate', 'startTime');
+
+var activitiesAPIStub = {
+  getActivity: function (id, callback) {
+    callback(null, dummyActivity);
+  },
+  allActivities: function (callback) {
+    console.log('callback: ', callback);
+
+    callback(null, [dummyActivity]);
+  }
+};
+
+var activityApp = proxyquire('../lib/activities', {
+  './activitiesAPI': function () {
+    return activitiesAPIStub;
+  }
+});
+
+var app = activityApp(express(), { get: function () {
+  return null;
+} });   // empty config
+
+
+describe('Activity application', function () {
+
+  it('shows the list of activities as retrieved from the store', function (done) {
+    var allActivities = sinon.spy(activitiesAPIStub, 'allActivities');
+
+    request(app)
+      .get('/')
+      .expect(200)
+      .expect(/Ankündigungen/)
+      .expect(/href="id"/)
+      .expect(/title/, function (err) {
+        allActivities.calledOnce.should.be.ok;
+        done(err);
+      });
+  });
+});
