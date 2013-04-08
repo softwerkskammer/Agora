865e92c3057791b61dae755f694ab7bfc803d634
Dimitry Polivaev <dpolivaev@gmx.de>
2013-04-08 08:53:07 +0200

    move newUserMustFillInRegistration logic to authentication module and apply it only when a protected page is visited

--- app.js
+++ app.js
@@ -58,8 +58,7 @@ function useApp(parent, url, conf, factory) {
 }
 
 module.exports = function (conf) {
-  var authentication = require('./lib/authentication')(conf),
-    urlPrefix = conf.get('publicUrlPrefix');
+  var authentication = require('./lib/authentication')(conf);
 
   // initialize winston and two concrete loggers
   initWinston(conf);
@@ -72,24 +71,6 @@ module.exports = function (conf) {
       httpLogger.info(message.replace(/(\r\n|\n|\r)/gm, ""));
     }
   };
-
-  function newUserMustFillInRegistration(req, res, next) {
-    var urlNew = '/members/new';
-    var originalUrl = req.originalUrl;
-
-    function isOK() {
-      return originalUrl !== urlNew &&
-        originalUrl !== '/members/submit' &&
-        !/.clientscripts./.test(originalUrl) &&
-        !/.stylesheets./.test(originalUrl) &&
-        !/.checknickname./.test(originalUrl);
-    }
-
-    if (req.user && !req.user.registered && isOK()) {
-      return res.redirect(urlPrefix + urlNew);
-    }
-    next();
-  }
 
   return {
     create: function () {
@@ -109,7 +90,6 @@ module.exports = function (conf) {
         app.use(express.methodOverride());
         app.use(express.session({secret: conf.get('secret')}));
         authentication.configure(app);
-        app.use(newUserMustFillInRegistration);
         app.use(app.router);
         app.use(express.static(path.join(__dirname, 'public')));
       });
