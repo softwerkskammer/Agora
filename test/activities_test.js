fb66e5ad50353c0d5bbf51c6117845a69cde386b
Markus M. May <mmay@javafreedom.org>
2013-04-14 20:22:29 +0200

    Rework id for activities

--- test/activities_test.js
+++ test/activities_test.js
@@ -9,14 +9,14 @@ require('chai').should();
 
 var Activity = require('../lib/activities/activity');
 
-var dummyActivity = new Activity('id', 'title', 'description', 'assignedGroup', 'location', 'direction', 'activityDate', 'startTime');
+var dummyActivity = new Activity('title', 'description', 'assignedGroup', 'location', 'direction', 'activityDate', 'startTime');
 
 var activitiesAPIStub = {
   getActivityForId: function (id, callback) {
     var activity;
     var err = null;
 
-    if (id === 'id') {
+    if (id === 'assignedGroup_title_activityDate') {
       activity = dummyActivity;
     } else {
       activity = null;
@@ -41,6 +41,18 @@ var app = activityApp(express(), { get: function () {
 
 describe('Activity application', function () {
 
+  it('removes all special characters from the id string', function () {
+    dummyActivity.createLink().should.equal('assignedGroup_title_activityDate');
+
+    var tmpActivity = new Activity('?!tit le?!', 'description', 'assignedGroup', 'location', 'direction', '2012-11-11', 'startTime');
+    tmpActivity.createLink().should.equal('assignedGroup___tit_le___2012-11-11');
+  });
+
+  it('object is not valid, if the required fields are not filled', function () {
+    var tmpActivity = new Activity('description', 'assignedGroup', 'location', 'direction', 'activityDate', 'startTime');
+    tmpActivity.isValid().should.equal.false;
+  });
+
   it('shows the list of activities as retrieved from the store', function (done) {
     var allActivities = sinon.spy(activitiesAPIStub, 'allActivities');
 
@@ -48,21 +60,22 @@ describe('Activity application', function () {
       .get('/')
       .expect(200)
       .expect(/Aktivitäten/)
-      .expect(/href="id"/)
+      .expect(/href="assignedGroup_title_activityDate"/)
       .expect(/title/, function (err) {
         allActivities.calledOnce.should.be.ok;
         activitiesAPIStub.allActivities.restore();
         done(err);
       });
 
+//    activitiesAPIStub.allActivities.restore();
   });
 
   it('shows the details of one activity as retrieved from the store', function (done) {
     var getActivityForId = sinon.spy(activitiesAPIStub, 'getActivityForId');
-    var id = dummyActivity.id;
+    var id = dummyActivity.createLink();
 
     request(app)
-      .get('/' + dummyActivity.id)
+      .get('/' + id)
       .expect(200)
       .expect(/<small> title/)
       .expect(/<h2>Aktivitäten/, function (err) {
@@ -70,19 +83,15 @@ describe('Activity application', function () {
         activitiesAPIStub.getActivityForId.restore();
         done(err);
       });
-
   });
 
   it('shows the list of activities if the id cannot be found in the store for the detail page', function (done) {
-    var getActivityForId = sinon.spy(activitiesAPIStub, 'getActivityForId');
-
-    var id = dummyActivity.id + '4711';
+    var link = dummyActivity.id + '4711';
 
     request(app)
-      .get('/' + id)
+      .get('/' + link)
       .expect(302)
       .expect(/activities/, function (err) {
-        getActivityForId.calledWith(id).should.be.true;
         done(err);
       });
   });
