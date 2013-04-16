fe0e5cc14f792e01714ebcb5cb201002d6ea2b34
Dimitry Polivaev <dpolivaev@gmx.de>
2013-04-16 21:14:15 +0200

    extract stubs and test app required for access_test and members_test to a separate js file

--- test/access_test.js
+++ test/access_test.js
@@ -7,59 +7,10 @@ var request = require('supertest'),
 
 require('chai').should();
 
-var Member = require('../lib/members/member');
-
-var dummymember = new Member();
-dummymember.nickname = 'hada';
-dummymember.site = 'http://my.blog';
-dummymember.firstname = 'Hans';
-dummymember.lastname = 'Dampf';
-
-var groupsAPIStub = {
-  getSubscribedGroupsForUser: function (email, callback) {
-    callback(null, []);
-  }
-};
-
-// disabling authentication
-var ensureLoggedInStub = {
-  ensureLoggedIn: function () {
-    return function (req, res, next) {
-      next();
-    };
-  }
-};
-
-var membersAPIStub = {
-  allMembers: function (callback) {
-    callback(null, [dummymember]);
-  },
-  getMember: function (nickname, callback) {
-    callback(null, dummymember);
-  }
-};
-
-var groupsAndMembers = proxyquire('../lib/groupsAndMembers/groupsAndMembersAPI', {
-  '../groups/groupsAPI': function () {
-    return groupsAPIStub;
-  },
-  '../members/membersAPI': function () {
-    return membersAPIStub;
-  }
-});
-
-var memberModule = proxyquire('../lib/members', {
-  './membersAPI': function () {
-    return membersAPIStub;
-  },
-  '../groupsAndMembers/groupsAndMembersAPI': groupsAndMembers,
-  'connect-ensure-login': ensureLoggedInStub
-});
-
 var authenticationModule = proxyquire('../lib/authentication', {});
 var authenticationAppFactory = authenticationModule(conf);
 
-var memberAppFactory = memberModule(conf);
+var memberAppFactory = require('./membertest_stubs').memberModule(conf);
 var appUnderTest = express();
 
 var authenticationState = {};
