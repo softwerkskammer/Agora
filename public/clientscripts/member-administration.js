5cfa44dc8601b2ed747e7e38c4bf58f36ef32f3e
leider <derleider@web.de>
2013-04-19 00:34:06 +0200

    UI cleanup

--- public/clientscripts/member-administration.js
+++ public/clientscripts/member-administration.js
@@ -3,16 +3,14 @@
 
 var initEditable = function () {
   $("table").find("a").filter(function () {return this.id !== "isAdmin"; }).editable({
-    showbuttons: false,
-    toggle: "mouseenter"
+    showbuttons: false
   });
   $("table").find("a").filter(function () {return this.id === "isAdmin"; }).editable(
     {source: [
       {value: 1, text: "Administrator"},
       {value: 2, text: "Normal"}
     ],
-      showbuttons: false,
-      toggle: "mouseenter"}
+      showbuttons: false}
   );
 };
 $(document).ready(initEditable);
