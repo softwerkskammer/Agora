44ca2a8d46ecc6f89ff15e468946afc35458ad6c
leider <derleider@web.de>
2013-04-28 00:40:43 +0200

    now we have restart persistent sessions!

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var winston = require('winston');
+var MongoStore = require('connect-mongo')(express);
 
 function ensureRequestedUrlEndsWithSlash(req, res, next) {
   function endsWithSlash(string) {
@@ -49,6 +50,14 @@ module.exports = function (conf) {
     }
   };
 
+  var sessionStore = new MongoStore({
+    db: 'swk',
+    host: conf.mongoHost,
+    port: conf.mongoPort,
+    username: conf.mongoUser,
+    password: conf.mongoPass
+  });
+
   return {
 
     create: function () {
@@ -66,7 +75,7 @@ module.exports = function (conf) {
         app.use(express.cookieParser());
         app.use(express.bodyParser());
         app.use(express.methodOverride());
-        app.use(express.session({secret: conf.get('secret'), cookie: {maxAge: 86400 * 1000 * 7}}));
+        app.use(express.session({secret: conf.get('secret'), cookie: {maxAge: 86400 * 1000 * 7}, store: sessionStore}));
         app.use(addToLocals);
         authentication.configure(app);
         app.use(members.newUserMustFillInRegistration);
