34882adb9970d02716b93d2dbaaa4419ec99ce53
leider <derleider@web.de>
2013-04-30 00:13:01 +0200

    few more tweaks

--- public/clientscripts/navbar-scripts.js
+++ public/clientscripts/navbar-scripts.js
@@ -7,5 +7,30 @@ var highlightCurrentSection = function () {
     return this.id && new RegExp(this.id).test(loc);
   }).addClass('active');
 };
+
+var addHelpButtonToTextarea = function () {
+  $('textarea').each(function () {
+    $(this).markdown({
+      additionalButtons: [
+        [
+          {
+            name: "groupCustom",
+            data: [
+              {
+                name: "cmdBeer",
+                title: "Help",
+                icon: "icon icon-question-sign",
+                callback: function (e) {
+                  window.open("http://daringfireball.net/projects/markdown/syntax", "_blank");
+                }
+              }
+            ]
+          }
+        ]
+      ]
+    });
+  });
+};
 $(document).ready(highlightCurrentSection);
+$(document).ready(addHelpButtonToTextarea);
 
--- public/clientscripts/surroundWithLink.js
+++ public/clientscripts/surroundWithLink.js
@@ -5,7 +5,7 @@ var surroundWithLink = function (text) {
   // shamelessly stolen from http://stackoverflow.com/questions/1500260/detect-urls-in-text-with-javascript
   var urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
   return text.replace(urlRegex, function (url) {
-    return "<a href=\"" + url + "\">" + url + "</a>";
+    return "<a href=\"" + url + "\" target=\"_blank\">" + "<i class=\"icon-external-link\"/> " + url + "</a>";
   });
 };
 
@@ -13,7 +13,7 @@ var surroundTwitterName = function (twittername) {
   if (twittername.trim().length === 0) {
     return twittername;
   }
-  return "<a href=\"http://twitter.com/" + twittername + "\">@" + twittername + "</a>";
+  return "<a href=\"http://twitter.com/" + twittername + "\" target=\"_blank\"><i class=\"icon-external-link\"/> @" + twittername + "</a>";
 };
 
 var surroundEmail = function (email) {
