5b6865289ad08a85513809feb5ec46a0db45f714
Nicole Rauch <nicole.m@gmx.de>
2013-05-01 16:59:43 +0200

    Used CoolBeans in members API and store.

---
+++ test/persistence/testpersistence.js
@@ -0,0 +1,20 @@
+"use strict";
+
+module.exports = {
+  list: function () {},
+
+  listByIds: function () {},
+
+  listByEMails: function () {},
+
+  listByField: function () {},
+
+  getById: function () {},
+
+  getByField: function () {},
+
+  save: function () {},
+
+  drop: function () {}
+};
+
