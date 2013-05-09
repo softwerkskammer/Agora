de32abe686b987fab495202bff6559fccff00e84
Dimitry Polivaev <dpolivaev@gmx.de>
2013-05-09 13:35:03 +0200

    mail object

---
+++ test/mailarchive/mail_object_test.js
@@ -0,0 +1,19 @@
+"use strict";
+
+require('../configureForTest');
+var conf = require('nconf');
+var expect = require('chai').expect;
+var moment = require('moment');
+
+var Mail = conf.get('beans').get('mail');
+
+describe('Mail', function () {
+  it('restores time from unix time', function (done) {
+    var expectedTime = moment("01 Jan 2010 21:14:14 +0100");
+    var mail = new Mail({
+      dateUnix : expectedTime.unix()
+    });
+    expect(mail.time.format()).to.equal(expectedTime.format());
+    done();
+  });
+});
