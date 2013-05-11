5173b4e191f3f19ccad01608f391e4959ffaa942
Dimitry Polivaev <dpolivaev@gmx.de>
2013-05-11 16:14:23 +0200

    controller and view pages for mail listings and mail content

--- test/mailarchive/mailarchive_test.js
+++ test/mailarchive/mailarchive_test.js
@@ -5,22 +5,30 @@ var express = require('express');
 var sinon = require('sinon');
 var sinonSandbox = sinon.sandbox.create();
 var expect = require('chai').expect;
+var moment = require('moment');
 
 var conf = require('../configureForTest');
 
 var app = conf.get('beans').get('mailsApp')(express());
 var mailsAPI = conf.get('beans').get('mailsAPI');
+var membersAPI = conf.get('beans').get('membersAPI');
 var Mail = conf.get('beans').get('mail');
 
-describe('Mailarchive application', function () {
-  beforeEach(function (done) {
-//    var mailHeaders = sinonSandbox.stub(mailsAPI, 'mailHeaders4groupAndMonth', function (query, callback) {callback(null, [displayedMailHeader]); });
-    done();
-  });
-
+describe('Mail content page', function () {
   afterEach(function (done) {
     sinonSandbox.restore();
     done();
+  });
+
+  it('shows text "Keine Mail gefunden" if mail is not found', function (done) {
+    var mailForId = sinonSandbox.stub(mailsAPI, 'mailForId', function (id, callback) {callback(null, undefined); });
+    request(app)
+      .get('/show/mailID')
+      .expect(200)
+      .expect(/Keine Mail gefunden/, function (err) {
+        expect(mailForId.calledOnce).to.be.ok;
+        done(err);
+      });
   });
 
   it('shows html if message contains html', function (done) {
@@ -48,3 +56,104 @@ describe('Mailarchive application', function () {
   });
 });
 
+describe('Mail index page', function () {
+  afterEach(function (done) {
+    sinonSandbox.restore();
+    done();
+  });
+
+  function stubMailHeaders(headers) {
+    sinonSandbox.stub(mailsAPI, 'mailHeaders', function (query, sortObject, callback) {callback(null, headers); });
+  }
+
+  it('shows group name in the title', function (done) {
+    request(app)
+      .get('/list/group')
+      .expect(200)
+      .expect(/<title>Softwerkskammer\s+-\s+group\s+mails\s+<\/title>/, function (err) {
+        done(err);
+      });
+  });
+
+  it('shows group name in the header', function (done) {
+    request(app)
+      .get('/list/group')
+      .expect(200)
+      .expect(/Gruppe group/, function (err) {
+        done(err);
+      });
+  });
+
+  it('shows text "Keine Mails gefunden" if no mails for group are available', function (done) {
+    request(app)
+      .get('/list/group')
+      .expect(200)
+      .expect(/Keine Mails gefunden/, function (err) {
+        done(err);
+      });
+  });
+
+  var mailTime = moment("01 Jan 2010 21:14:14 +0100");
+  mailTime.lang("de");
+  var displayedMailHeader = new Mail({
+    from: {name: "Sender Name"},
+    timeUnix: mailTime.unix(),
+    id: "<message1@nomail.com>",
+    subject: "Mail 1",
+    group: "group"
+  });
+
+  it('shows sender', function (done) {
+    stubMailHeaders([displayedMailHeader]);
+
+    request(app)
+      .get('/list/group')
+      .expect(200)
+      .expect(/Sender Name/, function (err) {
+        done(err);
+      });
+  });
+
+  it('shows subject', function (done) {
+    stubMailHeaders([displayedMailHeader]);
+
+    request(app)
+      .get('/list/group')
+      .expect(200)
+      .expect(/Mail 1/, function (err) {
+        done(err);
+      });
+  });
+
+  it('shows mail time', function (done) {
+    stubMailHeaders([displayedMailHeader]);
+
+    request(app)
+      .get('/list/group')
+      .expect(200)
+      .expect(new RegExp(mailTime.format("LLLL")), function (err) {
+        done(err);
+      });
+  });
+
+  it('references sender member page if available', function (done) {
+    var displayedMailHeader = new Mail({
+      from: {name: "Sender Name", id: "sender ID"},
+      timeUnix: mailTime.unix(),
+      id: "<message1@nomail.com>",
+      subject: "Mail 1",
+      group: "group"
+    });
+    stubMailHeaders([displayedMailHeader]);
+
+    var dummyMember = {nickname: "nickname", id: "sender ID"};
+    sinonSandbox.stub(membersAPI, 'getMemberForId', function (id, callback) {callback(null, dummyMember); });
+
+    request(app)
+      .get('/list/group')
+      .expect(200)
+      .expect(/href="..\/..\/members\/nickname"/, function (err) {
+        done(err);
+      });
+  });
+});
