6371b7ce2e1307abf6504ae7f93d30c5ddfe6947
Dimitry Polivaev <dpolivaev@gmx.de>
2013-05-12 13:11:03 +0200

    correct error description text

--- test/persistence/testpersistence.js
+++ test/persistence/testpersistence.js
@@ -1,24 +1,24 @@
 "use strict";
 
 module.exports = {
-  list: function () {throw new Error('function undefined'); },
+  list: function () {throw new Error('function not implemented'); },
 
-  listByIds: function () {throw new Error('function undefined'); },
+  listByIds: function () {throw new Error('function not implemented'); },
 
-  listByEMails: function () {throw new Error('function undefined'); },
+  listByEMails: function () {throw new Error('function not implemented'); },
 
-  listByField: function () {throw new Error('function undefined'); },
+  listByField: function () {throw new Error('function not implemented'); },
 
-  listByFieldWithOptions: function () {throw new Error('function undefined'); },
+  listByFieldWithOptions: function () {throw new Error('function not implemented'); },
 
-  getById: function () {throw new Error('function undefined'); },
+  getById: function () {throw new Error('function not implemented'); },
 
-  getByField: function () {throw new Error('function undefined'); },
+  getByField: function () {throw new Error('function not implemented'); },
 
-  save: function () {throw new Error('function undefined'); },
+  save: function () {throw new Error('function not implemented'); },
 
-  saveAll: function () {throw new Error('function undefined'); },
+  saveAll: function () {throw new Error('function not implemented'); },
 
-  drop: function () {throw new Error('function undefined'); }
+  drop: function () {throw new Error('function not implemented'); }
 };
 
