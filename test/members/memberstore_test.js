08e9ee5978a6cb40eb399db9163dc2e6c75127b3
Dimitry Polivaev <dpolivaev@gmx.de>
2013-05-20 10:29:21 +0200

    refactor memberstore_tests and fix bug : call trim() in getMember()

--- test/members/memberstore_test.js
+++ test/members/memberstore_test.js
@@ -13,25 +13,16 @@ describe('Members store', function () {
   var sampleMember = {nickname: 'nick'};
   var sampleMember2 = {nickname: 'nick2'};
   var sampleList = [sampleMember, sampleMember2];
-  var getByField;
-  var save;
 
-  before(function (done) {
-    getByField = sinonSandbox.stub(persistence, 'getByField');
-    getByField.callsArgWith(1, null, sampleMember);
-    var list = sinonSandbox.stub(persistence, 'list');
-    list.callsArgWith(1, null, sampleList);
-    save = sinonSandbox.stub(persistence, 'save');
-    save.callsArg(1);
-    done();
-  });
-
-  after(function (done) {
+  afterEach(function (done) {
     sinonSandbox.restore();
     done();
   });
 
   it('calls persistence.getByField for store.getMember and passes on the given callback', function (done) {
+    var getByField = sinonSandbox.stub(persistence, 'getByField');
+    getByField.callsArgWith(1, null, sampleMember);
+
     store.getMember('nick', function (err, member) {
       expect(member.nickname).to.equal(sampleMember.nickname);
       expect(getByField.calledWith({nickname: new RegExp()})).to.be.true;
@@ -40,6 +31,9 @@ describe('Members store', function () {
   });
 
   it('calls persistence.getByField trimmed for store.getMember and passes on the given callback', function (done) {
+    var getByField = sinonSandbox.stub(persistence, 'getByField');
+    getByField.callsArgWith(1, null, sampleMember);
+
     store.getMember('  nick  ', function (err, member) {
       expect(member.nickname).to.equal(sampleMember.nickname);
       expect(getByField.calledWith({nickname: new RegExp()})).to.be.true;
@@ -50,6 +44,9 @@ describe('Members store', function () {
   });
 
   it('calls persistence.getByField with an appropriate regex', function (done) {
+    var getByField = sinonSandbox.stub(persistence, 'getByField');
+    getByField.callsArgWith(1, null, sampleMember);
+
     store.getMember('nick', function (err, member) {
       expect(member.nickname).to.equal(sampleMember.nickname);
       expect(getByField.calledWith({nickname: new RegExp()})).to.be.true;
@@ -62,6 +59,9 @@ describe('Members store', function () {
   });
 
   it('calls persistence.list for store.allMembers and passes on the given callback', function (done) {
+    var list = sinonSandbox.stub(persistence, 'list');
+    list.callsArgWith(1, null, sampleList);
+
     store.allMembers(function (err, members) {
       expect(members[0].nickname).to.equal(sampleMember.nickname);
       expect(members[1].nickname).to.equal(sampleMember2.nickname);
@@ -70,6 +70,9 @@ describe('Members store', function () {
   });
 
   it('calls persistence.save for store.saveMember and passes on the given callback', function (done) {
+    var save = sinonSandbox.stub(persistence, 'save');
+    save.callsArg(1);
+
     store.saveMember(sampleMember, function (err) {
       expect(save.calledWith(sampleMember)).to.be.true;
       done(err);
