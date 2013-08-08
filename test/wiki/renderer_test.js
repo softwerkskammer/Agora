8520d1817d355347faaced5527479a95497baf7a
leider <derleider@web.de>
2013-08-08 23:53:12 +0200

    tests for the renderer

---
+++ test/wiki/renderer_test.js
@@ -0,0 +1,44 @@
+"use strict";
+var chai = require("chai");
+var expect = chai.expect;
+
+var Renderer = require("../../lib/wiki/renderer");
+
+describe("Renderer", function () {
+
+  it("should render bracket tags1", function () {
+    var text = "a [[Foo]] b";
+    expect(Renderer.render(text)).to.be.equal("<p>a <a class=\"internal\" href=\"/wiki/foo\">Foo</a> b</p>\n");
+  });
+
+  it("should render bracket tags2", function () {
+    var text = "a [[Foo]][[Foo]][[Foo]] b";
+    expect(Renderer.render(text)).to.be.equal("<p>a <a class=\"internal\" href=\"/wiki/foo\">Foo</a><a class=\"internal\" href=\"/wiki/foo\">Foo</a><a class=\"internal\" href=\"/wiki/foo\">Foo</a> b</p>\n");
+  });
+
+  it("should render bracket tags3", function () {
+    var text = "a [[Foo Bar]] b";
+    expect(Renderer.render(text)).to.be.equal("<p>a <a class=\"internal\" href=\"/wiki/foo-bar\">Foo Bar</a> b</p>\n");
+  });
+
+  it("should render bracket tags4", function () {
+    var text = "a [[Foo]][[Bar]] b";
+    expect(Renderer.render(text)).to.be.equal("<p>a <a class=\"internal\" href=\"/wiki/foo\">Foo</a><a class=\"internal\" href=\"/wiki/bar\">Bar</a> b</p>\n");
+  });
+
+  it("should render bracket tags5", function () {
+    var text = "a [[Foo]] [[Bar]] b";
+    expect(Renderer.render(text)).to.be.equal("<p>a <a class=\"internal\" href=\"/wiki/foo\">Foo</a> <a class=\"internal\" href=\"/wiki/bar\">Bar</a> b</p>\n");
+  });
+
+  it("should render bracket tags6", function () {
+    var text = "a [[Il marito di Foo|Foobar]] [[Bar]] b";
+    expect(Renderer.render(text)).to.be.equal("<p>a <a class=\"internal\" href=\"/wiki/foobar\">Il marito di Foo</a> <a class=\"internal\" href=\"/wiki/bar\">Bar</a> b</p>\n");
+  });
+
+  it("should render bracket tags7", function () {
+    var text = "a [[Foo / Bar]] b";
+    expect(Renderer.render(text)).to.be.equal("<p>a <a class=\"internal\" href=\"/wiki/foo---bar\">Foo / Bar</a> b</p>\n");
+  });
+
+});
