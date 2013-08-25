8a43e22652bc62caeb66f5accb94fac4664b3574
leider <derleider@web.de>
2013-08-25 16:17:03 +0200

    jshint and test adaption

--- test/wiki/renderer_test.js
+++ test/wiki/renderer_test.js
@@ -8,37 +8,37 @@ describe("Renderer", function () {
 
   it("should render bracket tags1", function () {
     var text = "a [[Foo]] b";
-    expect(Renderer.render(text)).to.equal("<p>a <a class=\"internal\" href=\"/wiki/foo\">Foo</a> b</p>\n");
+    expect(Renderer.render(text, 'subdir')).to.equal("<p>a <a class=\"internal\" href=\"/wiki/subdir/foo\">Foo</a> b</p>\n");
   });
 
   it("should render bracket tags2", function () {
     var text = "a [[Foo]][[Foo]][[Foo]] b";
-    expect(Renderer.render(text)).to.be.equal("<p>a <a class=\"internal\" href=\"/wiki/foo\">Foo</a><a class=\"internal\" href=\"/wiki/foo\">Foo</a><a class=\"internal\" href=\"/wiki/foo\">Foo</a> b</p>\n");
+    expect(Renderer.render(text, 'subdir')).to.be.equal("<p>a <a class=\"internal\" href=\"/wiki/subdir/foo\">Foo</a><a class=\"internal\" href=\"/wiki/subdir/foo\">Foo</a><a class=\"internal\" href=\"/wiki/subdir/foo\">Foo</a> b</p>\n");
   });
 
   it("should render bracket tags3", function () {
     var text = "a [[Foo Bar]] b";
-    expect(Renderer.render(text)).to.be.equal("<p>a <a class=\"internal\" href=\"/wiki/foo%20bar\">Foo Bar</a> b</p>\n");
+    expect(Renderer.render(text, 'subdir')).to.be.equal("<p>a <a class=\"internal\" href=\"/wiki/subdir/foo%20bar\">Foo Bar</a> b</p>\n");
   });
 
   it("should render bracket tags4", function () {
     var text = "a [[Foo]][[Bar]] b";
-    expect(Renderer.render(text)).to.be.equal("<p>a <a class=\"internal\" href=\"/wiki/foo\">Foo</a><a class=\"internal\" href=\"/wiki/bar\">Bar</a> b</p>\n");
+    expect(Renderer.render(text, 'subdir')).to.be.equal("<p>a <a class=\"internal\" href=\"/wiki/subdir/foo\">Foo</a><a class=\"internal\" href=\"/wiki/subdir/bar\">Bar</a> b</p>\n");
   });
 
   it("should render bracket tags5", function () {
     var text = "a [[Foo]] [[Bar]] b";
-    expect(Renderer.render(text)).to.be.equal("<p>a <a class=\"internal\" href=\"/wiki/foo\">Foo</a> <a class=\"internal\" href=\"/wiki/bar\">Bar</a> b</p>\n");
+    expect(Renderer.render(text, 'subdir')).to.be.equal("<p>a <a class=\"internal\" href=\"/wiki/subdir/foo\">Foo</a> <a class=\"internal\" href=\"/wiki/subdir/bar\">Bar</a> b</p>\n");
   });
 
   it("should render bracket tags6", function () {
     var text = "a [[Il marito di Foo|Foobar]] [[Bar]] b";
-    expect(Renderer.render(text)).to.be.equal("<p>a <a class=\"internal\" href=\"/wiki/foobar\">Il marito di Foo</a> <a class=\"internal\" href=\"/wiki/bar\">Bar</a> b</p>\n");
+    expect(Renderer.render(text, 'subdir')).to.be.equal("<p>a <a class=\"internal\" href=\"/wiki/subdir/foobar\">Il marito di Foo</a> <a class=\"internal\" href=\"/wiki/subdir/bar\">Bar</a> b</p>\n");
   });
 
   it("should render bracket tags7", function () {
     var text = "a [[Foo / Bar]] b";
-    expect(Renderer.render(text)).to.be.equal("<p>a <a class=\"internal\" href=\"/wiki/foo%20%2F%20bar\">Foo / Bar</a> b</p>\n");
+    expect(Renderer.render(text, 'subdir')).to.be.equal("<p>a <a class=\"internal\" href=\"/wiki/subdir/foo%20%2F%20bar\">Foo / Bar</a> b</p>\n");
   });
 
 });
