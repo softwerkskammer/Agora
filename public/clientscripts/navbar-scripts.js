b49b1e38bb6887c496e9fd50888c996f3d1a5f96
leider <derleider@web.de>
2013-09-07 21:45:48 +0200

    UI tweaks

--- public/clientscripts/navbar-scripts.js
+++ public/clientscripts/navbar-scripts.js
@@ -5,7 +5,7 @@ var highlightCurrentSection = function () {
   var loc = window.location.href; // returns the full URL
   $('li').filter(function () {
     return this.id && new RegExp(this.id).test(loc);
-  }).addClass('active');
+  }).last().addClass('active');
 };
 
 var addHelpButtonToTextarea = function () {
