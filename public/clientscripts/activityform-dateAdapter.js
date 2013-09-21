da0283baaaf20ce6ef4ff596fb0d5de63359505a
Nicole Rauch <nicole.m@gmx.de>
2013-09-21 13:15:46 +0100

    Separated jQuery stuff and actual date manipulation.

--- public/clientscripts/activityform-dateAdapter.js
+++ public/clientscripts/activityform-dateAdapter.js
@@ -35,14 +35,12 @@ var dateAdapter = function () {
       // only update the field if it was not empty or if the date is not the same
       var newEndDateString = dateString(newEndDate);
       currentTimes.endDate = newEndDateString;
-      $('#endDate').val(currentTimes.endDate);
     }
   };
 
   var setEndTimeTo = function (currentTimes, newEndDate) {
     var newEndTimeString = timeString(newEndDate);
     currentTimes.endTime = newEndTimeString;
-    $('#endTime').val(currentTimes.endTime);
   };
 
   function calculateNewDates(currentTimes) {
@@ -64,17 +62,31 @@ var dateAdapter = function () {
     return currentTimes;
   }
 
+  var init = function (initialDate) {
+    oldStartDate = initialDate;
+  };
+
+  var oldStartDate;
+
+  
+
+  init(toUtc($('#startDate').val(), $('#startTime').val()));
+
+
   var listener = function () {
 
     var currentStartDate = $('#startDate').val();
     var currentStartTime = $('#startTime').val();
     var currentEndDate = $('#endDate').val();
     var currentEndTime = $('#endTime').val();
-    var currentTimes2 = {startDate: currentStartDate, startTime: currentStartTime, endDate: currentEndDate, endTime: currentEndTime};
-    var currentTimes = calculateNewDates(currentTimes2);
+
+    var currentTimes = calculateNewDates({startDate: currentStartDate, startTime: currentStartTime, endDate: currentEndDate, endTime: currentEndTime});
+
+    $('#endDate').val(currentTimes.endDate);
+    $('#endTime').val(currentTimes.endTime);
   };
 
-  var oldStartDate = toUtc($('#startDate').val(), $('#startTime').val());
+
   $('#startDate').change(listener);
   $('#startTime').change(listener);
 };
