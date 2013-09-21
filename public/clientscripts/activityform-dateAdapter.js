47eecf465bb83dc55000093103de00f430aa2778
Nicole Rauch <nicole.m@gmx.de>
2013-09-21 09:57:01 +0100

    Extracted method & indented file.

--- public/clientscripts/activityform-dateAdapter.js
+++ public/clientscripts/activityform-dateAdapter.js
@@ -1,72 +1,77 @@
 var dateAdapter = function () {
 
-    var toUtc = function (dateString, timeString) {
-        if (dateString && timeString) {
-            return moment.utc(dateString + " " + timeString, 'D.M.YYYY H:m');
-        }
-        return null;
-    };
-
-    var dateString = function (date) {
-        if (date) {
-            return date.format('DD.MM.YYYY');
-        }
-        return "";
-    };
-
-    var timeString = function (time) {
-        if (time) {
-            return time.format('HH:mm');
-        }
-        return "";
-    };
-
-    var endDayStringOr = function (currentEndDate, oldStartDate) {
-        // if the endDate field is empty, use the old contents of the start date field
-        return currentEndDate || dateString(oldStartDate);
-    };
-
-    function endFieldWasNotEmptyOrDateChanged(newEndDate, currentStartDate, currentEndDate) {
-        return currentEndDate || currentStartDate !== dateString(newEndDate);
+  var toUtc = function (dateString, timeString) {
+    if (dateString && timeString) {
+      return moment.utc(dateString + " " + timeString, 'D.M.YYYY H:m');
     }
+    return null;
+  };
 
-    var setEndDateTo = function (newEndDate) {
-        if (endFieldWasNotEmptyOrDateChanged(newEndDate, $('#startDate').val(), $('#endDate').val())) {
-            // only update the field if it was not empty or if the date is not the same
-            $('#endDate').val(dateString(newEndDate));
-        }
-    };
+  var dateString = function (date) {
+    if (date) {
+      return date.format('DD.MM.YYYY');
+    }
+    return "";
+  };
 
-    var setEndTimeTo = function (newEndDate) {
-        $('#endTime').val(timeString(newEndDate));
-    };
+  var timeString = function (time) {
+    if (time) {
+      return time.format('HH:mm');
+    }
+    return "";
+  };
 
-    var listener = function () {
+  var endDayStringOr = function (currentEndDate, oldStartDate) {
+    // if the endDate field is empty, use the old contents of the start date field
+    return currentEndDate || dateString(oldStartDate);
+  };
 
-        var currentStartDate = $('#startDate').val();
-        var currentStartTime = $('#startTime').val();
-        var currentEndDate = $('#endDate').val();
-        var currentEndTime = $('#endTime').val();
+  function endFieldWasNotEmptyOrDateChanged(newEndDate, currentStartDate, currentEndDate) {
+    return currentEndDate || currentStartDate !== dateString(newEndDate);
+  }
 
-        var newStartDate = toUtc(currentStartDate, currentStartTime);
+  var setEndDateTo = function (newEndDate) {
+    if (endFieldWasNotEmptyOrDateChanged(newEndDate, $('#startDate').val(), $('#endDate').val())) {
+      // only update the field if it was not empty or if the date is not the same
+      $('#endDate').val(dateString(newEndDate));
+    }
+  };
 
-        var offset = oldStartDate && newStartDate ? newStartDate.diff(oldStartDate, 'minutes') : 0;
+  var setEndTimeTo = function (newEndDate) {
+    $('#endTime').val(timeString(newEndDate));
+  };
 
-        var endDayString = endDayStringOr(currentEndDate, oldStartDate);
+  function calculateNewDates(currentStartDate, currentStartTime, currentEndDate, currentEndTime) {
+    var currentTimes = {startDate: currentStartDate, startTime: currentStartTime, endDate: currentEndDate, endTime: currentEndTime};
 
-        oldStartDate = newStartDate;
+    var newStartDate = toUtc(currentTimes.startDate, currentTimes.startTime);
 
-        if (offset !== 0) {
-            var newEndDate = toUtc(endDayString, currentEndTime).add(offset, 'minutes');
-            if (newEndDate) {
-                setEndDateTo(newEndDate);
-                setEndTimeTo(newEndDate);
-            }
-        }
-    };
+    var offset = oldStartDate && newStartDate ? newStartDate.diff(oldStartDate, 'minutes') : 0;
 
-    var oldStartDate = toUtc($('#startDate').val(), $('#startTime').val());
-    $('#startDate').change(listener);
-    $('#startTime').change(listener);
+    var endDayString = endDayStringOr(currentTimes.endDate, oldStartDate);
+
+    oldStartDate = newStartDate;
+
+    if (offset !== 0) {
+      var newEndDate = toUtc(endDayString, currentTimes.endTime).add(offset, 'minutes');
+      if (newEndDate) {
+        setEndDateTo(newEndDate);
+        setEndTimeTo(newEndDate);
+      }
+    }
+  }
+
+  var listener = function () {
+
+    var currentStartDate = $('#startDate').val();
+    var currentStartTime = $('#startTime').val();
+    var currentEndDate = $('#endDate').val();
+    var currentEndTime = $('#endTime').val();
+    calculateNewDates(currentStartDate, currentStartTime, currentEndDate, currentEndTime);
+  };
+
+  var oldStartDate = toUtc($('#startDate').val(), $('#startTime').val());
+  $('#startDate').change(listener);
+  $('#startTime').change(listener);
 };
 
