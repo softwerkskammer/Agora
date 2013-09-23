81bb0fdbe682927032af580e5962bc65c2025882
Nicole Rauch <nicole.m@gmx.de>
2013-09-23 18:42:10 +0200

    fixed linting problem.

--- test/frontend/editactivity-dateAdjustment-tests.js
+++ test/frontend/editactivity-dateAdjustment-tests.js
@@ -10,7 +10,8 @@ var vm = require('vm');
 var includeInThisContext = function (path) {
   var code = fs.readFileSync(path);
   vm.runInThisContext(code, path);
-}.bind(this);
+};
+
 includeInThisContext(__dirname + "/../../public/clientscripts/moment.js");
 includeInThisContext(__dirname + "/../../public/clientscripts/activityDateModel.js");
 
