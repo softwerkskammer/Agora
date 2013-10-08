aaa0c41ba2f9c8f39be543f829abd413946ba926
leider <derleider@web.de>
2013-10-08 22:25:48 +0200

    transposed activity migration script to async

--- activityMigrationScript.js
+++ activityMigrationScript.js
@@ -1,35 +1,31 @@
 'use strict';
 
 require('./configure'); // initializing parameters
-var _ = require('underscore');
+var async = require('async');
 
-var conf = require('nconf');
-
-var beans = conf.get('beans');
+var beans = require('nconf').get('beans');
 var fieldHelpers = beans.get('fieldHelpers');
 var persistence = beans.get('activitiesPersistence');
 var Activity = beans.get('activity');
 
-
-
 function logResult(err, message) {
-  if (err) {
-    console.log('An error occurred: ' + err);
-  } else {
-    console.log(message);
-  }
+  if (err) { return console.log('An error occurred: ' + err); }
+  console.log(message);
 }
 
 console.log('Migrating all existing activities...');
 persistence.list({}, function (err, activities) {
-  _.each(activities, function (activityObject) {
+  async.map(activities, function (activityObject, callback) {
     var activity = new Activity(activityObject);
     var momentInLocalTimezone = fieldHelpers._toMomentInTimezone(activity.startMoment().utc(), fieldHelpers.defaultTimezone());
     activityObject.startUnix = momentInLocalTimezone.unix(); // the db object must be updated (not the new activity)
     momentInLocalTimezone = fieldHelpers._toMomentInTimezone(activity.endMoment().utc(), fieldHelpers.defaultTimezone());
     activityObject.endUnix = momentInLocalTimezone.unix(); // the db object must be updated (not the new activity)
-  });
-  persistence.saveAll(activities, function (err) {
-    logResult(err, 'All existing activities are now migrated.');
+    callback(null, activityObject);
+  }, function (err, results) {
+    persistence.saveAll(results, function (err) {
+      logResult(err, 'All existing activities are now migrated.');
+      process.exit();
+    });
   });
 });
