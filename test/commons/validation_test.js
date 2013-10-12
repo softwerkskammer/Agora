023931b40ef9388f58558d95c9b5d9243070f244
Manuel Naujoks <mnaujoks@gmail.com>
2013-10-12 17:08:38 +0200

    #312: client validator for group names allows ß and test optimizations

--- test/commons/validation_test.js
+++ test/commons/validation_test.js
@@ -7,27 +7,27 @@ var expect = require('chai').expect;
 describe('isValidGroup function', function () {
 
   it('allows groupnames that contain no special characters', function () {
-    expect(isValidGroup("Gruppe")).to.be.empty;
+    expect(validationErrorsOf("Gruppe")).to.be.empty;
   });
 
   it('allows groupnames that contain ü, ö, ä', function () {
-    expect(isValidGroup("Grüppe")).to.be.empty;
-    expect(isValidGroup("Gräppe")).to.be.empty;
-    expect(isValidGroup("Gröppe")).to.be.empty;
+    expect(validationErrorsOf("Grüppe")).to.be.empty;
+    expect(validationErrorsOf("Gräppe")).to.be.empty;
+    expect(validationErrorsOf("Gröppe")).to.be.empty;
   });
 
   it('allows groupnames that contain Ü, Ä, Ö', function () {
-    expect(isValidGroup("GrÜppe")).to.be.empty;
-    expect(isValidGroup("GrÄppe")).to.be.empty;
-    expect(isValidGroup("GrÖppe")).to.be.empty;
+    expect(validationErrorsOf("GrÜppe")).to.be.empty;
+    expect(validationErrorsOf("GrÄppe")).to.be.empty;
+    expect(validationErrorsOf("GrÖppe")).to.be.empty;
   });
 
   it('allows groupnames that contain ß', function () {
-    expect(isValidGroup("Grßppe")).to.be.empty;
+    expect(validationErrorsOf("Grßppe")).to.be.empty;
   });
   
 
-  function isValidGroup(groupname) {
+  function validationErrorsOf(groupname) {
     return validation.isValidGroup({ id : groupname, emailPrefix : "12345", longName : "a", description : "a", type : "a"});
   }
 
