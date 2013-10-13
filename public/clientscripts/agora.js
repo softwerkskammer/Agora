a03cc9b5d23831c2888d6e64d8aa10ef9b8f3136
leider <derleider@web.de>
2013-10-13 16:57:59 +0200

    jQuery is not always happy with easy iterations :(

--- public/clientscripts/agora.js
+++ public/clientscripts/agora.js
@@ -159,6 +159,10 @@ $(document).ready(function () {
   $('.mailtoify').each(function () {
     $(this).html(surroundEmail(this.innerHTML));
   });
-  $("[rel=tooltip]").popover({html: true, trigger: "hover"});
-  $("[rel=tooltip-in-body]").popover({container: "body", html: true, trigger: "hover"});
+  $("[rel=tooltip]").each(function () {
+    $(this).popover({html: true, trigger: "hover"});
+  });
+  $("[rel=tooltip-in-body]").each(function () {
+    $(this).popover({container: "body", html: true, trigger: "hover"});
+  });
 });
