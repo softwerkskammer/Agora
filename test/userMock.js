43a16b795693f38809d6e9a083c13c8880129729
Nicole Rauch <nicole.m@gmx.de>
2013-10-13 16:57:12 +0200

    userMock now accepts a user on construction.

--- test/userMock.js
+++ test/userMock.js
@@ -1,6 +1,12 @@
 "use strict";
 
-module.exports = function (req, res, next) {
-  req.user = {};
-  next();
+module.exports = function (user) {
+
+  var userMock = function (req, res, next) {
+    req.user = user || {};
+    next();
+  };
+
+  return userMock;
 };
+
