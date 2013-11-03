9d49fc6dc42e9a6f740132b33fa4c8a0e1c8f17b
Nicole Rauch <nicole.m@gmx.de>
2013-11-03 18:14:36 +0100

    Fixed the submit test.

--- test/activities/activities_submit_test.js
+++ test/activities/activities_submit_test.js
@@ -3,36 +3,19 @@
 var request = require('supertest');
 var express = require('express');
 var sinon = require('sinon').sandbox.create();
-var expect = require('chai').expect;
 
 var conf = require('../configureForTest');
-var userMock = require('../userMock');
-
-var fieldHelpers = conf.get('beans').get('fieldHelpers');
-var Activity = conf.get('beans').get('activity');
-var Member = conf.get('beans').get('member');
-
-var emptyActivity = new Activity({title: 'Title of the Activity', description: 'description1', assignedGroup: 'assignedGroup',
-  location: 'location1', direction: 'direction1', startUnix: fieldHelpers.parseToUnixUsingDefaultTimezone('01.01.2013'), url: 'urlOfTheActivity' });
-var activityWithParticipants = new Activity({title: 'Interesting Activity', description: 'description2', assignedGroup: 'assignedGroup',
-  location: 'location2', direction: 'direction2', startUnix: fieldHelpers.parseToUnixUsingDefaultTimezone('01.01.2013'), url: 'urlForInteresting',
-  resources: {default: {_registeredMembers: ['memberId1', 'memberId2']}} });
-var activityWithMultipleResources = new Activity({title: 'Interesting Activity', description: 'description2', assignedGroup: 'assignedGroup',
-  location: 'location2', direction: 'direction2', startUnix: fieldHelpers.parseToUnixUsingDefaultTimezone('01.01.2013'), url: 'urlForMultiple',
-  resources: {Einzelzimmer: {_registeredMembers: ['memberId1', 'memberId2']}, Doppelzimmer: {_registeredMembers: ['memberId3', 'memberId4']}} });
-
 
 var activitiesCoreAPI = conf.get('beans').get('activitiesCoreAPI');
-var activitiesAPI = conf.get('beans').get('activitiesAPI');
-
-var groupsAPI = conf.get('beans').get('groupsAPI');
-var membersAPI = conf.get('beans').get('membersAPI');
-var validation = conf.get('beans').get('validation');
-var colors = conf.get('beans').get('colorAPI');
 
 var app = conf.get('beans').get('activitiesApp')(express());
 
 describe('Activity application - on submit -', function () {
+
+  afterEach(function (done) {
+    sinon.restore();
+    done();
+  });
 
   it('rejects an activity with invalid and different url', function (done) {
     sinon.stub(activitiesCoreAPI, 'isValidUrl', function (nickname, callback) {
