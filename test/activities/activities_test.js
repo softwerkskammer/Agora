acb276dceeff397aa6ccbf1dd300880749ae39b7
leider <derleider@web.de>
2013-12-07 14:08:11 +0100

    separation of display concerns - finished

--- test/activities/activities_test.js
+++ test/activities/activities_test.js
@@ -242,7 +242,7 @@ describe('Activity application', function () {
       request(createApp('memberId3'))
         .get('/' + 'urlForInteresting')
         .expect(200)
-        .expect(/kannst Du Dich nicht bei der Softwerkskammer anmelden/, function (err) {
+        .expect(/Anmeldung ist nicht über die Softwerkskammer möglich./, function (err) {
           done(err);
         });
     });
