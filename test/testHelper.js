f2a24cfe7be5b32ce7e8bec83d45485dae2dfb3e
leider <derleider@web.de>
2013-12-08 21:52:07 +0100

    fixing tests

--- test/testHelper.js
+++ test/testHelper.js
@@ -3,11 +3,13 @@
 var express = require('express');
 var conf = require('./configureForTest');
 var userMock = require('./userMock');
-
+var i18n = require('i18next');
 var beans = conf.get('beans');
 
 module.exports = function (internalAppName) {
   var appName = internalAppName;
+
+  i18n.init({  });
 
   return {
     createApp: function (memberID) {
@@ -17,7 +19,7 @@ module.exports = function (internalAppName) {
         app.use(userMock({member: {id: memberID}}));
       }
       app.use(beans.get('accessrights'));
-      app.use(require('i18next'));
+      app.use(i18n);
       app.use('/', beans.get(appName)(express()));
       return app;
     }
