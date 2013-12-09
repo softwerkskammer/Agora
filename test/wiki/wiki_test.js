b828ae88bb1ffff8a6e103885954b41a2b3dd665
Nicole Rauch <nicole.m@gmx.de>
2013-12-09 18:38:58 +0100

    Wiki test now uses testHelper.createApp.

--- test/wiki/wiki_test.js
+++ test/wiki/wiki_test.js
@@ -1,19 +1,13 @@
 "use strict";
 
 var request = require('supertest');
-var express = require('express');
 var sinon = require('sinon').sandbox.create();
 var expect = require('chai').expect;
-var userMock = require('../userMock');
 
 var beans = require('../configureForTest').get('beans');
 var wikiAPI = beans.get('wikiAPI');
 
-var app = express();
-app.use(express.urlencoded());
-app.use(beans.get('accessrights'));
-var wikiApp = beans.get('wikiApp')(express());
-app.use('/', wikiApp);
+var createApp = require('../testHelper')('wikiApp').createApp;
 
 describe('Wiki application', function () {
 
@@ -37,7 +31,7 @@ describe('Wiki application', function () {
   });
 
   it('shows the index page of a group when requested', function (done) {
-    request(app)
+    request(createApp())
       .get('/global/')
       .expect(200)
       .expect(new RegExp(content))
@@ -49,7 +43,7 @@ describe('Wiki application', function () {
   });
 
   it('redirects to the index page of group "alle" when requested root', function (done) {
-    request(app)
+    request(createApp())
       .get('/')
       .expect(302)
       .expect('Location', '/wiki/alle/')
@@ -59,10 +53,7 @@ describe('Wiki application', function () {
   });
 
   it('redirects to the edit page of a page when the page does not exist yet and a user is logged in', function (done) {
-    var root = express();
-    root.use(userMock());
-    root.use('/', app);
-    request(root)
+    request(createApp('member'))
       .get('/' + nonExistingPage)
       .expect(302)
       .expect('Location', '/wiki/edit/' + nonExistingPage)
@@ -72,7 +63,7 @@ describe('Wiki application', function () {
   });
 
   it('redirects to 404 page when the page does not exist yet and a user is not logged in', function (done) {
-    request(app)
+    request(createApp())
       .get('/' + nonExistingPage)
       .expect(404)
       .end(function (err) {
