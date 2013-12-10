29d0364816bd0b139e7b95eab5315d7943bfb06f
Nicole Rauch <nicole.m@gmx.de>
2013-12-10 22:43:17 +0100

    How on earth could this ever have worked?!

--- test/testHelper.js
+++ test/testHelper.js
@@ -4,12 +4,18 @@ var express = require('express');
 var conf = require('./configureForTest');
 var userMock = require('./userMock');
 var i18n = require('i18next');
+var jade = require("jade");
 var beans = conf.get('beans');
 
 module.exports = function (internalAppName) {
   var appName = internalAppName;
 
-  i18n.init({  });
+  i18n.init({
+    supportedLngs: ['de'],
+    preload: ['de'],
+    fallbackLng: 'de',
+    resGetPath: 'locales/__ns__-__lng__.json'
+  });
 
   return {
     createApp: function (memberID) {
@@ -19,8 +25,13 @@ module.exports = function (internalAppName) {
         app.use(userMock({member: {id: memberID}}));
       }
       app.use(beans.get('accessrights'));
-      app.use(i18n);
+      app.use(i18n.handle);
       app.use('/', beans.get(appName)(express()));
+
+      i18n.registerAppHelper(app);
+      i18n.addPostProcessor("jade", function (val, key, opts) {
+        return jade.compile(val, opts)();
+      });
       return app;
     }
   };
