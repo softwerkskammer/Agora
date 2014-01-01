a1885bdff47572e6cbed7288a71b39c66df21166
Nicole Rauch <nicole.m@gmx.de>
2014-01-01 16:56:39 +0100

    Improved our timezone adaption code because it would yield incorrect results (& break tests) on Dec 31st and Jan 31st between 23:00 and midnight.

---
+++ test/commons/momentTimezoneQuirks.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var expect = require('chai').expect;
+var moment = require('moment-timezone');
+
+describe('timezone', function () {
+
+  it('shifts the time (and thus possibly the date) when switching from winter to summer', function () {
+    var result = moment('30.11.2013 23:30', 'D.M.YYYY H:m').tz('Europe/Berlin');
+    result.month(7); // set to August (!)
+    expect(result.format(), "Resulting date is 31st, not 30th!").to.equal('2013-08-31T00:30:00+02:00');
+  });
+
+  it('time shift is especially nasty when it shifts the month after setting the month', function () {
+    var result = moment('31.1.2013 23:30', 'D.M.YYYY H:m').tz('Europe/Berlin');
+    result.month(7); // set to August (!)
+    expect(result.format(), "Resulting month is September, not August!").to.equal('2013-09-01T00:30:00+02:00');
+  });
+
+});
