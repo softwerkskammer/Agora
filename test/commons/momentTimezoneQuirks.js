1dc5d75b0d529030aabd4f0c2c38fea4365c40a0
Nicole Rauch <nicole.m@gmx.de>
2014-01-02 13:12:59 +0100

    Now creating the moment in UTC - this allows to apply arbitrary formatting of the input

--- test/commons/momentTimezoneQuirks.js
+++ test/commons/momentTimezoneQuirks.js
@@ -6,13 +6,13 @@ var moment = require('moment-timezone');
 describe('timezone', function () {
 
   it('shifts the time (and thus possibly the date) when switching from winter to summer', function () {
-    var result = moment.tz('2013-11-30 23:30', 'Europe/Berlin');
+    var result = moment.utc('30.11.2013 23:30', 'D.M.YYYY H:m').tz('Europe/Berlin');
     result.month(7); // set to August (!)
     expect(result.format(), "Resulting date is 31st, not 30th!").to.equal('2013-08-31T00:30:00+02:00');
   });
 
   it('time shift is especially nasty when it shifts the month after setting the month', function () {
-    var result = moment.tz('2013-01-31 23:30', 'Europe/Berlin');
+    var result = moment.utc('31.1.2013 23:30', 'D.M.YYYY H:m').tz('Europe/Berlin');
     result.month(7); // set to August (!)
     expect(result.format(), "Resulting month is September, not August!").to.equal('2013-09-01T00:30:00+02:00');
   });
