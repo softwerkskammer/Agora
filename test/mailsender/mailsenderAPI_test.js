7875506e1a4f458a78e567e083a3d2d19a2c3c69
leider <derleider@web.de>
2014-01-03 14:29:11 +0100

    test for sending reminder for activity

--- test/mailsender/mailsenderAPI_test.js
+++ test/mailsender/mailsenderAPI_test.js
@@ -11,11 +11,12 @@ var activitiesAPI = beans.get('activitiesAPI');
 var api = beans.get('mailsenderAPI');
 var Activity = beans.get('activity');
 var Member = beans.get('member');
+var Message = beans.get('message');
 var fieldHelpers = beans.get('fieldHelpers');
+var mailtransport = beans.get('mailtransport');
 
 var emptyActivity = new Activity({title: 'Title of the Activity', description: 'description1', assignedGroup: 'assignedGroup',
   location: 'location1', direction: 'direction1', startUnix: fieldHelpers.parseToUnixUsingDefaultTimezone('01.01.2013'), url: 'urlOfTheActivity' });
-
 
 describe('MailsenderAPI', function () {
   var activityURL = 'acti_vi_ty';
@@ -36,54 +37,99 @@ describe('MailsenderAPI', function () {
     done();
   });
 
-  it('collects data for showing the edit form for an activity', function (done) {
-    api.dataForShowingMessageForActivity(activityURL, 'de', function (err, result) {
-      expect(result.message).to.exist;
-      expect(result.regionalgroups).to.exist;
-      expect(result.themegroups).to.exist;
-      expect(result.successURL).to.contain(activityURL);
-      done();
+  describe('preparing data', function () {
+    it('for showing the edit form for an activity', function (done) {
+      api.dataForShowingMessageForActivity(activityURL, 'de', function (err, result) {
+        expect(result.message).to.exist;
+        expect(result.regionalgroups).to.exist;
+        expect(result.themegroups).to.exist;
+        expect(result.successURL).to.contain(activityURL);
+        done();
+      });
+    });
+
+    it('for showing the edit form for a member', function (done) {
+      api.dataForShowingMessageToMember(nickname, function (err, result) {
+        expect(result.message).to.exist;
+        expect(result.regionalgroups).to.not.exist;
+        expect(result.themegroups).to.not.exist;
+        expect(result.successURL).to.contain(nickname);
+        done();
+      });
     });
   });
 
-  it('collects data for showing the edit form for a member', function (done) {
-    api.dataForShowingMessageToMember(nickname, function (err, result) {
-      expect(result.message).to.exist;
-      expect(result.regionalgroups).to.not.exist;
-      expect(result.themegroups).to.not.exist;
-      expect(result.successURL).to.contain(nickname);
-      done();
+  describe('activity markdown', function () {
+    it('with direction', function () {
+      var activity = new Activity().fillFromUI({
+        url: 'url',
+        description: 'description',
+        location: 'location',
+        direction: 'direction',
+        startDate: '4.5.2013',
+        startTime: '12:21'
+      });
+      var markdown = api.activityMarkdown(activity);
+      expect(markdown).to.contain('description');
+      expect(markdown).to.contain('4. Mai 2013');
+      expect(markdown).to.contain('12:21');
+      expect(markdown).to.contain('location');
+      expect(markdown).to.contain('Wegbeschreibung');
+      expect(markdown).to.contain('direction');
+    });
+
+    it('without direction', function () {
+      var activity = new Activity({
+        url: 'url',
+        description: 'description',
+        location: 'location',
+        direction: '',
+        startDate: '4.5.2013',
+        startTime: '12:21'
+      });
+      expect(api.activityMarkdown(activity)).to.not.contain('Wegbeschreibung');
     });
   });
 
-  it('creates the activity markdown with direction', function () {
-    var activity = new Activity().fillFromUI({
-      url: 'url',
-      description: 'description',
-      location: 'location',
-      direction: 'direction',
-      startDate: '4.5.2013',
-      startTime: '12:21'
+  describe('sending mail', function () {
+    var sendmail;
+    beforeEach(function () {
+      sendmail = sinon.stub(mailtransport, 'sendMail', function (transportobject, callback) {
+        if (!transportobject.bcc || transportobject.bcc.length === 0) {
+          // simulating the behaviour of nodemailer
+          return callback(new Error());
+        }
+        callback(null);
+      });
     });
-    var markdown = api.activityMarkdown(activity);
-    expect(markdown).to.contain('description');
-    expect(markdown).to.contain('4. Mai 2013');
-    expect(markdown).to.contain('12:21');
-    expect(markdown).to.contain('location');
-    expect(markdown).to.contain('Wegbeschreibung');
-    expect(markdown).to.contain('direction');
-  });
 
-  it('creates the activity markdown without direction', function () {
-    var activity = new Activity({
-      url: 'url',
-      description: 'description',
-      location: 'location',
-      direction: '',
-      startDate: '4.5.2013',
-      startTime: '12:21'
+    afterEach(function () {
+      sinon.restore();
     });
-    expect(api.activityMarkdown(activity)).to.not.contain('Wegbeschreibung');
+
+    it('as reminder for activity sends to vistors of activity', function (done) {
+      var emailAddress = 'emailAddress@e.mail';
+      var message = new Message({subject: 'subject', markdown: 'mark down'}, new Member());
+      emptyActivity.visitors = [new Member({email: emailAddress})];
+
+      api.sendMailToParticipantsOf(activityURL, message, function (err) {
+        expect(sendmail.calledOnce).to.be.ok;
+        var transportobject = sendmail.args[0][0];
+        expect(transportobject.bcc).to.contain(emailAddress);
+        expect(transportobject.html).to.contain('mark down');
+        done(err);
+      });
+    });
+
+    it('as reminder for activity does not send mail if no visitors', function (done) {
+      var message = new Message({subject: 'subject', markdown: 'mark down'}, new Member());
+      emptyActivity.visitors = [];
+
+      api.sendMailToParticipantsOf(activityURL, message, function (err) {
+        expect(err).to.exist;
+        done();
+      });
+    });
   });
 
 });
