542798b49afc64bbff812b09bebb89c784839b39
Nicole Rauch <nicole.m@gmx.de>
2014-01-05 21:35:17 +0100

    - Changed test from string list to array - Added tests for string lists and arrays - Generally grouped tests

--- test/groups/group_object_test.js
+++ test/groups/group_object_test.js
@@ -9,42 +9,66 @@ var expect = require('chai').expect;
 
 describe('Group object', function () {
 
-  it('should deliver two types of groups', function () {
+  describe('constructor', function () {
+
+    it('reads a single organizer into an array', function () {
+      var group = new Group({id: 'group', organizers: 'id'});
+
+      expect(group.organizers).to.be.an('Array');
+      expect(group.organizers).to.contain('id');
+    });
+
+    it('reads two organizers into an array', function () {
+      var group = new Group({id: 'group', organizers: 'idA,idB'});
+
+      expect(group.organizers).to.be.an('Array');
+      expect(group.organizers).to.contain('idA');
+      expect(group.organizers).to.contain('idB');
+    });
+
+    it('leaves an organizers array as-is', function () {
+      var group = new Group({id: 'group', organizers: ['idA', 'idB'] });
+
+      expect(group.organizers).to.be.an('Array');
+      expect(group.organizers).to.contain('idA');
+      expect(group.organizers).to.contain('idB');
+    });
+
+  });
+
+});
+
+describe('should deliver', function () {
+
+  it('two types of groups', function () {
     var allTypes = Group.allTypes();
     expect(allTypes.length).to.equal(2);
     expect(allTypes).to.contain('Themengruppe');
     expect(allTypes).to.contain('Regionalgruppe');
   });
 
-  it('should deliver the group for Themengruppe', function () {
+  it('the group for Themengruppe', function () {
     var group = new Group({id: 'abc', type: 'Themengruppe'});
     expect(Group.thematicsFrom([group])).to.deep.equal([group]);
     expect(Group.regionalsFrom([group])).to.deep.equal([]);
   });
 
-  it('should deliver the group for Regionalgruppe', function () {
+  it('the group for Regionalgruppe', function () {
     var group = new Group({id: 'abc', type: 'Regionalgruppe'});
     expect(Group.thematicsFrom([group])).to.deep.equal([]);
     expect(Group.regionalsFrom([group])).to.deep.equal([group]);
   });
 
-  it('should transform the id to lowercase', function () {
-    var group = new Group({id: 'NeuePlattform'});
-    expect(group.id).to.equal('neueplattform');
-  });
+});
 
-  it('should generate a list for the organizers based on members, only one in organizers', function () {
-    var group = new Group({id: 'NeuePlattform', organizers: ['Hans', 'Heinz']});
-    var members = [new Member({id: 'Hans'}), new Member({id: 'Karl'})];
-    var checkedOrganizers = group.checkedOrganizers(members);
-    expect(checkedOrganizers.length).to.equal(2);
-    expect(checkedOrganizers[0].member.id()).to.equal('Hans');
-    expect(checkedOrganizers[0].checked).to.be.true;
-    expect(checkedOrganizers[1].member.id()).to.equal('Karl');
-    expect(checkedOrganizers[1].checked).to.be.false;
-  });
+it('should transform the id to lowercase', function () {
+  var group = new Group({id: 'NeuePlattform'});
+  expect(group.id).to.equal('neueplattform');
+});
 
-  it('descriptionHTMLFiltered should filter the description html by given matchOpenClosedTag', function () {
+describe('descriptionHTMLFiltered should filter the description html by given', function () {
+
+  it('matchOpenClosedTag', function () {
     var descriptionHTML = function () {
       return '<a href="#">asda</a>description';
     };
@@ -54,7 +78,7 @@ describe('Group object', function () {
     expect(group.descriptionHTMLFiltered("a")).to.equal('description');
   });
 
-  it('descriptionHTMLFiltered should filter the description html by given matchSingleClosedTag', function () {
+  it('matchSingleClosedTag', function () {
     var descriptionHTML = function () {
       return '<img src="#"/>description';
     };
@@ -64,7 +88,7 @@ describe('Group object', function () {
     expect(group.descriptionHTMLFiltered("img")).to.equal('description');
   });
 
-  it('descriptionHTMLFiltered should filter the description html by given matchSingleTag', function () {
+  it('matchSingleTag', function () {
     var descriptionHTML = function () {
       return '<img src="#">description';
     };
@@ -74,43 +98,63 @@ describe('Group object', function () {
     expect(group.descriptionHTMLFiltered("img")).to.equal('description');
   });
 
-  it('answers that a memberId is one of its organizers', function () {
+});
+
+it('should generate a list for the organizers based on members, only one in organizers', function () {
+  var group = new Group({id: 'NeuePlattform', organizers: ['Hans', 'Heinz']});
+  var members = [new Member({id: 'Hans'}), new Member({id: 'Karl'})];
+  var checkedOrganizers = group.checkedOrganizers(members);
+  expect(checkedOrganizers.length).to.equal(2);
+  expect(checkedOrganizers[0].member.id()).to.equal('Hans');
+  expect(checkedOrganizers[0].checked).to.be.true;
+  expect(checkedOrganizers[1].member.id()).to.equal('Karl');
+  expect(checkedOrganizers[1].checked).to.be.false;
+});
+
+describe('answers that a', function () {
+
+  it('memberId is one of its organizers', function () {
     var group = new Group({id: 'groupA', organizers: 'id'});
     expect(group.isOrganizer('id')).to.be.true;
   });
 
-  it('answers that a wrong memberId is not one of its organizers', function () {
+  it('wrong memberId is not one of its organizers', function () {
     var group = new Group({id: 'groupA', organizers: 'id'});
     expect(group.isOrganizer('anotherId')).to.be.false;
   });
 
-  it('answers that a memberId is not one of its organizers if there are no organizers initialized', function () {
+  it('memberId is not one of its organizers if there are no organizers initialized', function () {
     var group = new Group();
     expect(group.isOrganizer('anotherId')).to.be.false;
   });
-  
-  it('delivers the symmetric difference of organizers to no other group (first arg is undefined)', function () {
+
+});
+
+describe('delivers the symmetric difference of organizers to', function () {
+
+  it('no other group (first arg is undefined)', function () {
     var group = new Group({id: 'groupA', organizers: 'id'});
     expect(Group.organizersOnlyInOneOf(undefined, group)).to.contain('id');
   });
 
-  it('delivers the symmetric difference of organizers to no other group (second arg is undefined)', function () {
+  it('no other group (second arg is undefined)', function () {
     var group = new Group({id: 'groupA', organizers: 'id'});
     expect(Group.organizersOnlyInOneOf(group)).to.contain('id');
   });
 
-  it('delivers the symmetric difference of organizers to another group (identical organizers)', function () {
+  it('another group (identical organizers)', function () {
     var groupA = new Group({id: 'groupA', organizers: 'id'});
     var groupB = new Group({id: 'groupB', organizers: 'id'});
     expect(Group.organizersOnlyInOneOf(groupA, groupB)).to.not.contain('id');
   });
 
-  it('delivers the symmetric difference of organizers to another group (additional organizers on both sides)', function () {
-    var groupA = new Group({id: 'groupA', organizers: 'id,idA'});
-    var groupB = new Group({id: 'groupB', organizers: 'id,idB'});
+  it('another group (additional organizers on both sides)', function () {
+    var groupA = new Group({id: 'groupA', organizers: ['id', 'idA'] });
+    var groupB = new Group({id: 'groupB', organizers: ['id', 'idB'] });
     expect(Group.organizersOnlyInOneOf(groupA, groupB)).to.contain('idA');
     expect(Group.organizersOnlyInOneOf(groupA, groupB)).to.contain('idB');
     expect(Group.organizersOnlyInOneOf(groupA, groupB)).to.not.contain('id');
   });
 
 });
+
