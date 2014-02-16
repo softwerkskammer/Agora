35263ca20ee0f513dec82df8ee64e41d91d10474
leider <derleider@web.de>
2014-02-16 14:37:21 +0100

    test cleanup

--- test/middleware/detectBrowser_test.js
+++ test/middleware/detectBrowser_test.js
@@ -8,9 +8,10 @@ var expect = require('chai').expect;
 describe('Detecting Browser', function () {
 
   function checkUserAgent(string) {
-    var res = {locals: {}};
     var req = {headers: {'user-agent': string}};
-    detectBrowser(req, res, function () {});
+    var res = {locals: {}};
+    var next = function () {};
+    detectBrowser(req, res, next);
     return res.locals.browserIsTooOld;
   }
 
