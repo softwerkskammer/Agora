65d5e456655817e68a6c172240f7fc6285e36857
Nicole Rauch <nicole.m@gmx.de>
2014-02-16 15:58:02 +0100

    In order to test middleware in integration, we need to be able to inject it into the test app.

--- test/testHelper.js
+++ test/testHelper.js
@@ -17,15 +17,17 @@ module.exports = function (internalAppName, configuredBeans) {
   });
 
   return {
-    createApp: function (memberID) {
+    createApp: function (memberID, middleware) {
       var app = express();
       app.use(express.cookieParser());
       app.use(express.urlencoded());
       app.use(express.session({secret: 'secret', cookie: {maxAge: 10000}, store: null}));
-
-      var Member = beans.get('member');
+      if (middleware) {
+        app.use(middleware);
+      }
 
       if (memberID) {
+        var Member = beans.get('member');
         app.use(userMock({member: new Member({id: memberID})}));
       }
       app.use(beans.get('accessrights'));
