d2e99d61b0b8832238c7689ccb741b826b828913
Nicole Rauch <nicole.m@gmx.de>
2014-02-24 22:50:49 +0100

    Now all occurrences of the path prefix are removed.

--- test/middleware/serverpathRemover_test.js
+++ test/middleware/serverpathRemover_test.js
@@ -25,21 +25,25 @@ describe('serverpathRemover removes any paths', function () {
   });
 
   it('that comes before lib or node_modules', function () {
-    expect(removeServerpaths('error 500 /dir1/dir2/dir3/lib/node_modules/etc and also /path1/path2/path3/node_modules/lib/etc')).to.equal('error 500 lib/node_modules/etc and also node_modules/lib/etc');
+    expect(removeServerpaths('error 500 /dir1/dir2/dir3/lib/node_modules/etc and also /dir1/dir2/dir3/node_modules/lib/etc')).to.equal('error 500 lib/node_modules/etc and also node_modules/lib/etc');
+  });
+
+  it('that occur in the string, even if not followed by lib or node_modules', function () {
+    expect(removeServerpaths('error 500 /dir1/dir2/dir3/lib/node_modules/etc and also /dir1/dir2/dir3/some_file.js')).to.equal('error 500 lib/node_modules/etc and also some_file.js');
   });
 
   it('copes with long strings', function () {
     var original = "Error: Empfänger wurde nicht gefunden.\n" +
-    "at /Users/rauch/Developer/GitRepositories/Agora/lib/mailsender/mailsenderAPI.js:76:37\n" +
-    "at toMember (/Users/rauch/Developer/GitRepositories/Agora/lib/members/memberstore.js:12:3)\n" +
-    "at bound (/Users/rauch/Developer/GitRepositories/Agora/node_modules/lodash/dist/lodash.js:957:21)\n" +
-    "at /Users/rauch/Developer/GitRepositories/Agora/lib/persistence/persistence.js:51:11\n" +
-    "at /Users/rauch/Developer/GitRepositories/Agora/node_modules/mongodb/lib/mongodb/cursor.js:162:16\n" +
-    "at commandHandler (/Users/rauch/Developer/GitRepositories/Agora/node_modules/mongodb/lib/mongodb/cursor.js:706:16)\n" +
-    "at /Users/rauch/Developer/GitRepositories/Agora/node_modules/mongodb/lib/mongodb/db.js:1806:9\n" +
-    "at Server.Base._callHandler (/Users/rauch/Developer/GitRepositories/Agora/node_modules/mongodb/lib/mongodb/connection/base.js:442:41)\n" +
-    "at /Users/rauch/Developer/GitRepositories/Agora/node_modules/mongodb/lib/mongodb/connection/server.js:485:18\n" +
-    "at MongoReply.parseBody (/Users/rauch/Developer/GitRepositories/Agora/node_modules/mongodb/lib/mongodb/responses/mongo_reply.js:68:5)";
+    "at /Users/user/Developer/GitRepositories/Agora/lib/mailsender/mailsenderAPI.js:76:37\n" +
+    "at toMember (/Users/user/Developer/GitRepositories/Agora/lib/members/memberstore.js:12:3)\n" +
+    "at bound (/Users/user/Developer/GitRepositories/Agora/node_modules/lodash/dist/lodash.js:957:21)\n" +
+    "at /Users/user/Developer/GitRepositories/Agora/lib/persistence/persistence.js:51:11\n" +
+    "at /Users/user/Developer/GitRepositories/Agora/node_modules/mongodb/lib/mongodb/cursor.js:162:16\n" +
+    "at commandHandler (/Users/user/Developer/GitRepositories/Agora/node_modules/mongodb/lib/mongodb/cursor.js:706:16)\n" +
+    "at /Users/user/Developer/GitRepositories/Agora/node_modules/mongodb/lib/mongodb/db.js:1806:9\n" +
+    "at Server.Base._callHandler (/Users/user/Developer/GitRepositories/Agora/node_modules/mongodb/lib/mongodb/connection/base.js:442:41)\n" +
+    "at /Users/user/Developer/GitRepositories/Agora/node_modules/mongodb/lib/mongodb/connection/server.js:485:18\n" +
+    "at MongoReply.parseBody (/Users/user/Developer/GitRepositories/Agora/node_modules/mongodb/lib/mongodb/responses/mongo_reply.js:68:5)";
 
     var shortened = "Error: Empfänger wurde nicht gefunden.\n" +
     "at lib/mailsender/mailsenderAPI.js:76:37\n" +
