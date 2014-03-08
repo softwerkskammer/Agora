37ebbbb3b0cda42529fcde55d61d881d6d9e4385
leider <derleider@web.de>
2014-03-08 19:21:44 +0100

    changed the API of status message and transformed the prototype based object to one without own prototype

--- test/commons/statusmessage_test.js
+++ test/commons/statusmessage_test.js
@@ -8,19 +8,19 @@ describe('Statusmessage', function () {
   
   it('has type "danger" when created as error', function () {
     var session = {};
-    statusmessage.errorMessage({session: session}, '', '');
+    statusmessage.errorMessage('', '').putIntoSession({session: session});
     expect(session.statusmessage.type).to.equal('alert-danger');
   });
   
   it('has type "success" when created as success', function () {
     var session = {};
-    statusmessage.successMessage({session: session}, '', '');
+    statusmessage.successMessage('', '').putIntoSession({session: session});
     expect(session.statusmessage.type).to.equal('alert-success');
   });
   
   it('is in res when recreated from object', function () {
     var locals = {};
-    statusmessage.fromObject({type: 'alert-success'}, {session: {}}, {locals: locals});
+    statusmessage.fromObject({type: 'alert-success'}).putIntoSession({session: {}}, {locals: locals});
     expect(locals.statusmessage.type).to.equal('alert-success');
   });
   
