31168fe9364ad4ae1a713d19c116721d9c1eb3be
leider <derleider@web.de>
2014-03-16 15:37:30 +0100

    major update to express 4

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 "use strict";
 
 var express = require('express');
+var expressSession = require('express-session');
 var http = require('http');
 var path = require('path');
 var passport = require('passport');
-var MongoStore = require('connect-mongo')(express);
+var MongoStore = require('connect-mongo')(expressSession);
+var cookieParser = require('cookie-parser');
+var favicon = require('static-favicon');
+var morgan = require('morgan');
+var bodyparser = require('body-parser');
+var methodOverride = require('method-override');
+var compress = require('compression');
+var csurf = require('csurf');
+var serveStatic = require('serve-static');
 var i18n = require('i18next');
 var jade = require("jade");
 
@@ -15,7 +24,7 @@ function useApp(parent, url, factory) {
   }
 
   var child = factory(express());
-  child.locals({pretty: true});
+  child.locals.pretty = true;
   parent.get('/' + url, ensureRequestedUrlEndsWithSlash);
   parent.use('/' + url + '/', child);
   return child;
@@ -39,7 +48,6 @@ i18n.init({
   resGetPath: 'locales/__ns__-__lng__.json'
 });
 
-
 var sessionStore = new MongoStore({
   db: 'swk',
   host: conf.get('mongoHost'),
@@ -58,41 +66,38 @@ var winstonStream = {
 module.exports = {
   create: function () {
     var app = express();
-    app.configure(function () {
-      app.set('view engine', 'jade');
-      app.set('views', path.join(__dirname, 'views'));
-      app.use(express.favicon(path.join(__dirname, 'public/img/Softwerkskammer16x16.ico')));
-      app.use(express.logger({stream: winstonStream}));
-      app.use(express.cookieParser());
-      app.use(express.urlencoded());
-      app.use(i18n.handle);
-      app.use(express.methodOverride());
-      app.use(express.compress());
-      app.use(express.static(path.join(__dirname, 'public')));
+    app.set('view engine', 'jade');
+    app.set('views', path.join(__dirname, 'views'));
+    app.use(favicon(path.join(__dirname, 'public/img/Softwerkskammer16x16.ico')));
+    app.use(morgan({stream: winstonStream}));
+    app.use(cookieParser());
+    app.use(bodyparser.urlencoded());
+    app.use(methodOverride());
+    app.use(compress());
+    app.use(serveStatic(path.join(__dirname, 'public')));
 
-      var sevenDays = 86400 * 1000 * 7;
-      if (conf.get('dontUsePersistentSessions')) {
-        // TODO: Umbau als CoolBean mit SessionStore als InMemoryStore von Express statt if Konstrukt (leider)
-        app.use(express.session({secret: conf.get('secret'), cookie: {maxAge: sevenDays}, store: null}));
-      } else {
-        app.use(express.session({secret: conf.get('secret'), cookie: {maxAge: sevenDays}, store: sessionStore}));
-      }
-      app.use(passport.initialize());
-      app.use(passport.session());
-      app.use(beans.get('serverpathRemover'));
-      app.use(beans.get('accessrights'));
-      app.use(beans.get('secureByLogin'));
-      app.use(beans.get('secureSuperuserOnly'));
-      app.use(beans.get('expressViewHelper'));
-      app.use(beans.get('redirectRuleForNewUser'));
-      app.use(beans.get('announcementsInSidebar'));
-      app.use(beans.get('wikiSubdirs'));
-      app.use(beans.get('detectBrowser'));
-      app.use(beans.get('secureAgainstClickjacking'));
-      app.use(express.csrf());
-      app.use(beans.get('addCsrfTokenToLocals'));
-      app.use(app.router);
-    });
+    var sevenDays = 86400 * 1000 * 7;
+    if (conf.get('dontUsePersistentSessions')) {
+      // TODO: Umbau als CoolBean mit SessionStore als InMemoryStore von Express statt if Konstrukt (leider)
+      app.use(expressSession({secret: conf.get('secret'), cookie: {maxAge: sevenDays}}));
+    } else {
+      app.use(expressSession({secret: conf.get('secret'), cookie: {maxAge: sevenDays}, store: sessionStore}));
+    }
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(i18n.handle);
+    app.use(beans.get('serverpathRemover'));
+    app.use(beans.get('accessrights'));
+    app.use(beans.get('secureByLogin'));
+    app.use(beans.get('secureSuperuserOnly'));
+    app.use(beans.get('expressViewHelper'));
+    app.use(beans.get('redirectRuleForNewUser'));
+    app.use(beans.get('announcementsInSidebar'));
+    app.use(beans.get('wikiSubdirs'));
+    app.use(beans.get('detectBrowser'));
+    app.use(beans.get('secureAgainstClickjacking'));
+    app.use(csurf());
+    app.use(beans.get('addCsrfTokenToLocals'));
 
     app.use('/', beans.get('siteApp'));
     useApp(app, 'administration', beans.get('administrationApp'));
