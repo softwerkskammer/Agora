6de4e16e10abcc232e5a83434c47f1815812e613
leider <derleider@web.de>
2014-03-28 17:52:15 +0100

    3rd party content now loaded via https (google maps, google fonts, gravatar) to conform with new https site

--- test/activities/activities_test.js
+++ test/activities/activities_test.js
@@ -99,6 +99,7 @@ describe('Activity application', function () {
       .expect(/description1/)
       .expect(/location1/)
       .expect(/direction1/)
+      .expect(/script src="https:\/\/maps.googleapis.com\/maps\/api\/js\?sensor=false"/)
       .expect(/Bislang gibt es keine Teinahmezusagen./, function (err, res) {
         expect(res.text).to.not.contain('Angelegt von');
         done(err);
