b12974232c299f35311b8ffe39384674f15f5f2f
Nicole Rauch <nicole.m@gmx.de>
2014-03-29 20:55:19 +0100

    Tests for addonAPI.

--- test/activities/addonAPI_test.js
+++ test/activities/addonAPI_test.js
@@ -8,18 +8,34 @@ var expect = require('chai').expect;
 var beans = require('../../testutil/configureForTest').get('beans');
 
 var addonAPI = beans.get('addonAPI');
+var stripeAPI = beans.get('stripeAPI');
 var activitystore = beans.get('activitystore');
 
 var Activity = beans.get('activity');
 
-describe('addon', function () {
+describe('Addon API', function () {
+
+  var savedActivity;
+
+  beforeEach(function () {
+    sinon.stub(activitystore, 'getActivity', function (id, callback) { callback(null, new Activity({})); });
+
+    savedActivity = null;
+    sinon.stub(activitystore, 'saveActivity', function (activity, callback) {
+      savedActivity = activity;
+      callback(null);
+    });
+
+    sinon.stub(stripeAPI, 'transaction', function () {
+      return { charges: { create: function (charge, callback) { callback(null, { amount: 123456 }); } } };
+    });
+  });
 
   afterEach(function () {
     sinon.restore();
   });
 
-  it('is never undefined', function (done) {
-    sinon.stub(activitystore, 'getActivity', function (id, callback) { callback(null, new Activity({})); });
+  it('addon and addonConfig are never undefined', function (done) {
     addonAPI.addonForMember(null, 'unknown member id', function (err, addon, addonConfig) {
       expect(addon).to.exist;
       expect(addonConfig).to.exist;
@@ -27,5 +43,32 @@ describe('addon', function () {
     });
   });
 
+  it('saveAddon enhances activity with UI input and saves it', function (done) {
+    var uiInputObject = { homeAddress: 'At home', billingAddress: 'At work', tShirtSize: 'XL', roommate: 'My best friend'};
+    addonAPI.saveAddon('activity', 'member', uiInputObject, function (err) {
+      expect(savedActivity.addonForMember('member').homeAddress()).to.equal('At home');
+      expect(savedActivity.addonForMember('member').billingAddress()).to.equal('At work');
+      expect(savedActivity.addonForMember('member').tShirtSize()).to.equal('XL');
+      expect(savedActivity.addonForMember('member').roommate()).to.equal('My best friend');
+      done(err);
+    });
+  });
+
+  it('payWithTransfer enhances activity with money transfer info and saves it', function (done) {
+    addonAPI.payWithTransfer('activity', 'member', function (err) {
+      expect(savedActivity.addonForMember('member').moneyTransferred()).to.be.truthy;
+      expect(savedActivity.addonForMember('member').creditCardPaid()).to.be.falsy;
+      done(err);
+    });
+  });
+
+  it('payWithCreditCard enhances activity with money transfer info and saves it', function (done) {
+    addonAPI.payWithCreditCard('activity', 'member', 'stripe-id', function (err, charge) {
+      expect(savedActivity.addonForMember('member').moneyTransferred()).to.be.falsy;
+      expect(savedActivity.addonForMember('member').creditCardPaid()).to.be.truthy;
+      expect(charge.amount).to.equal(123456);
+      done(err);
+    });
+  });
 });
 
