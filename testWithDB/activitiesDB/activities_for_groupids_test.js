e0a774792820b1e87131f6103e52a1cff97f428d
Nicole Rauch <nicole.m@gmx.de>
2014-04-27 00:50:02 +0200

    We only return those activities that end in the future.

--- testWithDB/activitiesDB/activities_for_groupids_test.js
+++ testWithDB/activitiesDB/activities_for_groupids_test.js
@@ -21,6 +21,7 @@ describe('Activity application with DB - shows activities for Group-Ids -', func
   var tomorrow_late = moment().add(1, 'days').add(1, 'hours');
   var day_after_tomorrow = moment().add(2, 'days');
   var yesterday = moment().subtract(1, 'days');
+  var day_before_yesterday = moment().subtract(2, 'days');
 
   var futureActivity1 = new Activity({id: "futureActivity1", title: 'Future Activity 1', description: 'description1', assignedGroup: 'groupname1',
     location: 'location1', direction: 'direction1', startUnix: tomorrow_early.unix(), endUnix: day_after_tomorrow.unix(),
@@ -31,22 +32,29 @@ describe('Activity application with DB - shows activities for Group-Ids -', func
 
   var currentActivity1 = new Activity({id: "currentActivity1", title: 'Current Activity 1', description: 'description1', assignedGroup: 'groupname1',
     location: 'location1', direction: 'direction1', startUnix: yesterday.unix(), endUnix: tomorrow_early.unix(),
-    url: 'url_current', owner: 'owner', resources: {Veranstaltung: {_registeredMembers: [], _registrationOpen: true }}, version: 1});
+    url: 'url_current', owner: 'owner', resources: {Veranstaltung: {_registeredMembers: ['memberId'], _registrationOpen: true }}, version: 1});
   var currentActivity2 = new Activity({id: "currentActivity2", title: 'Current Activity 2', description: 'description1', assignedGroup: 'groupname2',
     location: 'location1', direction: 'direction1', startUnix: yesterday.unix(), endUnix: tomorrow_early.unix(),
     url: 'url_current', owner: 'owner', resources: {Veranstaltung: {_registeredMembers: [], _registrationOpen: true }}, version: 1});
 
+  var pastActivity = new Activity({id: "pastActivity", title: 'Past Activity', description: 'description1', assignedGroup: 'groupname',
+    location: 'location1', direction: 'direction1', startUnix: day_before_yesterday.unix(), endUnix: yesterday.unix(),
+    url: 'url_past', owner: 'owner', resources: {Veranstaltung: {_registeredMembers: ['memberId'], _registrationOpen: true }}, version: 1});
+
   beforeEach(function (done) { // if this fails, you need to start your mongo DB
 
     persistence.drop(function () {
-      activitystore.saveActivity(futureActivity1, function (err) {
+      activitystore.saveActivity(pastActivity, function (err) {
         if (err) { done(err); }
-        activitystore.saveActivity(futureActivity2, function (err) {
+        activitystore.saveActivity(futureActivity1, function (err) {
           if (err) { done(err); }
-          activitystore.saveActivity(currentActivity1, function (err) {
+          activitystore.saveActivity(futureActivity2, function (err) {
             if (err) { done(err); }
-            activitystore.saveActivity(currentActivity2, function (err) {
-              done(err);
+            activitystore.saveActivity(currentActivity1, function (err) {
+              if (err) { done(err); }
+              activitystore.saveActivity(currentActivity2, function (err) {
+                done(err);
+              });
             });
           });
         });
