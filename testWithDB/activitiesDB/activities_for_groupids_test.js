2e324723cf5c72be65af02efbc76064a6a1ad997
Nicole Rauch <nicole.m@gmx.de>
2014-04-27 21:04:02 +0200

    Activity should only be selected once even if member is subscribed to multiple resources.

--- testWithDB/activitiesDB/activities_for_groupids_test.js
+++ testWithDB/activitiesDB/activities_for_groupids_test.js
@@ -26,7 +26,8 @@ describe('Activity application with DB - shows activities for Group-Ids -', func
 
   var futureActivity1 = new Activity({id: "futureActivity1", title: 'Future Activity 1', description: 'description1', assignedGroup: 'groupname1',
     location: 'location1', direction: 'direction1', startUnix: tomorrow_early.unix(), endUnix: day_after_tomorrow.unix(),
-    url: 'url_future', owner: 'owner', resources: {Veranstaltung: {_registeredMembers: [{memberId: 'memberId2'}], _registrationOpen: true }}, version: 1});
+    url: 'url_future', owner: 'owner', resources: {Veranstaltung: {_registeredMembers: [{memberId: 'memberId2'}], _registrationOpen: true },
+      AndereVeranstaltung: {_registeredMembers: [{memberId: 'memberId2'}], _registrationOpen: true }}, version: 1});
   var futureActivity2 = new Activity({id: "futureActivity2", title: 'Future Activity 2', description: 'description1', assignedGroup: 'groupname2',
     location: 'location1', direction: 'direction1', startUnix: tomorrow_late.unix(), endUnix: day_after_tomorrow.unix(),
     url: 'url_future', owner: 'owner', resources: {Veranstaltung: {_registeredMembers: [{memberId: 'memberId'}], _registrationOpen: true }}, version: 1});
@@ -91,6 +92,15 @@ describe('Activity application with DB - shows activities for Group-Ids -', func
       expect(activities[0].title()).to.equal('Current Activity 1');
       expect(activities[1].title()).to.equal('Future Activity 1');
       expect(activities[2].title()).to.equal('Future Activity 2');
+      done(err);
+    });
+  });
+
+  it('shows activity only once even if member is subscribed to multiple resources', function (done) {
+
+    activitystore.activitiesForGroupIdsAndRegisteredMemberId([], 'memberId2', true, function (err, activities) {
+      expect(activities.length).to.equal(1);
+      expect(activities[0].title()).to.equal('Future Activity 1');
       done(err);
     });
   });
