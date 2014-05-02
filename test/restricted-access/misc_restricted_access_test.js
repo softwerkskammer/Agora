4dbfe23a7095b1ccf0b7e37cb7156c5d2cf6e6d2
leider <derleider@web.de>
2014-05-02 14:51:17 +0200

    dashboard only available for registered members

---
+++ test/restricted-access/misc_restricted_access_test.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var request = require('supertest');
+require('../../testutil/configureForTest');
+
+var app = require('../../app').create();
+
+describe('Some pages security for normal visitors does not allow to access', function () {
+
+  it('/dashboard', function (done) {
+    request(app).get('/dashboard/').expect(302).expect('location', /login/, done);
+  });
+
+});
