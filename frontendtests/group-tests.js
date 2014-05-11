5e571dcc7d3155062ed4d4f512e61c284d17654d
leider <derleider@web.de>
2014-05-11 16:45:44 +0200

    moving tests from quint to jasmine (part 2)

--- frontendtests/group-tests.js
+++ frontendtests/group-tests.js
@@ -1,102 +1,107 @@
-/*global groups_validator, groupnameAlreadyTaken, prefixAlreadyTaken */
+/*global groups_validator, groupnameAlreadyTaken, prefixAlreadyTaken, contentsOfName*/
 (function () {
   "use strict";
 
-  // mocking the ajax request
-  $.mockjax({
-    url: "/groups/checkgroupname",
-    response: function (formdata) {
-      var groupname = formdata.data.id.trim(),
-        groupnames = ["groupa", "groupb"];
-      this.responseText = "true";
-      if ($.inArray(groupname, groupnames) !== -1) {
-        this.responseText = "false";
-      }
-    },
-    responseTime: 50,
-    logging: false
-  });
+  describe("Announcements Form", function () {
+    var id = $("#groupform [name=id]");
+    var emailPrefix = $("#groupform [name=emailPrefix]");
 
-  $.mockjax({
-    url: "/groups/checkemailprefix",
-    response: function (formdata) {
-      var prefix = formdata.data.emailPrefix.trim(),
-        groupnames = ["groupa", "groupb"];
-      this.responseText = "true";
-      if ($.inArray(prefix, groupnames) !== -1) {
-        this.responseText = "false";
-      }
-    },
-    responseTime: 50,
-    logging: false
-  });
+    var checkFieldMandatory = function (fieldname, value) {
+      var field = $(fieldname);
+      field.val("");
+      expect(groups_validator.element(field)).toBe(false);
+      expect(groups_validator.errorList[0].message).toBe('Dieses Feld ist ein Pflichtfeld.');
+      field.val(value || "a");
+      expect(groups_validator.element(field)).toBe(true);
+    };
 
-  test("A groupname 'NochNichtVorhanden' is valid", 2, function () {
-    var groupname = $("#groupform [name=id]");
-    stop();
-    groupname.val("NochNichtVorhanden");
-    // trigger validation
-    groupname.trigger("change");
-    $(document).ajaxStop(function () {
-      $(document).unbind("ajaxStop");
-      equal(groups_validator.element(groupname), true);
-      deepEqual(groups_validator.errorList, []);
-      start();
+    beforeEach(function (done) {
+      $(function () {
+        id.val("");
+        id.trigger("change");
+        emailPrefix.val("");
+        emailPrefix.trigger("change");
+        jasmine.Ajax.install();
+        done();
+      });
     });
-  });
 
-  test("A groupname 'groupa' is invalid", 3, function () {
-    var groupname = $("#groupform [name=id]");
-    groups_validator.element(groupname);
-    stop();
-    groupname.val("groupa");
-    equal(groups_validator.element(groupname), true);
-    $(document).ajaxStop(function () {
-      $(document).unbind("ajaxStop");
-      equal(groups_validator.element(groupname), false);
-      equal(groups_validator.errorList[0].message, groupnameAlreadyTaken);
-      start();
+    afterEach(function () {
+      jasmine.Ajax.uninstall();
     });
-  });
 
-  test("A prefix less than 5 letters is invalid", 1, function () {
-    var prefix = $("#groupform [name=emailPrefix]");
-    prefix.val("Much");
-    equal(groups_validator.element(prefix), false);
-  });
+    it("checks that a groupname check response is handled for 'true'", function () {
+      jasmine.Ajax.stubRequest("/groups/checkgroupname?id=group1").andReturn({responseText: "true"});
+      id.val("group1");
+      // trigger validation
+      id.trigger("change");
 
-  test("A prefix longer than 15 letters is invalid", 1, function () {
-    var prefix = $("#groupform [name=emailPrefix]");
-    prefix.val("MuchTooMuchText1");
-    equal(groups_validator.element(prefix), false);
-  });
-
-  test("A prefix 'Valid' is valid", 2, function () {
-    var prefix = $("#groupform [name=emailPrefix]");
-    stop();
-    prefix.val("Valid");
-    // trigger validation
-    prefix.trigger("change");
-    $(document).ajaxStop(function () {
-      $(document).unbind("ajaxStop");
-      equal(groups_validator.element(prefix), true);
-      deepEqual(groups_validator.errorList, []);
-      start();
+      expect(groups_validator.element(id)).toBe(true);
+      expect(groups_validator.errorList).toEqual([]);
     });
-  });
 
-  test("A prefix 'groupa' is invalid", 3, function () {
-    var prefix = $("#groupform [name=emailPrefix]");
-    groups_validator.element(prefix);
-    stop();
-    prefix.val("groupa");
-    prefix.trigger("change");
-    equal(groups_validator.element(prefix), true);
-    $(document).ajaxStop(function () {
-      $(document).unbind("ajaxStop");
-      equal(groups_validator.element(prefix), false);
-      equal(groups_validator.errorList[0].message, prefixAlreadyTaken);
-      start();
+    it("checks that a groupname check response is handled for 'false'", function () {
+      jasmine.Ajax.stubRequest("/groups/checkgroupname?id=group2").andReturn({responseText: "false"});
+      id.val("group2");
+      // trigger validation
+      id.trigger("change");
+
+      expect(groups_validator.element(id)).toBe(false);
+      expect(groups_validator.errorList).toContain(jasmine.objectContaining({message: groupnameAlreadyTaken}));
     });
+
+    it("checks that a prefix check response is handled for 'true'", function () {
+      jasmine.Ajax.stubRequest("/groups/checkemailprefix?emailPrefix=prefix1").andReturn({responseText: "true"});
+      emailPrefix.val("prefix1");
+      // trigger validation
+      emailPrefix.trigger("change");
+
+      expect(groups_validator.element(emailPrefix)).toBe(true);
+      expect(groups_validator.errorList).toEqual([]);
+    });
+
+    it("checks that a prefix check response is handled for 'false'", function () {
+      jasmine.Ajax.stubRequest("/groups/checkemailprefix?emailPrefix=prefix2").andReturn({responseText: "false"});
+      emailPrefix.val("prefix2");
+      // trigger validation
+      emailPrefix.trigger("change");
+
+      expect(groups_validator.element(emailPrefix)).toBe(false);
+      expect(groups_validator.errorList).toContain(jasmine.objectContaining({message: prefixAlreadyTaken}));
+    });
+
+    it("checks that 'id' is mandatory", function () {
+      checkFieldMandatory("#groupform [name=id]", "aa");
+    });
+
+    it("checks that 'emailPrefix' is mandatory", function () {
+      checkFieldMandatory("#groupform [name=emailPrefix]", "12345");
+    });
+
+    it("checks that a 'emailPrefix' shorter than 5 letters is invalid", function () {
+      var prefix = $("#groupform [name=emailPrefix]");
+      prefix.val("Much");
+      expect(groups_validator.element(emailPrefix)).toBe(false);
+    });
+
+    it("checks that a 'emailPrefix' longer than 15 letters is invalid", function () {
+      var prefix = $("#groupform [name=emailPrefix]");
+      prefix.val("MuchTooMuchText1");
+      expect(groups_validator.element(emailPrefix)).toBe(false);
+    });
+
+    it("checks that 'longName' is mandatory", function () {
+      checkFieldMandatory("#groupform [name=longName]");
+    });
+
+    it("checks that 'description' is mandatory", function () {
+      checkFieldMandatory("#groupform [name=description]");
+    });
+
+    it("checks that 'type' is mandatory", function () {
+      checkFieldMandatory("#groupform [name=type]", "Themengruppe");
+    });
+
   });
 }());
+
