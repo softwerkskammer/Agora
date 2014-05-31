b34e885a4366122e4fb749ea7186fa67da5502da
Raimo Radczewski <raimoradczewski@googlemail.com>
2014-05-31 11:52:04 +0200

    Implemented stub for storeImage

--- test/images/imagerepository_test.js
+++ test/images/imagerepository_test.js
@@ -5,11 +5,22 @@ var expect = require('must');
 var api = require('../../lib/images/imagerepositoryAPI');
 var conf = require('nconf');
 
-describe("the image repository", function () {
-  it('should retrieve the document folder from nconf', function () {
-    var directoryForUploads = '/tmp';
+var directoryForUploads = '/tmp';
+
+describe("the image repository - ", function () {
+  before(function () {
     conf.set('imageDirectory', directoryForUploads);
+  })
+
+  it('should retrieve the document folder from nconf', function () {
 
     expect(api.directory).to.equal(directoryForUploads);
+
+  });
+
+  it('storeImage should call a callback as result', function (done) {
+    api.storeImage(null, function (err) {
+      done();
+    })
   });
 });
