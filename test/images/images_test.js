d772dcd9062f5c24c34edb1f4459e27b1043cb39
Andreas Simon <softwerkskammer@andreas-simon.info>
2014-05-31 23:19:51 +0200

    Pass the received image to the imagerepository

--- test/images/images_test.js
+++ test/images/images_test.js
@@ -1,4 +1,5 @@
 'use strict';
+var stream = require('stream');
 var conf = require('../../testutil/configureForTest');
 var request = require('supertest');
 var sinon = require('sinon').sandbox.create();
@@ -12,11 +13,14 @@ var createApp = require('../../testutil/testHelper')('imagesApp').createApp;
 var CREATED = 201;
 
 describe('/images', function () {
+  var imagePath = 'test/images/fixtures/image.jpg';
   var imageId;
+  var storedImagePath;
 
   beforeEach(function () {
     imageId = '8fe5861b-53cb-49db-929f-81eb77b4d05c';
-    sinon.stub(imageRepository, 'storeImage', function (stream, callback) {
+    sinon.stub(imageRepository, 'storeImage', function (imagePath, callback) {
+      storedImagePath = imagePath;
       callback(null, imageId);
     });
   });
@@ -24,13 +28,25 @@ describe('/images', function () {
   afterEach(function () {
     sinon.restore();
   });
-  describe('POST /', function () {
 
+  describe('POST /', function () {
     it('responds with the image Location', function (done) {
       request(createApp())
         .post('/')
+        .attach('image', imagePath)
         .expect(CREATED)
         .expect('Location', '/images/' + imageId, done);
     });
+
+    it('stores the image to the imagerepository', function (done) {
+      request(createApp())
+        .post('/')
+        .attach('image', imagePath)
+        .end(function () {
+          expect(storedImagePath).to.match(/\/tmp\/[0-9a-z\-]+\.jpg/);
+          done();
+        });
+    });
+
   });
 });
