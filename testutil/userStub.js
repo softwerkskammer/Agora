b44e0f9e5f75282a0440d8bc5dceaa1d2f4a2458
Nicole Rauch <nicole.m@gmx.de>
2014-06-14 12:46:49 +0100

    Inline local variable.

--- testutil/userStub.js
+++ testutil/userStub.js
@@ -1,14 +1,11 @@
 'use strict';
 
 module.exports = function (user) {
-
-  var userStub = function (req, res, next) {
+  return function (req, res, next) {
     req.user = user || {}; // for model checks
     res.locals.user = req.user; // for jade checks
     req.isAuthenticated = function () { return true; };
     next();
   };
-
-  return userStub;
 };
 
