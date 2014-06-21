8fa1947478e3063454e117d370d3293ba91031a6
Raimo Radczewski <raimoradczewski@googlemail.com>
2014-06-21 14:29:52 +0200

    Added test stub for activityresultstore

---
+++ test/activityresults/activityresultstore_test.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var expect = require('must');
+var sinon = require('sinon').sandbox.create();
+
+var conf = require('../../testutil/configureForTest');
+var persistence = conf.get('beans').get('activityResultsPersistence');
+var store = conf.get('beans').get('activityresultstore');
+
+describe('ActivityResult store', function () {
+  var activityResult1 = {id: 'Hackergarten1', photos: [{uri: '/path/to/image1.jpg'}]};
+  var activityResult2 = {id: 'Hackergarten2', photos: [{uri: '/path/to/image2.jpg'}]};
+  var sampleList = [activityResult1, activityResult2];
+  var getByField;
+  var getById;
+  var list;
+
+  beforeEach(function () {
+    list = sinon.stub(persistence, 'list', function (sortOrder, callback) {
+      return callback(null, sampleList);
+    });
+    sinon.stub(persistence, 'listByField', function (searchObject, sortOrder, callback) {
+      return callback(null, sampleList);
+    });
+    getByField = sinon.stub(persistence, 'getByField', function (object, callback) {
+      return callback(null, activityResult1);
+    });
+    getById = sinon.stub(persistence, 'getById', function (object, callback) {
+      return callback(null, activityResult1);
+    });
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
+
+
+});
