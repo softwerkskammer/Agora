3bbf33c88090acd6d1bd8194ec91628f6a6e2684
Raimo Radczewski <raimoradczewski@googlemail.com>
2014-06-21 15:54:45 +0200

    Added test for non-existing activityresult

--- test/activityresults/activityresultstore_test.js
+++ test/activityresults/activityresultstore_test.js
@@ -9,22 +9,9 @@ var store = conf.get('beans').get('activityresultstore');
 
 describe('ActivityResult store', function () {
   var activityResult1 = {id: 'Hackergarten1', photos: [{uri: '/path/to/image1.jpg'}]};
-  var activityResult2 = {id: 'Hackergarten2', photos: [{uri: '/path/to/image2.jpg'}]};
-  var sampleList = [activityResult1, activityResult2];
-  var getByField;
   var getById;
-  var list;
 
   beforeEach(function () {
-    list = sinon.stub(persistence, 'list', function (sortOrder, callback) {
-      return callback(null, sampleList);
-    });
-    sinon.stub(persistence, 'listByField', function (searchObject, sortOrder, callback) {
-      return callback(null, sampleList);
-    });
-    getByField = sinon.stub(persistence, 'getByField', function (object, callback) {
-      return callback(null, activityResult1);
-    });
     getById = sinon.stub(persistence, 'getById', function (object, callback) {
       return callback(null, activityResult1);
     });
@@ -38,6 +25,19 @@ describe('ActivityResult store', function () {
     it('should return the activityResult for an id', function (done) {
       store.getActivityResultById(activityResult1.id, function (err, activityResult) {
         activityResult.should.have.property('id', activityResult1.id);
+        done();
+      });
+    });
+
+    it('should return an error if activity does not exist', function (done) {
+      getById.restore();
+      sinon.stub(persistence, 'getById', function (object, callback) {
+        return callback(new Error('not found'), null);
+      });
+
+      store.getActivityResultById('non-existing-id', function (err, activityResult) {
+        expect(err).to.exist();
+        expect(activityResult).to.be.null();
         done();
       });
     });
