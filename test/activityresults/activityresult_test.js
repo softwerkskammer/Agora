8a245d5df083940f818de592a54f1d433aa0937f
Raimo Radczewski <raimoradczewski@googlemail.com>
2014-06-21 14:23:02 +0200

    Added subdocument for photos with href prop

--- test/activityresults/activityresult_test.js
+++ test/activityresults/activityresult_test.js
@@ -3,6 +3,7 @@
 require('../../testutil/configureForTest');
 var conf = require('nconf');
 var expect = require('must');
+var should = require('should');
 
 var ActivityResult = conf.get('beans').get('activityresult');
 
@@ -10,5 +11,19 @@ describe('An activity result', function () {
   it('should have an id', function () {
     var activityResult = new ActivityResult({id: "hackergarten2_2"});
     expect(activityResult.id).to.be('hackergarten2_2');
+  });
+
+  it('should have a list of photo urls', function () {
+    var activityResult = new ActivityResult({
+      id: 'hackergarten2_2',
+      photos: [
+        {uri: '/path/to/image1.jpg'},
+        {uri: '/path/to/image2.jpg'}
+      ]
+    });
+
+    activityResult.photos.should.containEql({uri: '/path/to/image1.jpg'});
+    activityResult.photos.should.containEql({uri: '/path/to/image2.jpg'});
+
   });
 });
