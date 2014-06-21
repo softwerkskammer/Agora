2cd706bb0c05587850099f17b402476ac6935884
Raimo Radczewski <raimoradczewski@googlemail.com>
2014-06-21 11:32:07 +0200

    Added test for index route of activityresults

---
+++ test/activityresults/activityresults_test.js
@@ -0,0 +1,28 @@
+'use strict';
+var stream = require('stream');
+var conf = require('../../testutil/configureForTest');
+var request = require('supertest');
+var sinon = require('sinon').sandbox.create();
+var expect = require('must');
+
+var beans = conf.get('beans');
+
+var createApp = require('../../testutil/testHelper')('activityresultsApp').createApp;
+
+var OK = 200;
+var CREATED = 201;
+
+describe('/activityresults', function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+
+  describe('GET /', function () {
+    it('should return an OK', function (done) {
+      request(createApp())
+        .get('/')
+        .expect(OK, done);
+    });
+  });
+
+});
