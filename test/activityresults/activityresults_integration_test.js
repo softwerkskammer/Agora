b60a211f4848169881c158bc847e38134bc41b64
Raimo Radczewski <raimoradczewski@googlemail.com>
2014-08-02 19:37:14 +0200

    Wrote failing test for the get page containing a used tag

--- test/activityresults/activityresults_integration_test.js
+++ test/activityresults/activityresults_integration_test.js
@@ -44,12 +44,33 @@ describe('/activityresults', function () {
 
     it('should render the results if the activity result is known', function (done) {
       sinon.stub(activityresultsService, 'getActivityResultByName', function (activityResultName, callback) {
-        callback(null, new ActivityResult());
+        callback(null, new ActivityResult({
+          id: activityResultName,
+          photos: [
+            { uri: '/gallery/fef400b5-2a1f-4f6d-86b7-d5b5716711ba.JPG', timestamp: new Date(), tags: ["hopper"]},
+            { uri: '/gallery/73120aa1-80b1-4f2f-a342-4b03abc665e5.JPG', timestamp: new Date(), tags: ["hopper"]},
+            { uri: '/gallery/a24a1d14-8936-403f-91d7-c6ba36343cc7.JPG', timestamp: new Date(), tags: ["hopper"]},
+            { uri: '/gallery/cba13ad5-43fc-485d-a73b-7adb0138debe.JPG', timestamp: new Date(), tags: ["lovelace"]},
+            { uri: '/gallery/5ca87f1d-0de0-4371-9de0-1c755fc1b9c1.JPG', timestamp: new Date(), tags: ["lovelace"]},
+            { uri: '/gallery/250b1910-663c-4bb1-a0d6-4d5ed89d040d.JPG', timestamp: new Date(), tags: ["lovelace"]},
+            { uri: '/gallery/e63b12ee-48ef-4126-af2c-9f87dd68bf4b.JPG', timestamp: new Date(), tags: ["lovelace"]},
+            { uri: '/gallery/9aa23ef0-6565-457f-8d91-9922f22ba292.JPG', timestamp: new Date(), tags: ["lovelace"]},
+            { uri: '/gallery/e766dcbc-e2da-4e72-928f-d67b9717f261.JPG', timestamp: new Date(), tags: ["lovelace"]},
+            { uri: '/gallery/ad1f8bea-1785-4912-9b88-96f3cbe4b978.JPG', timestamp: new Date(), tags: ["liskov"]},
+            { uri: '/gallery/9475905d-b979-4296-af47-ac0164788a1a.JPG', timestamp: new Date(), tags: ["liskov"]},
+            { uri: '/gallery/fdd09e6d-baf1-4c44-a767-a786d22002bd.JPG', timestamp: new Date(), tags: ["liskov"]},
+            { uri: '/gallery/85f9a5c1-46a5-470d-b3d2-fda330537457.JPG', timestamp: new Date(), tags: ["elsewhere"]},
+            { uri: '/gallery/1806f88e-8ed7-4b3e-92bd-3173c9965541.JPG', timestamp: new Date(), tags: ["elsewhere"]},
+            { uri: '/gallery/fc7ea3d4-9e4d-46cf-885c-2f9a98bfa058.JPG', timestamp: new Date(), tags: ["elsewhere"]},
+            { uri: '/gallery/627adb49-b7ef-4765-94b9-d094463007a6.JPG', timestamp: new Date(), tags: ["elsewhere"]},
+            { uri: '/gallery/9afcfea0-1aa4-41c1-9f8c-6dba1e16d6c4.JPG', timestamp: new Date(), tags: ["elsewhere"]}
+          ]
+        }));
       });
 
       request(createApp())
         .get('/known-activity-results')
-        .expect(OK, done);
+        .expect(OK, /elsewhere/, done);
     });
   });
 
