d12dbbc9042bc635681749091b2921ac20c5b69e
leider <derleider@web.de>
2014-09-04 21:35:30 +0200

    fix for #816

--- frontendtests/surroundInterestsWithLinks-tests.js
+++ frontendtests/surroundInterestsWithLinks-tests.js
@@ -3,8 +3,8 @@
   'use strict';
 
   describe('surround interests with link', function () {
-    var expected = '<a href="/members/interests/a">a</a>,<a href="/members/interests/b"> b</a>,' +
-      '<a href="/members/interests/a%20b"> a b</a>,<a href="/members/interests/a%3Bb"> a;b </a>,<a href="/members/interests/cb">cb</a>';
+    var expected = '<a href="/members/interests?interest=a">a</a>,<a href="/members/interests?interest=b"> b</a>,' +
+      '<a href="/members/interests?interest=a%20b"> a b</a>,<a href="/members/interests?interest=a%3Bb"> a;b </a>,<a href="/members/interests?interest=cb">cb</a>';
 
     it('creates links for each interest in an interests string (comma separated)', function () {
       var result = surroundInterestsWithLinks('a, b, a b, a;b ,cb');
