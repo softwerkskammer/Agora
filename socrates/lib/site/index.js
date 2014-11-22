eff94197ac7a2b5e87fb32980bc124762d698957
Nicole Rauch <nicole.m@gmx.de>
2014-11-22 20:53:44 +0100

    Adding the SWK member to the socrates session user.

--- socrates/lib/site/index.js
+++ socrates/lib/site/index.js
@@ -13,6 +13,8 @@ var misc = beans.get('misc');
 var sponsors = require('./sponsors.json');
 var jwt_secret = conf.get('jwt_secret');
 
+var memberstore = beans.get('memberstore');
+
 var app = misc.expressAppIn(__dirname);
 
 app.get('/', function (req, res, next) {
@@ -20,14 +22,34 @@ app.get('/', function (req, res, next) {
 });
 
 app.get('/loggedIn', function (req, res, next) {
-  var token = jwt.decode(req.query.id_token, jwt_secret);
-  if (!token.userId) {
-    return next(new Error("Authentication failed."));
+
+  function getTokenFrom(req) {
+    return jwt.decode(req.query.id_token, jwt_secret);
   }
-  req._passport.session.user = {authenticationId: token.userId};
-  passport.authenticate('session')(req, res, function () {
-    console.log("Hallo " + req.user.member.displayName());
-    res.redirect('/');
+
+  function createUserObject(token, callback) {
+    if (!token.userId) {
+      return callback(new Error("Authentication failed."));
+    }
+    // load member and participant:
+    // TODO (siehe membersService.findMemberFor ??)
+    memberstore.getMemberForAuthentication(token.userId, function (err, member) {
+      if (err) { return callback(err); }
+      // no member: this person+auth is unknown in SWK
+      if (!member) { return callback(null, {authenticationId: token.userId, profile: token.profile}); }
+      // no participant: this person+auth is known in SWK but not in SoCraTes
+      return callback(null, {authenticationId: token.userId, member: member});
+    });
+  }
+
+  createUserObject(getTokenFrom(req), function (err, userObject) {
+    if (err) { return next(err); }
+
+    req._passport.session.user = userObject;
+    passport.authenticate('session')(req, res, function () {
+      console.log("Hallo " + req.user.member.displayName());
+      res.redirect('/');
+    });
   });
 });
 
