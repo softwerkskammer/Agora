cf3909033fbf24ce4cf069035d9269925a6cc7cd
leider <derleider@web.de>
2014-12-07 23:15:15 +0100

    moved logic from UI to service layer in activity results

--- softwerkskammer/lib/activityresults/index.js
+++ softwerkskammer/lib/activityresults/index.js
@@ -1,20 +1,14 @@
 'use strict';
 
 var Form = require('multiparty').Form;
-var moment = require('moment-timezone');
-var async = require('async');
-var _ = require('lodash');
 
 var nconf = require('nconf');
 var beans = nconf.get('beans');
 var ActivityResult = beans.get('activityresult');
 var activityresultsPersistence = beans.get('activityresultsPersistence');
 var activityresultsService = beans.get('activityresultsService');
-var galleryService = beans.get('galleryService');
 var misc = beans.get('misc');
-var galleryApp = beans.get('galleryApp');
 var fieldHelpers = beans.get('fieldHelpers');
-var logger = require('winston').loggers.get('application');
 
 var app = misc.expressAppIn(__dirname);
 
@@ -38,41 +32,27 @@ app.post('/', function (req, res, next) {
 app.post('/:activityResultName/upload', function (req, res, next) {
   var activityResultName = req.params.activityResultName;
   new Form().parse(req, function (err, fields, files) {
-    if (err || !files) {
+    if (err || !files || files.length < 1) {
       return res.redirect(app.path() + activityResultName); // Es fehlen Prüfungen im Frontend
     }
-    var image = files.image[0];
-
-    async.waterfall([
-      function (callback) { galleryService.storeImage(image.path, callback); },
-      function (imageUri, callback) {
-        galleryService.getMetadataForImage(imageUri, function (err, metadata) { callback(err, metadata, imageUri); });
-      },
-      function (metadata, imageUri, callback) {
-        activityresultsService.addPhotoToActivityResult(activityResultName, metadata, imageUri, req.user.member.id(), function (err) { callback(err, imageUri); });
-      }
-    ], function (err, imageUri) {
+    activityresultsService.addPhotoToActivityResult(activityResultName, files.image[0], req.user.member.id(), function (err, imageUri) {
       if (err) { return next(err); }
       res.redirect(app.path() + activityResultName + '/photo/' + imageUri + '/edit');
     });
+
   });
 });
 
 app.get('/:activityResultName/photo/:photoId/delete', function (req, res, next) {
   var activityResultName = req.params.activityResultName;
-  if (!res.locals.accessrights.canDeletePhoto()) {
-    return res.redirect(app.path() + activityResultName);
-  }
-  var photoId = req.params.photoId;
-  activityresultsService.getActivityResultByName(activityResultName, function (err, activityResult) {
-    activityresultsService.deletePhotoOfActivityResult(activityResult, photoId, function (err) {
+  if (res.locals.accessrights.canDeletePhoto()) {
+    var photoId = req.params.photoId;
+    return activityresultsService.deletePhotoOfActivityResult(activityResultName, photoId, function (err) {
       if (err) { return next(err); }
-      galleryService.deleteImage(photoId, function (err) {
-        if (err) {return next(err); }
-        return res.redirect(app.path() + activityResultName);
-      });
+      res.redirect(app.path() + activityResultName);
     });
-  });
+  }
+  res.redirect(app.path() + activityResultName);
 });
 
 app.get('/:activityResultName/photo/:photoId/edit', function (req, res, next) {
@@ -86,7 +66,7 @@ app.get('/:activityResultName/photo/:photoId/edit', function (req, res, next) {
     if (model.photo && res.locals.accessrights.canEditPhoto(model.photo)) {
       return res.render('edit_photo', model);
     }
-    return res.redirect(app.path() + activityResultName);
+    res.redirect(app.path() + activityResultName);
   });
 });
 
@@ -99,20 +79,11 @@ app.post('/:activityResultName/photo/:photoId/edit', function (req, res, next) {
     timestamp: fieldHelpers.parseToMomentUsingDefaultTimezone(req.body.date, req.body.time).toDate()
   };
 
-  activityresultsService.getActivityResultByName(activityResultName, function (err, activityResult) {
-    if (err || !activityResult) { return next(err); }
-
-    var photo = activityResult.getPhotoById(photoId);
-    if (!photo) { return next(err); }
-
-    if (res.locals.accessrights.canEditPhoto(photo)) {
-      return activityresultsService.updatePhotoOfActivityResult(activityResult, photoId, photoData, function (err) {
-        if (err) { return next(err); }
-        res.redirect(app.path() + activityResultName);
-      });
-    }
-    return res.redirect(app.path() + activityResultName);
+  activityresultsService.updatePhotoOfActivityResult(activityResultName, photoId, photoData, res.locals.accessrights, function (err) {
+    if (err) { return next(err); }
+    res.redirect(app.path() + activityResultName);
   });
+
 });
 
 app.get('/:activityResultName', function (req, res, next) {
--- softwerkskammer/test/activityresults/activityresultsservice_test.js
+++ softwerkskammer/test/activityresults/activityresultsservice_test.js
@@ -4,15 +4,19 @@ var expect = require('must');
 var sinon = require('sinon').sandbox.create();
 
 var conf = require('../../testutil/configureForTest');
-var persistence = conf.get('beans').get('activityresultsPersistence');
-var service = conf.get('beans').get('activityresultsService');
-var ActivityResult = conf.get('beans').get('activityresult');
+var beans = conf.get('beans');
+
+var galleryService = beans.get('galleryService');
+var persistence = beans.get('activityresultsPersistence');
+var service = beans.get('activityresultsService');
+var ActivityResult = beans.get('activityresult');
 
 describe('ActivityResult service', function () {
-  var activityResult = {id: 'Hackergarten2', photos: [{uri: '/path/to/image1.jpg'}]};
+  var activityResult;
   var getById;
 
   beforeEach(function () {
+    activityResult = {id: 'Hackergarten2', photos: [{id: 'image1.jpg'}]};
     getById = sinon.stub(persistence, 'getById', function (object, callback) {
       return callback(null, activityResult);
     });
@@ -50,18 +54,34 @@ describe('ActivityResult service', function () {
     });
   });
 
-  describe('addPhotoToActivityResult', function () {
-    it('should add an image to an activityresult', function (done) {
-      var saveStub = sinon.stub(persistence, 'save', function (object, callback) {
-        callback();
-      });
+  it('addPhotoToActivityResult should add an image to an activityresult', function (done) {
+    var saveStub = sinon.stub(persistence, 'save', function (object, callback) {
+      callback();
+    });
 
-      service.addPhotoToActivityResult('Hackergarten2', undefined, 'my_uri', 'memberId', function (err) {
+    sinon.stub(galleryService, 'storeImage', function (path, callback) { callback(null, path); });
+    sinon.stub(galleryService, 'getMetadataForImage', function (path, callback) { callback(null); });
+
+    service.addPhotoToActivityResult('Hackergarten2', {path: 'my_uri'}, 'memberId', function (err) {
+      expect(saveStub.called).to.be(true);
+      var objectToSave = saveStub.args[0][0];
+      expect(objectToSave.photos).to.have.length(2);
+      done(err);
+    });
+  });
+
+  it('updatePhotoOfActivityResult should change an image in an activityresult', function (done) {
+    var saveStub = sinon.stub(persistence, 'save', function (object, callback) {
+      callback();
+    });
+
+    service.updatePhotoOfActivityResult('Hackergarten2', 'image1.jpg', {title: 'Photo 1'},
+      {canEditPhoto: function () { return true; }}, function (err) {
         expect(saveStub.called).to.be(true);
         var objectToSave = saveStub.args[0][0];
-        expect(objectToSave.photos).to.have.length(2);
+        expect(objectToSave.photos).to.have.length(1);
+        expect(objectToSave.photos[0]).to.have.ownProperty('title', 'Photo 1');
         done(err);
       });
-    });
   });
 });
