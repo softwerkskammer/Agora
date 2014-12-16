0c024e73f2cf8f89d2a6bbbb4be91c46304e1e5b
Nicole Rauch <nicole.m@gmx.de>
2014-12-16 07:22:50 +0000

    Probably it's not the worst idea to close the DB before exit...

--- softwerkskammer/lib/wiki/createChangeNotifications.js
+++ softwerkskammer/lib/wiki/createChangeNotifications.js
@@ -17,6 +17,7 @@ logger.info('== Wiki Changes ===================================================
 persistence.getByField({id: 'lastWikiNotifications'}, function (err, result) {
   if (err) {
     logger.error(err);
+    persistence.closeDB();
     process.exit();
   }
   var yesterday = moment().subtract(1, 'days');
@@ -25,25 +26,30 @@ persistence.getByField({id: 'lastWikiNotifications'}, function (err, result) {
   wikiService.findPagesForDigestSince(moment(lastNotified.moment), function (err, changes) {
     if (err) {
       logger.error(err);
+      persistence.closeDB();
       process.exit();
     }
     if (changes.length === 0) {
       logger.info('no changes to report');
+      persistence.closeDB();
       process.exit();
     }
     notifications.wikiChanges(changes, function (err, stringifiedOptions) {
       if (err) {
         logger.error(err);
+        persistence.closeDB();
         process.exit();
       }
       lastNotified.moment = moment().toDate();
       persistence.save(lastNotified, function (err) {
         if (err) {
           logger.error(err);
+          persistence.closeDB();
           process.exit();
         }
         logger.info("Wiki-Changes notified at: " + lastNotified.moment);
         logger.info("Options were returned: " + !!stringifiedOptions);
+        persistence.closeDB();
         process.exit();
       });
     });
