3dc84fb5a38616f0e8927de1ceac9ce38be023ee
Nicole Rauch <nicole.m@gmx.de>
2015-01-02 16:53:00 +0100

    Added tests related to submitting members.

--- socrates/test/members/members_test.js
+++ socrates/test/members/members_test.js
@@ -2,12 +2,15 @@
 
 var request = require('supertest');
 var sinon = require('sinon').sandbox.create();
+var expect = require('must');
 
 var beans = require('../../testutil/configureForTest').get('beans');
 var userWithoutMember = require('../../testutil/userWithoutMember');
-var accessrights = beans.get('accessrights');
+var membersService = beans.get('membersService');
 var memberstore = beans.get('memberstore');
 var participantstore = beans.get('participantstore');
+var notifications = beans.get('notifications');
+var groupsAndMembersService = beans.get('groupsAndMembersService');
 var Member = beans.get('member');
 var Participant = beans.get('participant');
 
@@ -51,7 +54,6 @@ describe('SoCraTes members application', function () {
     appWithSoftwerkskammerMember = request(createApp({member: softwerkskammerMember}));
     appWithSocratesMember = request(createApp({member: socratesMember}));
   });
-
 
   afterEach(function () {
     sinon.restore();
@@ -125,7 +127,163 @@ describe('SoCraTes members application', function () {
 
   });
 
-  describe('submitting a member page', function () {
+  describe('submitting a member form', function () {
+
+    it('rejects a member with invalid and different nickname on submit', function (done) {
+      sinon.stub(membersService, 'isValidNickname', function (nickname, callback) {
+        callback(null, false);
+      });
+
+      appWithSoftwerkskammerMember
+        .post('/submit')
+        .send('id=0815&firstname=A&lastname=B&email=c@d.de&previousEmail=c@d.de')
+        .send('nickname=nickerinack')
+        .send('previousNickname=bibabu')
+        .expect(200)
+        .expect(/Failed/)
+        .expect(/Validation/)
+        .expect(/This nickname is not available\./, done);
+    });
+
+    it('rejects a member with invalid and different email address on submit', function (done) {
+      sinon.stub(membersService, 'isValidEmail', function (nickname, callback) {
+        callback(null, false);
+      });
+
+      appWithSoftwerkskammerMember
+        .post('/submit')
+        .send('id=0815&firstname=Hans&lastname=Dampf&nickname=hada&previousNickname=hada')
+        .send('email=here@there.org')
+        .send('previousEmail=there@wherever.com')
+        .expect(200)
+        .expect(/Failed/)
+        .expect(/Validation/)
+        .expect(/This e-mail address is already registered\./, done);
+    });
+
+    it('rejects a member with missing first and last name on submit', function (done) {
+      appWithSoftwerkskammerMember
+        .post('/submit')
+        .send('id=0815&&nickname=hada&previousNickname=hada&email=here@there.org&previousEmail=here@there.org')
+        .expect(200)
+        .expect(/Failed/)
+        .expect(/Validation/)
+        .expect(/First name is required\./)
+        .expect(/Last name is required\./, done);
+    });
+
+    it('rejects a member with missing first name who validly changed their nickname and mailaddress on submit', function (done) {
+      // attention: This combination is required to prove the invocations of the callbacks in case of no error!
+      sinon.stub(membersService, 'isValidNickname', function (nickname, callback) {
+        callback(null, true);
+      });
+      sinon.stub(membersService, 'isValidEmail', function (nickname, callback) {
+        callback(null, true);
+      });
+
+      appWithSoftwerkskammerMember
+        .post('/submit')
+        .send('id=0815&&nickname=hadaNew&previousNickname=hada&lastname=x&email=hereNew@there.org&previousEmail=here@there.org')
+        .expect(200)
+        .expect(/Failed/)
+        .expect(/Validation/)
+        .expect(/First name is required\./, done);
+    });
+
+    it('rejects a member with invalid nickname and email address on submit, giving two error messages', function (done) {
+      sinon.stub(membersService, 'isValidNickname', function (nickname, callback) {
+        callback(null, false);
+      });
+      sinon.stub(membersService, 'isValidEmail', function (nickname, callback) {
+        callback(null, false);
+      });
+
+      appWithSoftwerkskammerMember
+        .post('/submit')
+        .send('id=0815&firstname=A&lastname=B')
+        .send('nickname=nickerinack')
+        .send('previousNickname=hada')
+        .send('email=here@there.org')
+        .send('previousEmail=there@wherever.com')
+        .expect(200)
+        .expect(/Failed/)
+        .expect(/Validation/)
+        .expect(/This nickname is not available\./)
+        .expect(/This e-mail address is already registered\./, done);
+    });
+
+    it('saves an existing Softwerkskammer member, creates a participant because it is not yet there, and does not trigger notification sending', function (done) {
+      sinon.stub(membersService, 'isValidNickname', function (nickname, callback) { callback(null, true); });
+      sinon.stub(membersService, 'isValidEmail', function (nickname, callback) { callback(null, true); });
+      sinon.stub(memberstore, 'saveMember', function (member, callback) { callback(null); });
+      var participantSave = sinon.stub(participantstore, 'saveParticipant', function (participant, callback) { callback(null); });
+      var notificationCall = sinon.stub(notifications, 'newMemberRegistered', function () { return undefined; });
+
+      // the following stub indicates that the member already exists
+      sinon.stub(groupsAndMembersService, 'getUserWithHisGroups', function (nickname, callback) { callback(null, softwerkskammerMember); });
+      // and that the participant is not yet there
+      sinon.stub(participantstore, 'getParticipant', function (id, callback) { callback(null); });
+      appWithSoftwerkskammerMember
+        .post('/submit')
+        .send('id=0815&firstname=A&lastname=B')
+        .send('nickname=nickerinack')
+        .send('email=here@there.org')
+        .expect(302)
+        .expect('location', '/', function (err) {
+          expect(participantSave.called).to.be(true);
+          expect(notificationCall.called).to.be(false);
+          done(err);
+        });
+    });
+
+    it('saves an existing SoCraTes member, creates no participant because it is already there, and does not trigger notification sending', function (done) {
+      sinon.stub(membersService, 'isValidNickname', function (nickname, callback) { callback(null, true); });
+      sinon.stub(membersService, 'isValidEmail', function (nickname, callback) { callback(null, true); });
+      sinon.stub(memberstore, 'saveMember', function (member, callback) { callback(null); });
+      var participantSave = sinon.stub(participantstore, 'saveParticipant', function (participant, callback) { callback(null); });
+      var notificationCall = sinon.stub(notifications, 'newMemberRegistered', function () { return undefined; });
+
+      // the following stub indicates that the member already exists
+      sinon.stub(groupsAndMembersService, 'getUserWithHisGroups', function (nickname, callback) { callback(null, socratesMember); });
+      // and that the participant also exists
+      sinon.stub(participantstore, 'getParticipant', function (id, callback) { callback(null, socratesParticipant); });
+      appWithSocratesMember
+        .post('/submit')
+        .send('id=0815&firstname=A&lastname=B')
+        .send('nickname=nickerinack')
+        .send('email=here@there.org')
+        .expect(302)
+        .expect('location', '/', function (err) {
+          expect(participantSave.called).to.be(false);
+          expect(notificationCall.called).to.be(false);
+          done(err);
+        });
+    });
+
+    it('saves a new SoCraTes member and a new participant and triggers notification sending', function (done) {
+      sinon.stub(membersService, 'isValidNickname', function (nickname, callback) { callback(null, true); });
+      sinon.stub(membersService, 'isValidEmail', function (nickname, callback) { callback(null, true); });
+      sinon.stub(memberstore, 'allMembers', function (callback) { callback(null, [softwerkskammerMember, socratesMember]); });
+      sinon.stub(memberstore, 'saveMember', function (member, callback) { callback(null); });
+      var participantSave = sinon.stub(participantstore, 'saveParticipant', function (participant, callback) { callback(null); });
+      var notificationCall = sinon.stub(notifications, 'newMemberRegistered', function () { return undefined; });
+
+      // the following stub indicates that the member does not exist yet
+      sinon.stub(groupsAndMembersService, 'getUserWithHisGroups', function (nickname, callback) { callback(null); });
+      // and that the participant does not exist either
+      sinon.stub(participantstore, 'getParticipant', function (id, callback) { callback(null); });
+      appWithSocratesMember
+        .post('/submit')
+        .send('id=0815&firstname=A&lastname=B')
+        .send('nickname=nickerinack')
+        .send('email=here@there.org')
+        .expect(302)
+        .expect('location', '/', function (err) {
+          expect(participantSave.called).to.be(true);
+          expect(notificationCall.called).to.be(true);
+          done(err);
+        });
+    });
 
   });
 
