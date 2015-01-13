0fad3b796d9d8411d06cc4a795d18f99f7362557
leider <derleider@web.de>
2015-01-13 17:50:39 +0100

    make conversion of activity to calendar event local in calendarService

--- softwerkskammer/test/activities/calendarService_test.js
+++ softwerkskammer/test/activities/calendarService_test.js
@@ -1,27 +1,41 @@
 'use strict';
 
 var expect = require('must');
+var sinon = require('sinon').sandbox.create();
 
 var beans = require('../../testutil/configureForTest').get('beans');
 
 var calendarService = beans.get('calendarService');
 var fieldHelpers = beans.get('fieldHelpers');
 var Activity = beans.get('activity');
+var activitystore = beans.get('activitystore');
 
 describe('Calendar Service', function () {
+  var start = fieldHelpers.parseToMomentUsingDefaultTimezone('01.04.2013');
+  var end = fieldHelpers.parseToMomentUsingDefaultTimezone('01.05.2013');
+  var activity = new Activity({
+    title: 'Title',
+    startUnix: fieldHelpers.parseToUnixUsingDefaultTimezone('04.04.2013'),
+    endUnix: fieldHelpers.parseToUnixUsingDefaultTimezone('05.04.2013'),
+    url: 'myURL'
+  });
 
-  it('converts a wellformed Activity to a calendar display event without colors given', function () {
-    var activity = new Activity({
-      title: 'Title',
-      startUnix: fieldHelpers.parseToUnixUsingDefaultTimezone('04.04.2013'),
-      endUnix: fieldHelpers.parseToUnixUsingDefaultTimezone('05.04.2013'),
-      url: 'myURL'
+  before(function () {
+    sinon.stub(activitystore, 'allActivitiesByDateRangeInAscendingOrder', function (rangeFrom, rangeTo, callback) {
+      callback(null, [activity]);
     });
-    var event = calendarService.asCalendarEvent(activity);
-    expect('Title').to.equal(event.title);
-    expect('2013-04-04T00:00:00+02:00').to.equal(event.start); // includes timezone offset!
-    expect('/activities/myURL').to.equal(event.url);
-    expect('#353535').to.equal(event.color);
+  });
+
+  it('loads and converts a wellformed Activity to a calendar display event without colors given', function (done) {
+    calendarService.eventsBetween(start, end, null, function (err, activities) {
+      expect(activities).to.have.length(1);
+      var event = activities[0];
+      expect('Title').to.equal(event.title);
+      expect('2013-04-04T00:00:00+02:00').to.equal(event.start); // includes timezone offset!
+      expect('/activities/myURL').to.equal(event.url);
+      expect('#353535').to.equal(event.color);
+      done();
+    });
   });
 
 });
