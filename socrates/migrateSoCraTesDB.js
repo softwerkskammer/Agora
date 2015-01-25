e38885cc33ce0a56618ef183282985add87ec072
Nicole Rauch <nicole.m@gmx.de>
2015-01-25 22:28:55 +0100

    Migration script that transfers last year's home address, billing address and T-Shirt size to the respective SoCraTes subscribers.

--- socrates/migrateSoCraTesDB.js
+++ socrates/migrateSoCraTesDB.js
@@ -2,10 +2,13 @@
 
 require('./configure'); // initializing parameters
 var _ = require('lodash');
+var util = require('util');
 var async = require('async');
 var beans = require('simple-configure').get('beans');
 var subscriberService = beans.get('subscriberService');
 var memberstore = beans.get('memberstore');
+var subscriberstore = beans.get('subscriberstore');
+var activitystore = beans.get('activitystore');
 
 var really = process.argv[2];
 var doSave = process.argv[3] === 'doSave';
@@ -15,19 +18,37 @@ if (!really || really !== 'really') {
   process.exit();
 }
 
-var count = 1;
-memberstore.socratesOnlyMembers(function (err, members) {
-  async.each(members,
-    function (member, callback) {
-      console.log('Socrates-Only ' + count + ': ' + member.displayName());
-      count = count + 1;
-      if (doSave) {
-        return subscriberService.createSubscriberIfNecessaryFor(member.id(), callback);
+activitystore.getActivity('socrates-2014', function (err, activity) {
+  if (err) { return console.log(err); }
+  console.log('activity: ' + util.inspect(activity));
+  var memberIds = activity.memberIdsOfAddons();
+
+  async.eachSeries(memberIds, function (memberId, callback) {
+    console.log('-------');
+    subscriberstore.getSubscriber(memberId, function (err, subscriber) {
+      if (err) {
+        console.log('load failed for id ' + memberId + ' : ' + err);
+        return callback(err);
       }
-      callback(null, null);
-    },
-    function (err) {
-      if (err) { console.log(err); }
-      process.exit();
+      console.log('subscriber: ' + util.inspect(subscriber));
+      var addon = activity.addonForMember(memberId);
+      console.log(' with 2014 addon-state ' + util.inspect(addon.state));
+      subscriber.addon().fillFromUI(addon.state);
+      console.log('  yields subscriber: ' + util.inspect(subscriber));
+      if (doSave) {
+        subscriberstore.saveSubscriber(subscriber, function (err) {
+          if (err) {
+            console.log('Save failed: ' + err);
+            return callback(err);
+          }
+          callback(null);
+        });
+      } else {
+        callback(null);
+      }
     });
+  }, function (err) {
+    if (err) { return console.log(err); }
+    process.exit();
+  });
 });
