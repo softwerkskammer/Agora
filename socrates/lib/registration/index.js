cd08da8673776cb354d0bcae7fb6dc14d280e0ff
Nicole Rauch <nicole.m@gmx.de>
2015-01-25 20:40:58 +0100

    Adapt the registration form to match the new data layout.

--- socrates/lib/registration/index.js
+++ socrates/lib/registration/index.js
@@ -5,27 +5,29 @@ var beans = require('simple-configure').get('beans');
 var misc = beans.get('misc');
 var membersService = beans.get('membersService');
 var mailsenderService = beans.get('mailsenderService');
+var subscriberstore = beans.get('subscriberstore');
 
 var app = misc.expressAppIn(__dirname);
 
-app.get('/participate', function (req, res) {
-  var participation = {
-    member: req.user.member,
-    user: {
-      privateaddress: 'Morgenstr. 41\n76131 Karlsruhe\nDeutschland',
-      billingaddress: 'Same',
-      tshirtsize: 'L'
-    },
-    room: 'double',
-    nights: 2
-  };
-  var roomOptions = [
-    {id: 'single', name: 'Single', two: 214, three: 329, four: 412},
-    {id: 'double', name: 'Double shared …', shareable: true, two: 174, three: 269, four: 332},
-    {id: 'junior', name: 'Junior shared …', shareable: true, two: 168, three: 260, four: 320},
-    {id: 'juniorAlone', name: 'Junior (exclusive)', two: 236, three: 362, four: 456}
-  ];
-  res.render('participate', {participation: participation, roomOptions: roomOptions});
+app.get('/participate', function (req, res, next) {
+  if (!req.user.member) {return next(); }
+
+  subscriberstore.getSubscriber(req.user.member.id(), function (err, subscriber) {
+    if (err) { return next(err); }
+    var participation = {
+      member: req.user.member,
+      addon: subscriber.addon(),
+      room: 'double',
+      nights: 2
+    };
+    var roomOptions = [
+      {id: 'single', name: 'Single', two: 214, three: 329, four: 412},
+      {id: 'double', name: 'Double shared …', shareable: true, two: 174, three: 269, four: 332},
+      {id: 'junior', name: 'Junior shared …', shareable: true, two: 168, three: 260, four: 320},
+      {id: 'juniorAlone', name: 'Junior (exclusive)', two: 236, three: 362, four: 456}
+    ];
+    res.render('participate', {participation: participation, roomOptions: roomOptions});
+  });
 });
 
 app.post('/participate', function (req, res) {
