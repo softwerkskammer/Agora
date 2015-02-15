bbb146fdbe4ff525c0dd24fe3e019a549e1aa167
Nicole Rauch <nicole.m@gmx.de>
2015-02-15 19:15:20 +0100

    Simplified /registration/participate (for later use)

--- socrates/lib/registration/index.js
+++ socrates/lib/registration/index.js
@@ -37,13 +37,7 @@ app.get('/participate', function (req, res, next) {
       room: 'double',
       nights: 2
     };
-    var roomOptions = [
-      {id: 'single', name: 'Single', two: 214, three: 329, four: 412},
-      {id: 'double', name: 'Double shared …', shareable: true, two: 174, three: 269, four: 332},
-      {id: 'junior', name: 'Junior shared …', shareable: true, two: 168, three: 260, four: 320},
-      {id: 'juniorAlone', name: 'Junior (exclusive)', two: 236, three: 362, four: 456}
-    ];
-    res.render('participate', {participation: participation, roomOptions: roomOptions});
+    res.render('participate', {participation: participation});
   });
 });
 
