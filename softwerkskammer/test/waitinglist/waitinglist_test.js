f1c7982f1c12d1d3ce2880479cc1c366f6d16865
leider <derleider@web.de>
2015-03-07 21:14:37 +0100

    first waitinglist app tests (to increase coverage)

---
+++ softwerkskammer/test/waitinglist/waitinglist_test.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var request = require('supertest');
+var sinon = require('sinon').sandbox.create();
+var expect = require('must');
+var moment = require('moment-timezone');
+
+var beans = require('../../testutil/configureForTest').get('beans');
+
+var Activity = beans.get('activity');
+var activitiesService = beans.get('activitiesService');
+var waitinglistService = beans.get('waitinglistService');
+
+var app = require('../../testutil/testHelper')('waitinglistApp').createApp('superuser');
+
+describe('Waitinglist application', function () {
+
+  beforeEach(function () {
+    sinon.stub(activitiesService, 'getActivityWithGroupAndParticipants', function (url, callback) {
+      callback(null, new Activity({url: url, title: 'Activity\'s Title'}));
+    });
+    sinon.stub(waitinglistService, 'waitinglistFor', function (url, callback) {
+      callback(null, []);
+    });
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
+
+  it('shows the list of announcements as retrieved from the store', function (done) {
+    request(app)
+      .get('/activity')
+      .expect(200)
+      .expect(/<h2>Activity\'s Title/)
+      .expect(/<small> Warteliste/)
+      .expect(/activities\/activity/)
+      .expect(/Für die gewählten Wartelisteneinträge /).expect(/title/, done);
+  });
+
+});
