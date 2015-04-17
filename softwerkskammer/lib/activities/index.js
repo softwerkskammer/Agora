729b46c33f227a92069b763cfcda59f666f620a8
Nicole Rauch <nicole.m@gmx.de>
2015-04-17 21:50:26 +0200

    Put the activities' participants list back in place (oops...)

--- softwerkskammer/lib/activities/index.js
+++ softwerkskammer/lib/activities/index.js
@@ -344,6 +344,15 @@ app.post('/removeFromWaitinglist', function (req, res, next) { // removeFromWait
   });
 });
 
+app.get('/addons/:url', function (req, res, next) {
+  activitiesService.getActivityWithGroupAndParticipants(req.params.url, function (err, activity) {
+    if (!res.locals.accessrights.canEditActivity(activity)) {
+      return res.redirect('/activities/' + encodeURIComponent(req.params.url));
+    }
+    res.render('managementTables', {activity: activity});
+  });
+});
+
 app.get('/delete/:activityUrl', function (req, res, next) {
   var url = req.params.activityUrl;
   activitystore.getActivity(url, function (err, activity) {
