a46e13d5e2cf67cb0f25e21f484ad2784890298f
leider <derleider@web.de>
2015-04-26 11:34:02 +0200

    on registration we now send a mail to the superusers

--- socrates/lib/notifications/index.js
+++ socrates/lib/notifications/index.js
@@ -12,21 +12,25 @@ var logger = require('winston').loggers.get('transactions');
 var jade = require('jade');
 var path = require('path');
 
+function renderingOptions(member) {
+  return {
+    pretty: true,
+    member: member,
+    url: conf.get('publicUrlPrefix')
+  };
+}
+
 module.exports = {
   newSoCraTesMemberRegistered: function (member) {
     memberstore.allMembers(function (err, members) {
       if (err || !members) { return logger.error(err); }
       subscriberstore.allSubscribers(function (err, subscribers) {
         if (err || !subscribers) { return logger.error(err); }
-        var renderingOptions = {
-          pretty: true,
-          member: member,
-          url: conf.get('publicUrlPrefix'),
-          count: subscribers.length
-        };
+        var options = renderingOptions(member);
+        options.count = subscribers.length;
         var filename = path.join(__dirname, 'jade/newmembertemplate.jade');
         var receivers = Member.superuserEmails(members);
-        notifications._sendMail(receivers, 'Neuer Interessent', jade.renderFile(filename, renderingOptions));
+        notifications._sendMail(receivers, 'Neuer Interessent', jade.renderFile(filename, options));
       });
     });
   },
@@ -34,30 +38,28 @@ module.exports = {
   newParticipant: function (memberID, bookingdetails) {
     memberstore.getMemberForId(memberID, function (err, member) {
       if (err || !member) { return logger.error(err); }
-      var renderingOptions = {
-        pretty: true,
-        member: member,
-        url: conf.get('publicUrlPrefix'),
-        bookingdetails: bookingdetails
-      };
+      var options = renderingOptions(member);
+      options.bookingdetails = bookingdetails;
       var filename = path.join(__dirname, 'jade/registrationConfirmation.jade');
       var receivers = [member.email()];
-      notifications._sendMail(receivers, 'SoCraTes Registration Confirmation', jade.renderFile(filename, renderingOptions));
+      notifications._sendMail(receivers, 'SoCraTes Registration Confirmation', jade.renderFile(filename, options));
+      memberstore.allMembers(function (err, members) {
+        if (err || !members) { return logger.error(err); }
+        var filenameSuperuser = path.join(__dirname, 'jade/superuserRegistrationNotification.jade');
+        var superusers = Member.superuserEmails(members);
+        notifications._sendMail(superusers, 'New SoCraTes Registration', jade.renderFile(filenameSuperuser, options));
+      });
     });
   },
 
   newWaitinglistEntry: function (memberID, bookingdetails) {
     memberstore.getMemberForId(memberID, function (err, member) {
       if (err || !member) { return logger.error(err); }
-      var renderingOptions = {
-        pretty: true,
-        member: member,
-        url: conf.get('publicUrlPrefix'),
-        bookingdetails: bookingdetails
-      };
+      var options = renderingOptions(member);
+      options.bookingdetails = bookingdetails;
       var filename = path.join(__dirname, 'jade/waitinglistConfirmation.jade');
       var receivers = [member.email()];
-      notifications._sendMail(receivers, 'SoCraTes Waitinglist Confirmation', jade.renderFile(filename, renderingOptions));
+      notifications._sendMail(receivers, 'SoCraTes Waitinglist Confirmation', jade.renderFile(filename, options));
     });
   }
 };
