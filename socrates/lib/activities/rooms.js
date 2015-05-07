5a0b2986da1df9ec7111a68fde885f1b80fa43d1
Nicole Rauch <nicole.m@gmx.de>
2015-05-07 09:46:56 +0200

    Added more functionality to the rooms.

--- socrates/lib/activities/rooms.js
+++ socrates/lib/activities/rooms.js
@@ -10,9 +10,26 @@ function Rooms(roomsObject, allKnownMemberIds) {
 }
 
 Rooms.prototype.add = function (memberId1, memberId2) {
-  if (this.allKnownMemberIds.indexOf(memberId1) > -1 && this.allKnownMemberIds.indexOf(memberId2) > -1 && memberId1 !== memberId2) {
+  var participantsInRoom = this.participantsInRoom();
+  var bothAreKnown = this.allKnownMemberIds.indexOf(memberId1) > -1 && this.allKnownMemberIds.indexOf(memberId2) > -1;
+  var bothAreNotInARoom = participantsInRoom.indexOf(memberId1) === -1 && participantsInRoom.indexOf(memberId2) === -1;
+  var theyAreDifferent = memberId1 !== memberId2;
+
+  if (bothAreKnown && bothAreNotInARoom && theyAreDifferent) {
     this.state.push({participant1: memberId1, participant2: memberId2});
   }
+};
+
+Rooms.prototype.participantsInRoom = function () {
+  var participantsInRoom = [];
+  participantsInRoom.push(_.pluck(this.state, 'participant1'));
+  participantsInRoom.push(_.pluck(this.state, 'participant2'));
+  return _.flatten(participantsInRoom);
+};
+
+Rooms.prototype.participantsWithoutRoom = function () {
+  var participantsInRoom = this.participantsInRoom();
+  return _.filter(this.allKnownMemberIds, function (memberId) {return participantsInRoom.indexOf(memberId) === -1; });
 };
 
 module.exports = Rooms;
--- socrates/test/activities/rooms_object_test.js
+++ socrates/test/activities/rooms_object_test.js
@@ -24,13 +24,13 @@ describe('Rooms', function () {
     var roomsInResource = [];
     var rooms = new Rooms(roomsInResource, allKnownMemberIds);
 
-    rooms.add('memberId1', 'unknownMemberId');
+    rooms.add('memberId1', undefined);
     expect(roomsInResource).to.have.length(0);
 
-    rooms.add('unknownMemberId', 'memberId5');
+    rooms.add(null, 'memberId5');
     expect(roomsInResource).to.have.length(0);
 
-    rooms.add('unknownMemberId', 'anotherUnknownMemberId');
+    rooms.add(undefined, null);
     expect(roomsInResource).to.have.length(0);
   });
 
@@ -43,16 +43,52 @@ describe('Rooms', function () {
   });
 
   it('does not create a room if one of the participants already is in a room', function () {
+    var roomsInResource = [{ participant1: 'memberId1', participant2: 'memberId2' }];
+    var rooms = new Rooms(roomsInResource, allKnownMemberIds);
 
+    rooms.add('memberId1', 'memberId3');
+    expect(roomsInResource).to.have.length(1);
+    expect(roomsInResource[0]).to.eql({participant1: 'memberId1', participant2: 'memberId2'});
+
+    rooms.add('memberId4', 'memberId2');
+    expect(roomsInResource).to.have.length(1);
+    expect(roomsInResource[0]).to.eql({participant1: 'memberId1', participant2: 'memberId2'});
+
+    rooms.add('memberId2', 'memberId1');
+    expect(roomsInResource).to.have.length(1);
+    expect(roomsInResource[0]).to.eql({participant1: 'memberId1', participant2: 'memberId2'});
   });
+
   it('does not create a room if one of the participants is not known', function () {
+    var roomsInResource = [];
+    var rooms = new Rooms(roomsInResource, allKnownMemberIds);
 
+    rooms.add('memberId1', 'unknownMemberId');
+    expect(roomsInResource).to.have.length(0);
+
+    rooms.add('unknownMemberId', 'memberId5');
+    expect(roomsInResource).to.have.length(0);
+
+    rooms.add('unknownMemberId', 'anotherUnknownMemberId');
+    expect(roomsInResource).to.have.length(0);
   });
+
   it('lists those participants that already are in a room', function () {
+    var roomsInResource = [{ participant1: 'memberId1', participant2: 'memberId2' }];
+    var rooms = new Rooms(roomsInResource, allKnownMemberIds);
 
+    var participantsInRoom = rooms.participantsInRoom();
+
+    expect(participantsInRoom).to.eql(['memberId1', 'memberId2']);
   });
-  it('lists those participants that are not yet in a room', function () {
 
+  it('lists those participants that are not yet in a room', function () {
+    var roomsInResource = [{ participant1: 'memberId1', participant2: 'memberId2' }];
+    var rooms = new Rooms(roomsInResource, allKnownMemberIds);
+
+    var participantsWithoutRoom = rooms.participantsWithoutRoom();
+
+    expect(participantsWithoutRoom).to.eql(['memberId3', 'memberId4', 'memberId5']);
   });
 
 });
