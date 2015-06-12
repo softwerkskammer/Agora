166a67339f4d96984097614e6413df2b1099032a
leider <derleider@web.de>
2015-06-12 23:08:57 +0200

    fixing "pay" button behavior for waitinglist members

--- socrates/test/middleware/accessrights_test.js
+++ socrates/test/middleware/accessrights_test.js
@@ -1,9 +1,11 @@
 'use strict';
 
 var expect = require('must');
+var sinon = require('sinon').sandbox.create();
 
 var conf = require('../../testutil/configureForTest');
 var beans = conf.get('beans');
+var socratesActivitiesService = beans.get('socratesActivitiesService');
 var accessrights = beans.get('accessrights');
 
 describe('accessrights', function () {
@@ -11,13 +13,20 @@ describe('accessrights', function () {
   var req;
   var res;
   var next;
+  var socrates;
 
   beforeEach(function () {
+    socrates = {};
+    sinon.stub(socratesActivitiesService, 'getCurrentSocrates', function (callback) { callback(null, socrates); });
     req = {};
     res = {locals: {}};
     next = function () { return; };
 
     accessrights(req, res, next);
+  });
+
+  afterEach(function () {
+    sinon.restore();
   });
 
   var loginAs = function (memberId) {
@@ -39,11 +48,44 @@ describe('accessrights', function () {
   });
 
   it('allows activity editing for SoCraTes-Admins', function () {
-    conf.addProperties({ socratesAdmins: ['memberId'] });
+    conf.addProperties({socratesAdmins: ['memberId']});
 
     loginAs('memberId');
     expect(res.locals.accessrights.canEditActivity()).to.be.true();
 
-    conf.addProperties({ socratesAdmins: [] });
+    conf.addProperties({socratesAdmins: []});
+  });
+
+  describe.only('tells if the current user needs to pay', function () {
+    beforeEach(function () {
+      loginAs('memberId');
+    });
+
+    it('yes, if he is subscribed and has not paid', function () {
+      socrates.resources = function () {return {resourceNamesOf: function () { return ['name1']; }}; };
+      req.user.subscriber = {needsToPay: function () { return true; }};
+
+      expect(res.locals.accessrights.needsToPay()).to.be.true();
+    });
+
+    it('no, if he is not really subscribed (-> waitinglist) and has not paid', function () {
+      socrates.resources = function () {return {resourceNamesOf: function () { return []; }}; };
+      req.user.subscriber = {needsToPay: function () { return true; }};
+
+      expect(res.locals.accessrights.needsToPay()).to.be.false();
+    });
+
+    it('no, if he has paid', function () {
+      socrates.resources = function () {return {resourceNamesOf: function () { return ['name']; }}; };
+      req.user.subscriber = {needsToPay: function () { return false; }};
+
+      expect(res.locals.accessrights.needsToPay()).to.be.false();
+    });
+
+    it('no, if there is no subscriber at all', function () {
+      socrates.resources = function () {return {resourceNamesOf: function () { return []; }}; };
+
+      expect(res.locals.accessrights.needsToPay()).to.be.false();
+    });
   });
 });
