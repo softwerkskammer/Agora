2f5f76d2bb5e93e78eda57dbe84abf85b7d61d3a
leider <derleider@web.de>
2015-10-29 22:54:18 +0100

    eslint complains about sync file access

--- softwerkskammer/test/activities/chadoreporter.js
+++ softwerkskammer/test/activities/chadoreporter.js
@@ -7,7 +7,8 @@ after(function () {
   var analyzer = chado.analyzer;
   var reportArray = analyzer.read(chado.repo);
   chado.consoleReporter.logReport();
-  
+
+  /*eslint no-sync: 0*/
   fs.writeFileSync('chado-result.json', JSON.stringify(chado.repo, null, 2));
   
   expect(analyzer.getNotVerifiedAssumptions(reportArray)).to.have.length(0);
