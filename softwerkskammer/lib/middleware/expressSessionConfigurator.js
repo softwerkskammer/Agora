7ee968cef66a6589173f09bfe02275b76f5b1a72
leider <derleider@web.de>
2016-02-11 19:03:37 +0100

    replaced connect-mongostore by connect-mongo - much less dependencies (smaller / faster) - annoying message that bson-ext cannot be found is gone

--- softwerkskammer/lib/middleware/expressSessionConfigurator.js
+++ softwerkskammer/lib/middleware/expressSessionConfigurator.js
@@ -2,14 +2,23 @@
 var conf = require('simple-configure');
 var expressSession = require('express-session');
 var sevenDays = 86400 * 1000 * 7;
+var oneHour = 3600;
 
 var sessionStore;
 
 if (!conf.get('dontUsePersistentSessions')) {
-  var MongoStore = require('connect-mongostore')(expressSession);
-  sessionStore = new MongoStore(conf.get('mongoURL'));
-  sessionStore.options.stringify = true;
+  var MongoStore = require('connect-mongo')(expressSession);
+  sessionStore = new MongoStore({
+    url: conf.get('mongoURL'),
+    touchAfter: oneHour
+  });
 }
 
-module.exports = expressSession({key: conf.get('sessionkey'),
-  secret: conf.get('secret'), cookie: {maxAge: sevenDays}, store: sessionStore, resave: true, saveUninitialized: true});
+module.exports = expressSession({
+  key: conf.get('sessionkey'),
+  secret: conf.get('secret'),
+  cookie: {maxAge: sevenDays},
+  store: sessionStore,
+  resave: false,
+  saveUninitialized: false
+});
