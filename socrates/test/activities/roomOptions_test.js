ec865e2bc8bde7ea7ec45b87da7df0cb32fd5110
Nicole Rauch <nicole.m@gmx.de>
2016-03-03 01:01:32 +0100

    Test for roomOptions.all()

--- socrates/test/activities/roomOptions_test.js
+++ socrates/test/activities/roomOptions_test.js
@@ -26,6 +26,37 @@ describe('Room Options', function () {
       expect(roomOptions.informationFor('single', '4').until).to.be('sunday evening');
       expect(roomOptions.informationFor('single', '5').until).to.be('monday morning');
     });
+
+    it('returns whether the registration and waitinglist checkboxes must be displayed', function () {
+      function activity(alreadyRegistered, isFull) {
+        return {
+          isAlreadyRegistered: function () { return alreadyRegistered; },
+          resourceNamed: function () {
+            return {
+              canSubscribe: function () { return !isFull; }, // isRegistrationOpen (always true) && ! isFull
+              hasWaitinglist: function () { return true; }  // has a _hasWaitinglist entry (always true)
+            };
+          }
+        };
+      }
+
+      var registrationIsOpen = true;
+      // if registered, it does not matter whether the resource is full - we always show the checkboxes to indicate the registered slot
+      expect(roomOptions.all(activity(true, true), 'member-id', registrationIsOpen)[0].displayRegistrationCheckboxes).to.eql(true);
+      expect(roomOptions.all(activity(true, false), 'member-id', registrationIsOpen)[0].displayRegistrationCheckboxes).to.eql(true);
+
+      // if not registered, the resource can only be selected if it is not full
+      expect(roomOptions.all(activity(false, true), 'member-id', registrationIsOpen)[0].displayRegistrationCheckboxes).to.eql(false);
+      expect(roomOptions.all(activity(false, false), 'member-id', registrationIsOpen)[0].displayRegistrationCheckboxes).to.eql(true);
+
+      // if the registration is not open, the checkboxes are always displayed, no matter the status of the resource
+      var registrationIsNotOpen = false;
+      expect(roomOptions.all(activity(true, true), 'member-id', registrationIsNotOpen)[0].displayRegistrationCheckboxes).to.eql(true);
+      expect(roomOptions.all(activity(true, false), 'member-id', registrationIsNotOpen)[0].displayRegistrationCheckboxes).to.eql(true);
+      expect(roomOptions.all(activity(false, true), 'member-id', registrationIsNotOpen)[0].displayRegistrationCheckboxes).to.eql(true);
+      expect(roomOptions.all(activity(false, false), 'member-id', registrationIsNotOpen)[0].displayRegistrationCheckboxes).to.eql(true);
+    });
+
   });
 
   describe('for a room\'s waitinglist', function () {
