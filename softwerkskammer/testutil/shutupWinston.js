c585fb7d2d65730dbec41650b75e54f5672f6b48
Alexandre Soler Sanandres <alexandre@soler-sanandres.net>
2016-04-18 23:03:05 +0200

    Revert "race condition logging und entsprechenden Tests"
    This reverts commit ec32eeb5d533f66be71e4fe87fdbed39db2186b4.

--- softwerkskammer/testutil/shutupWinston.js
+++ softwerkskammer/testutil/shutupWinston.js
@@ -2,24 +2,17 @@
 var winston = require('winston');
 
 function shutup() {
-  var warnings = [];
-
   winston.loggers = {
     add: function () { return undefined; },
     get: function () {
       var dummyLogger = {
-        warn: function (message) {
-          warnings.push(message);
-          return undefined; },
+        warn: function () { return undefined; },
         info: function () { return undefined; },
-        error: function () { return undefined; },
-        getWarnings: function() {return warnings;},
-        clearWarnings: function() {warnings = [];}
+        error: function () { return undefined; }
       };
       return dummyLogger;
     }
   };
-
 }
 
 module.exports = shutup;
