ec32eeb5d533f66be71e4fe87fdbed39db2186b4
Alexandre Soler Sanandres <alexandre@soler-sanandres.net>
2016-04-18 22:52:53 +0200

    race condition logging und entsprechenden Tests

--- softwerkskammer/testutil/shutupWinston.js
+++ softwerkskammer/testutil/shutupWinston.js
@@ -2,17 +2,24 @@
 var winston = require('winston');
 
 function shutup() {
+  var warnings = [];
+
   winston.loggers = {
     add: function () { return undefined; },
     get: function () {
       var dummyLogger = {
-        warn: function () { return undefined; },
+        warn: function (message) {
+          warnings.push(message);
+          return undefined; },
         info: function () { return undefined; },
-        error: function () { return undefined; }
+        error: function () { return undefined; },
+        getWarnings: function() {return warnings;},
+        clearWarnings: function() {warnings = [];}
       };
       return dummyLogger;
     }
   };
+
 }
 
 module.exports = shutup;
