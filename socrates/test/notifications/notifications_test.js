bb340de4251340009d98bb932fe6aec0a454d7c3
Christoph Neuroth <christoph.neuroth@gmail.com>
2016-07-04 13:31:29 +0200

    Extract function

--- socrates/test/notifications/notifications_test.js
+++ socrates/test/notifications/notifications_test.js
@@ -29,6 +29,10 @@ var superman = new Member({
 });
 
 describe('Notifications', function () {
+  var stubOrganizers = function (concernedParties) {
+    sinon.stub(memberstore, 'superUsers',
+      function (callback) { callback(null, concernedParties); });
+  };
 
   beforeEach(function () {
     sinon.stub(transport, 'sendMail');
@@ -40,7 +44,7 @@ describe('Notifications', function () {
 
   describe('for new members', function () {
     it('creates a meaningful text and subject', function () {
-      sinon.stub(memberstore, 'superUsers', function (callback) { callback(null, [superman]); });
+      stubOrganizers([superman]);
       sinon.stub(subscriberstore, 'allSubscribers', function (callback) { callback(null, ['p1', 'p2', 'p3']); });
 
       notifications.newSoCraTesMemberRegistered(hans);
@@ -55,7 +59,7 @@ describe('Notifications', function () {
     });
 
     it('triggers mail sending for superusers', function () {
-      sinon.stub(memberstore, 'superUsers', function (callback) { callback(null, [superman]); });
+      stubOrganizers([superman]);
       sinon.stub(subscriberstore, 'allSubscribers', function (callback) { callback(null, ['p1', 'p2', 'p3']); });
 
       notifications.newSoCraTesMemberRegistered(hans);
@@ -68,8 +72,8 @@ describe('Notifications', function () {
       expect(options.from).to.contain('Softwerkskammer Benachrichtigungen');
     });
 
-    it('does not trigger mail sending if there are no superusers', function () {
-      sinon.stub(memberstore, 'superUsers', function (callback) { callback(null, []); });
+    it('does not trigger mail sending if there are no concerned parties', function () {
+      stubOrganizers([]);
       sinon.stub(subscriberstore, 'allSubscribers', function (callback) { callback(null, ['p1', 'p2', 'p3']); });
 
       notifications.newSoCraTesMemberRegistered(hans);
@@ -80,7 +84,7 @@ describe('Notifications', function () {
   describe('for participation', function () {
     it('creates a meaningful text and subject for immediate registrants', function () {
       sinon.stub(memberstore, 'getMemberForId', function (id, callback) { callback(null, hans); });
-      sinon.stub(memberstore, 'superUsers', function (callback) { callback(null, []); });
+      stubOrganizers([]);
 
       notifications.newParticipant(hans, roomOptions.informationFor('junior', 3));
       expect(transport.sendMail.calledOnce).to.be(true);
@@ -93,7 +97,7 @@ describe('Notifications', function () {
 
     it('creates a meaningful text and subject for registrants coming from the waitinglist', function () {
       sinon.stub(memberstore, 'getMemberForId', function (id, callback) { callback(null, hans); });
-      sinon.stub(memberstore, 'superUsers', function (callback) { callback(null, []); });
+      stubOrganizers([]);
 
       var bookingdetails = roomOptions.informationFor('junior', 3);
       bookingdetails.fromWaitinglist = true;
@@ -106,7 +110,7 @@ describe('Notifications', function () {
 
     it('sends a meaningful mail to superusers', function () {
       sinon.stub(memberstore, 'getMemberForId', function (id, callback) { callback(null, hans); });
-      sinon.stub(memberstore, 'superUsers', function (callback) { callback(null, [superman]); });
+      stubOrganizers([superman]);
 
       notifications.newParticipant(hans, roomOptions.informationFor('junior', 3));
       expect(transport.sendMail.calledTwice).to.be(true);
@@ -122,7 +126,7 @@ describe('Notifications', function () {
   describe('for waitinglist', function () {
     it('creates a meaningful text and subject', function () {
       sinon.stub(memberstore, 'getMemberForId', function (id, callback) { callback(null, hans); });
-      sinon.stub(memberstore, 'superUsers', function (callback) { callback(null, []); });
+      stubOrganizers([]);
 
       notifications.newWaitinglistEntry(hans, [roomOptions.informationFor('junior', 3)]);
       expect(transport.sendMail.calledOnce).to.be(true);
@@ -131,9 +135,9 @@ describe('Notifications', function () {
       expect(options.html).to.contain('junior room (exclusively)');
     });
 
-    it('sends a meaningful mail to superusers', function () {
+    it('sends a meaningful mail to members concerned with registration', function () {
       sinon.stub(memberstore, 'getMemberForId', function (id, callback) { callback(null, hans); });
-      sinon.stub(memberstore, 'superUsers', function (callback) { callback(null, [superman]); });
+      stubOrganizers([superman]);
 
       notifications.newWaitinglistEntry(hans, [roomOptions.informationFor('junior', 3)]);
       expect(transport.sendMail.calledTwice).to.be(true);
