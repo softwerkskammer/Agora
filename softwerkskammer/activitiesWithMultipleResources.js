631b6cfd46afee8080022ad5db9188621d08665d
leider <derleider@web.de>
2016-09-19 23:03:07 +0200

    script for finding activities with more than one resource

---
+++ softwerkskammer/activitiesWithMultipleResources.js
@@ -0,0 +1,30 @@
+/*eslint no-process-exit: 0 */
+/* eslint no-console: 0 */
+'use strict';
+
+require('./configure'); // initializing parameters
+var beans = require('simple-configure').get('beans');
+var activitystore = beans.get('activitystore');
+
+var really = process.argv[2];
+if (!really || really !== 'really') {
+  console.log('If you want to run this script, append "really" to the command line.');
+  process.exit();
+}
+
+activitystore.allActivities((err, activities) => {
+  if (err) {
+    console.log(err);
+    process.exit();
+  }
+  var simple = activities.filter(activity => activity.resourceNames().length === 1);
+  var more = activities.filter(activity => activity.resourceNames().length > 1);
+
+  more.forEach(activity => console.log(activity.title() + ' (url: ' + activity.url() + ')'))
+  console.log(more.length + ' activites with multiple resources');
+  console.log(simple.length + ' activites with onle one resource');
+  console.log(activities.length + ' activites total');
+
+  process.exit();
+
+});
