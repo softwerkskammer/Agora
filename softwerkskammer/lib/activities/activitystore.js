e0370f1deb430bf758b204c7bae9ff5c9f24f8c3
leider <derleider@web.de>
2016-09-23 20:04:12 +0200

    activitystore also without lodash and ES6ified

--- softwerkskammer/lib/activities/activitystore.js
+++ softwerkskammer/lib/activities/activitystore.js
@@ -1,15 +1,16 @@
 /*global emit */
 'use strict';
 
-var beans = require('simple-configure').get('beans');
-var _ = require('lodash');
-var misc = beans.get('misc');
-var moment = require('moment-timezone');
+const beans = require('simple-configure').get('beans');
+const R = require('ramda');
 
-var logger = require('winston').loggers.get('transactions');
-var persistence = beans.get('activitiesPersistence');
-var Activity = beans.get('activity');
-var SoCraTesActivity = beans.get('socratesActivity');
+const misc = beans.get('misc');
+const moment = require('moment-timezone');
+
+const logger = require('winston').loggers.get('transactions');
+const persistence = beans.get('activitiesPersistence');
+const Activity = beans.get('activity');
+const SoCraTesActivity = beans.get('socratesActivity');
 
 function toActivity(callback, err, jsobject) {
   if (jsobject && jsobject.isSoCraTes) {
@@ -20,12 +21,7 @@ function toActivity(callback, err, jsobject) {
 
 function toActivityList(callback, err, jsobjects) {
   if (err) { return callback(err); }
-  callback(null, _.map(jsobjects, function (each) {
-    if (each && each.isSoCraTes) {
-      return new SoCraTesActivity(each);
-    }
-    return new Activity(each);
-  }));
+  callback(null, jsobjects.map(record => (record && record.isSoCraTes) ? new SoCraTesActivity(record) : new Activity(record)));
 }
 
 function allActivitiesByDateRange(rangeFrom, rangeTo, sortOrder, callback) {
@@ -34,7 +30,7 @@ function allActivitiesByDateRange(rangeFrom, rangeTo, sortOrder, callback) {
       {endUnix: {$gt: rangeFrom}},
       {startUnix: {$lt: rangeTo}}
     ]
-  }, sortOrder, _.partial(toActivityList, callback));
+  }, sortOrder, R.partial(toActivityList, [callback]));
 }
 
 function allActivitiesByDateRangeInAscendingOrder(rangeFrom, rangeTo, callback) {
@@ -46,64 +42,64 @@ function allActivitiesByDateRangeInDescendingOrder(rangeFrom, rangeTo, callback)
 }
 
 function flattenAndSortMongoResultCollection(collection) {
-  return _.sortBy(_.flattenDeep(collection[0].value), 'startUnix');
+  return R.sortBy(R.prop('startUnix'), R.flatten(collection[0].value));
 }
 
 module.exports = {
-  allActivities: function (callback) {
-    persistence.list({startUnix: 1}, _.partial(toActivityList, callback));
+  allActivities: function allActivities(callback) {
+    persistence.list({startUnix: 1}, R.partial(toActivityList, [callback]));
   },
 
   allActivitiesByDateRangeInAscendingOrder: allActivitiesByDateRangeInAscendingOrder,
 
   allActivitiesByDateRangeInDescendingOrder: allActivitiesByDateRangeInDescendingOrder,
 
-  upcomingActivities: function (callback) {
-    var start = moment().unix();
-    var end = moment().add(10, 'years').unix();
+  upcomingActivities: function upcomingActivities(callback) {
+    const start = moment().unix();
+    const end = moment().add(10, 'years').unix();
     allActivitiesByDateRangeInAscendingOrder(start, end, callback);
   },
 
-  pastActivities: function (callback) {
-    var start = 0;
-    var end = moment().unix();
+  pastActivities: function pastActivities(callback) {
+    const start = 0;
+    const end = moment().unix();
     allActivitiesByDateRangeInDescendingOrder(start, end, callback);
   },
 
-  getActivity: function (url, callback) {
-    persistence.getByField({url: url}, _.partial(toActivity, callback));
+  getActivity: function getActivity(url, callback) {
+    persistence.getByField({url: url}, R.partial(toActivity, [callback]));
   },
 
-  getActivityForId: function (id, callback) {
-    persistence.getById(id, _.partial(toActivity, callback));
+  getActivityForId: function getActivityForId(id, callback) {
+    persistence.getById(id, R.partial(toActivity, [callback]));
   },
 
-  saveActivity: function (activity, callback) {
+  saveActivity: function saveActivity(activity, callback) {
     persistence.saveWithVersion(activity.state, callback);
   },
 
-  removeActivity: function (activity, callback) {
-    persistence.remove(activity.id(), function (err) {
+  removeActivity: function removeActivity(activity, callback) {
+    persistence.remove(activity.id(), err => {
       logger.info('Activity removed:' + JSON.stringify(activity));
       callback(err);
     });
   },
 
-  upcomingActivitiesForGroupIds: function (groupIds, callback) {
-    var start = moment().unix();
+  upcomingActivitiesForGroupIds: function upcomingActivitiesForGroupIds(groupIds, callback) {
+    const start = moment().unix();
 
     persistence.listByField({
       $and: [
         {endUnix: {$gt: start}},
         {assignedGroup: {$in: groupIds}}
       ]
-    }, {startUnix: 1}, _.partial(toActivityList, callback));
+    }, {startUnix: 1}, R.partial(toActivityList, [callback]));
   },
 
-  activitiesForGroupIdsAndRegisteredMemberId: function (groupIds, memberId, upcoming, callback) {
-    var map = function () {
+  activitiesForGroupIdsAndRegisteredMemberId: function activitiesForGroupIdsAndRegisteredMemberId(groupIds, memberId, upcoming, callback) {
+    function map() {
       /* eslint no-underscore-dangle: 0 */
-      var activity = this; // "this" holds the activity that is currently being examined
+      const activity = this; // "this" holds the activity that is currently being examined
 
       // is the assigned group in the list of groups?
       if (groupIds.indexOf(activity.assignedGroup) > -1) {
@@ -111,43 +107,37 @@ module.exports = {
       } else { // only try this if the first one failed -> otherwise we get duplicate entries!
 
         // is the member registered in one of the resources?
-        var memberIsRegistered = false;
-        var checkMemberId = function (elem) {
-          if (elem.memberId === memberId) {
-            emit(memberId, activity);
-            memberIsRegistered = true;
-          }
-        };
-        var resource;
-        for (resource in activity.resources) {
+        for (var resource in activity.resources) {
           if (activity.resources.hasOwnProperty(resource) && activity.resources[resource]._registeredMembers) {
-            activity.resources[resource]._registeredMembers.forEach(checkMemberId);
-            if (memberIsRegistered) { return; } // we only want to add the activity once
+            const memberIsRegistered = activity.resources[resource]._registeredMembers.some(
+              function (mem) { return mem.memberId === memberId; }
+            );
+            if (memberIsRegistered) {
+              emit(memberId, activity);
+              return;
+            } // we only want to add the activity once
           }
         }
       }
-    };
+    }
 
-    var reduce = function (key, values) {
+    function reduce(key, values) {
       return values;
-    };
+    }
 
-    var now = moment().unix();
-    var query = upcoming ? {endUnix: {$gt: now}} : {endUnix: {$lt: now}};
-    var parameters = {out: {inline: 1}, scope: {memberId: memberId, groupIds: groupIds}, query: query, jsMode: true};
+    const now = moment().unix();
+    const query = upcoming ? {endUnix: {$gt: now}} : {endUnix: {$lt: now}};
+    const parameters = {out: {inline: 1}, scope: {memberId: memberId, groupIds: groupIds}, query: query, jsMode: true};
 
-    persistence.mapReduce(map, reduce, parameters, function (err, collection) {
+    persistence.mapReduce(map, reduce, parameters, (err, collection) => {
       if (err && err.errmsg === 'ns doesn\'t exist') { return callback(null, []); } // no mongostore available -> nevermind
       if (err) { return callback(err); }
       if (!collection || collection.length === 0) {
         return callback(null, []);
       }
       // when there are many results, the value will be a nested array, so we need to flatten it:
-      var results = flattenAndSortMongoResultCollection(collection);
-      if (!upcoming) {
-        results = results.reverse();
-      }
-      return toActivityList(callback, null, results);
+      const results = flattenAndSortMongoResultCollection(collection);
+      return toActivityList(callback, null, !upcoming ? results.reverse() : results);
     });
   },
 
