6450b3ad0a6957fa8c82454532857fce80779bd4
Nicole Rauch <nicole.m@gmx.de>
2016-10-09 00:35:39 +0200

    Introduced more arrow functions.

--- socrates/lib/activities/index.js
+++ socrates/lib/activities/index.js
@@ -25,10 +25,10 @@ const reservedURLs = '^new$|^edit$|^submit$|^checkurl$\\+';
 const app = misc.expressAppIn(__dirname);
 
 function activitySubmitted(req, res, next) {
-  eventstoreService.getSoCraTesCommandProcessor(req.body.url, function (err, socratesCommandProcessor) {
+  eventstoreService.getSoCraTesCommandProcessor(req.body.url, (err, socratesCommandProcessor) => {
     if (err) { return next(err); }
     const events = socratesCommandProcessor.createConferenceEvents(req.body);
-    eventstoreService.saveCommandProcessor(socratesCommandProcessor, events, function (err1) {
+    eventstoreService.saveCommandProcessor(socratesCommandProcessor, events, err1 => {
       if (err1 && err1.message === CONFLICTING_VERSIONS) {
         // we try again because of a racing condition during save:
         statusmessage.errorMessage('message.title.conflict', 'message.content.save_error_retry').putIntoSession(req);
@@ -37,12 +37,12 @@ function activitySubmitted(req, res, next) {
       if (err1) { return next(err1); }
 
       // update the activity because we need it for the display in the SWK calendar
-      activitiesService.getActivityWithGroupAndParticipants(req.body.url, function (err2, activity) { // here we need a real activity
+      activitiesService.getActivityWithGroupAndParticipants(req.body.url, (err2, activity) => { // here we need a real activity
         if (err2) { return next(err2); }
         if (!activity) { activity = new Activity({owner: req.user.member.id()}); }
         req.body.isSoCraTes = true; // mark activity as SoCraTes activity (important for SWK)
         activity.fillFromUI(req.body);
-        activitystore.saveActivity(activity, function (err3) {
+        activitystore.saveActivity(activity, err3 => {
           if (err3 && err3.message === CONFLICTING_VERSIONS) {
             // we try again because of a racing condition during save:
             statusmessage.errorMessage('message.title.conflict', 'message.content.save_error_retry').putIntoSession(req);
@@ -57,12 +57,11 @@ function activitySubmitted(req, res, next) {
   });
 }
 
-app.get('/new', function (req, res) {
-  res.render('edit', {socratesReadModel: eventstoreService.newSoCraTesReadModel(), roomTypes: roomOptions.allIds()});
-});
+app.get('/new', (req, res) =>
+  res.render('edit', {socratesReadModel: eventstoreService.newSoCraTesReadModel(), roomTypes: roomOptions.allIds()}));
 
-app.get('/edit/:url', function (req, res, next) {
-  eventstoreService.getSoCraTesReadModel(req.params.url, function (err, socratesReadModel) {
+app.get('/edit/:url', (req, res, next) => {
+  eventstoreService.getSoCraTesReadModel(req.params.url, (err, socratesReadModel) => {
     if (err || !socratesReadModel) { return next(err); }
     if (!res.locals.accessrights.canEditActivity()) {
       return res.redirect('/registration/');
@@ -71,7 +70,7 @@ app.get('/edit/:url', function (req, res, next) {
   });
 });
 
-app.post('/submit', function (req, res, next) {
+app.post('/submit', (req, res, next) => {
   const year = req.body.startDate.split('/')[2];
   req.body.title = 'SoCraTes ' + year;
   req.body.url = 'socrates-' + year;
@@ -80,16 +79,13 @@ app.post('/submit', function (req, res, next) {
 
   async.parallel(
     [
-      function (callback) {
+      callback => {
         // we need this helper function (in order to have a closure?!)
         const validityChecker = function (url, cb) { eventstoreService.isValidUrl(url, cb); };
         validation.checkValidity(req.body.previousUrl.trim(), req.body.url.trim(), validityChecker, req.i18n.t('validation.url_not_available'), callback);
       },
-      function (callback) {
-        const errors = validation.isValidForActivity(req.body);
-        return callback(null, errors);
-      },
-      function (callback) {
+      callback => callback(null, validation.isValidForActivity(req.body)),
+      callback => {
         let errors = [];
         if (req.body.previousUrl && req.body.previousUrl !== req.body.url) {
           errors.push('It is impossible to alter the year of an existing SoCraTes conference.');
@@ -97,7 +93,7 @@ app.post('/submit', function (req, res, next) {
         return callback(null, errors);
       }
     ],
-    function (err, errorMessages) {
+    (err, errorMessages) => {
       if (err) { return next(err); }
       const realErrors = R.flatten(errorMessages).filter(message => message);
       if (realErrors.length === 0) {
@@ -108,23 +104,22 @@ app.post('/submit', function (req, res, next) {
   );
 });
 
-app.get('/checkurl', function (req, res) {
-  misc.validate(req.query.url, req.query.previousUrl, R.partial(eventstoreService.isValidUrl, [reservedURLs]), res.end);
-});
+app.get('/checkurl', (req, res) =>
+  misc.validate(req.query.url, req.query.previousUrl, R.partial(eventstoreService.isValidUrl, [reservedURLs]), res.end));
 
 // for management tables:
 
-app.get('/fromWaitinglistToParticipant/:roomType/:nickname', function (req, res) {
+app.get('/fromWaitinglistToParticipant/:roomType/:nickname', (req, res) => {
   const duration = 2;
 
-  socratesActivitiesService.fromWaitinglistToParticipant({nickname: req.params.nickname, roomType: req.params.roomType, duration}, moment.tz(), function (err) {
+  socratesActivitiesService.fromWaitinglistToParticipant({nickname: req.params.nickname, roomType: req.params.roomType, duration}, moment.tz(), err => {
     if (err) { return res.send('Error: ' + err); }
     res.send('-> Teilnehmer');
   });
 });
 
 function updateFor(updater, params, res, next) {
-  updater(params, function (err) {
+  updater(params, err => {
     if (err && err instanceof ValidationErrors) {
       return res.render('../../../views/errorPages/validationError', {errors: err.errors});
     }
@@ -133,15 +128,13 @@ function updateFor(updater, params, res, next) {
   });
 }
 
-app.post('/newDuration', function (req, res, next) {
-  updateFor(socratesActivitiesService.newDurationFor, R.pick(['nickname', 'roomType', 'duration'], req.body), res, next);
-});
+app.post('/newDuration', (req, res, next) =>
+  updateFor(socratesActivitiesService.newDurationFor, R.pick(['nickname', 'roomType', 'duration'], req.body), res, next));
 
-app.post('/newResource', function (req, res, next) {
-  updateFor(socratesActivitiesService.newRoomTypeFor, {nickname: req.body.nickname, newRoomType: req.body.newResourceName}, res, next);
-});
+app.post('/newResource', (req, res, next) =>
+  updateFor(socratesActivitiesService.newRoomTypeFor, {nickname: req.body.nickname, newRoomType: req.body.newResourceName}, res, next));
 
-app.post('/newWaitinglist', function (req, res, next) {
+app.post('/newWaitinglist', (req, res, next) => {
   let desiredRoomTypes = req.body['waitinglistOptions_' + encodeURIComponent(req.body.nickname)];
   if (desiredRoomTypes && !(desiredRoomTypes instanceof Array)) {
     desiredRoomTypes = [desiredRoomTypes];
@@ -149,20 +142,16 @@ app.post('/newWaitinglist', function (req, res, next) {
   updateFor(socratesActivitiesService.newWaitinglistFor, {nickname: req.body.nickname, newDesiredRoomTypes: desiredRoomTypes}, res, next);
 });
 
-app.post('/newParticipantPair', function (req, res, next) {
-  updateFor(socratesActivitiesService.addParticipantPairFor, {roomType: req.body.roomType, participant1Nick: req.body.participant1, participant2Nick: req.body.participant2}, res, next);
-});
+app.post('/newParticipantPair', (req, res, next) =>
+  updateFor(socratesActivitiesService.addParticipantPairFor, {roomType: req.body.roomType, participant1Nick: req.body.participant1, participant2Nick: req.body.participant2}, res, next));
 
-app.post('/removeParticipantPair', function (req, res, next) {
-  updateFor(socratesActivitiesService.removeParticipantPairFor, {roomType: req.body.roomType, participant1Nick: req.body.participant1, participant2Nick: req.body.participant2}, res, next);
-});
+app.post('/removeParticipantPair', (req, res, next) =>
+  updateFor(socratesActivitiesService.removeParticipantPairFor, {roomType: req.body.roomType, participant1Nick: req.body.participant1, participant2Nick: req.body.participant2}, res, next));
 
-app.post('/removeParticipant', function (req, res, next) {
-  updateFor(socratesActivitiesService.removeParticipantFor, {roomType: req.body.roomType, participantNick: req.body.participant}, res, next);
-});
+app.post('/removeParticipant', (req, res, next) =>
+  updateFor(socratesActivitiesService.removeParticipantFor, {roomType: req.body.roomType, participantNick: req.body.participant}, res, next));
 
-app.post('/removeWaitinglistMember', function (req, res, next) {
-  updateFor(socratesActivitiesService.removeWaitinglistMemberFor, {desiredRoomTypes: [req.body.roomType], waitinglistMemberNick: req.body.waitinglistMember}, res, next);
-});
+app.post('/removeWaitinglistMember', (req, res, next) =>
+  updateFor(socratesActivitiesService.removeWaitinglistMemberFor, {desiredRoomTypes: [req.body.roomType], waitinglistMemberNick: req.body.waitinglistMember}, res, next));
 
 module.exports = app;
