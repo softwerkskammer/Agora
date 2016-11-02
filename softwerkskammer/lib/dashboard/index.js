0047aa9976523ab9bc12c9437e673486bf03be94
leider <derleider@web.de>
2016-11-02 23:17:07 +0100

    further dashboard cleanup

--- softwerkskammer/lib/dashboard/index.js
+++ softwerkskammer/lib/dashboard/index.js
@@ -4,21 +4,12 @@ const beans = require('simple-configure').get('beans');
 const misc = beans.get('misc');
 const dashboardService = beans.get('dashboardService');
 
-const transformResult = require('./transformToPlainJson');
-
 const app = misc.expressAppIn(__dirname);
 
 app.get('/', (req, res, next) => {
   dashboardService.dataForDashboard(req.user.member.nickname(), (err, result) => {
     if (err) { return next(err); }
     res.render('index', result);
-  });
-});
-
-app.get('/json', (req, res, next) => {
-  dashboardService.dataForDashboard(req.user.member.nickname(), (err, result) => {
-    if (err) { return next(err); }
-    res.render('indexJson', transformResult(result, res.locals.language));
   });
 });
 
