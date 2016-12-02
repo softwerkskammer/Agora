6dc9862eeddbe425b882b7a5a9bbc0d4ad4dfacc
leider <derleider@web.de>
2016-12-02 16:19:28 +0100

    es6ifying waitinglist stuff (+ lodash removal)

--- softwerkskammer/lib/waitinglist/index.js
+++ softwerkskammer/lib/waitinglist/index.js
@@ -1,83 +1,82 @@
 'use strict';
 
-var async = require('async');
-var _ = require('lodash');
+const async = require('async');
 
-var beans = require('simple-configure').get('beans');
+const beans = require('simple-configure').get('beans');
 
-var waitinglistService = beans.get('waitinglistService');
-var activitiesService = beans.get('activitiesService');
-var memberstore = beans.get('memberstore');
-var misc = beans.get('misc');
+const waitinglistService = beans.get('waitinglistService');
+const activitiesService = beans.get('activitiesService');
+const memberstore = beans.get('memberstore');
+const misc = beans.get('misc');
 
 function accessAllowedTo(activityUrl, res, callback) {
-  var accessrights = res.locals.accessrights;
+  const accessrights = res.locals.accessrights;
 
-  activitiesService.getActivityWithGroupAndParticipants(activityUrl, function (err, activity) {
-    var canEditActivity = !!activity && accessrights.canEditActivity(activity);
+  activitiesService.getActivityWithGroupAndParticipants(activityUrl, (err, activity) => {
+    const canEditActivity = !!activity && accessrights.canEditActivity(activity);
     if (err || !canEditActivity) { return callback(err); }
     return callback(null, activity);
   });
 }
 
-var app = misc.expressAppIn(__dirname);
+const app = misc.expressAppIn(__dirname);
 
-app.get('/:activityUrl', function (req, res, next) {
-  var activityUrl = req.params.activityUrl;
-  accessAllowedTo(activityUrl, res, function (err, activity) {
+app.get('/:activityUrl', (req, res, next) => {
+  const activityUrl = req.params.activityUrl;
+  accessAllowedTo(activityUrl, res, (err, activity) => {
     if (err || !activity) { return res.redirect('/activities/upcoming'); }
-    waitinglistService.waitinglistFor(activityUrl, function (err1, waitinglist) {
+    waitinglistService.waitinglistFor(activityUrl, (err1, waitinglist) => {
       if (err1) { return next(err1); }
-      res.render('waitinglistTable', { waitinglist: waitinglist, activity: activity });
+      res.render('waitinglistTable', {waitinglist: waitinglist, activity: activity});
     });
   });
 });
 
-app.post('/add', function (req, res, next) {
-  var activityUrl = req.body.activityUrl;
-  accessAllowedTo(activityUrl, res, function (err, activity) {
+app.post('/add', (req, res, next) => {
+  const activityUrl = req.body.activityUrl;
+  accessAllowedTo(activityUrl, res, (err, activity) => {
     if (err || !activity) { return res.redirect('/activities/upcoming'); }
 
-    var args = {nickname: req.body.nickname, activityUrl: activityUrl};
-    waitinglistService.saveWaitinglistEntry(args, function (err1) {
+    const args = {nickname: req.body.nickname, activityUrl: activityUrl};
+    waitinglistService.saveWaitinglistEntry(args, err1 => {
       if (err1) { return next(err1); }
       res.redirect('/waitinglist/' + encodeURIComponent(activityUrl));
     });
   });
 });
 
-app.post('/allowRegistration', function (req, res, next) {
-  var activityUrl = req.body.activityUrl;
-  accessAllowedTo(activityUrl, res, function (err, activity) {
+app.post('/allowRegistration', (req, res, next) => {
+  const activityUrl = req.body.activityUrl;
+  accessAllowedTo(activityUrl, res, (err, activity) => {
     if (err || !activity) { return res.redirect('/activities/upcoming'); }
 
-    var selectedRow = req.body.selectedRow;
+    let selectedRow = req.body.selectedRow;
     if (!selectedRow) { return res.redirect('/waitinglist/' + encodeURIComponent(activityUrl)); }
-    var selectedRows = selectedRow instanceof Array ? selectedRow : [ selectedRow ];
+    const selectedRows = selectedRow instanceof Array ? selectedRow : [selectedRow];
 
-    var rows = _.map(selectedRows, function (rowString) {
-      var result = JSON.parse(rowString);
+    const rows = selectedRows.map(rowString => {
+      const result = JSON.parse(rowString);
       result.hoursstring = req.body.registrationValidForHours;
       return result;
     });
 
-    async.eachSeries(rows, waitinglistService.allowRegistrationForWaitinglistEntry, function (err1) {
+    async.eachSeries(rows, waitinglistService.allowRegistrationForWaitinglistEntry, err1 => {
       if (err1) { return next(err1); }
       res.redirect('/waitinglist/' + encodeURIComponent(activityUrl));
     });
   });
 });
 
-app.post('/remove', function (req, res, next) {
-  var activityUrl = req.body.activityUrl;
-  accessAllowedTo(activityUrl, res, function (err, activity) {
+app.post('/remove', (req, res, next) => {
+  const activityUrl = req.body.activityUrl;
+  accessAllowedTo(activityUrl, res, (err, activity) => {
     if (err) { return next(err); }
     if (!res.locals.accessrights.canEditActivity(activity)) {
       res.redirect('/activites/' + encodeURIComponent(req.body.activityUrl));
     }
-    memberstore.getMember(req.body.nickname, function (err1, member) {
+    memberstore.getMember(req.body.nickname, (err1, member) => {
       if (err1) { return res.send(400); }
-      activitiesService.removeFromWaitinglist(member.id(), activityUrl, function (err2) {
+      activitiesService.removeFromWaitinglist(member.id(), activityUrl, err2 => {
         if (err2) { return next(err2); }
         res.send('ok');
       });
