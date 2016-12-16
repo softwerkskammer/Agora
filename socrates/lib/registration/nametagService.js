b75a1cf72ca7f9281a268688dfc0cc53e14210b7
Nicole Rauch <nicole.m@gmx.de>
2016-12-16 19:57:06 +0100

    With explicit "this" bindings, the functions can be put back into the object.

--- socrates/lib/registration/nametagService.js
+++ socrates/lib/registration/nametagService.js
@@ -27,79 +27,64 @@ const tagHeight = orcana ? orcanaTags.tagHeight : gillardonTags.tagHeight;
 const bottomMargin = orcana ? orcanaTags.bottomMargin : gillardonTags.bottomMargin;
 const paperMargins = orcana ? orcanaTags.paperMargins : gillardonTags.paperMargins;
 
-function _prefix() {
-
-  return '\\documentclass[12pt,a4paper]{scrbook}\n' +
-    '\n' +
-    '\\usepackage{ngerman}\n' +
-    '\\usepackage[default,osfigures,scale=0.95]{opensans}\n' +
-    '\\usepackage[utf8]{inputenc}\n' +
-    '\\usepackage[T1]{fontenc}\n' +
-    '\\usepackage[a4paper, ' + paperMargins + ', landscape]{geometry}\n' +
-    '\n' +
-    '\\pagestyle{empty}\n' +
-    '\n' +
-    '\n' +
-    '\\newcommand{\\nametag}[3]{%\n' +
-    '  \\parbox[t]{' + colWidth + '}{\\rule[0mm]{0mm}{' + tagHeight + '}%\n' +
-    '    \\begin{minipage}[b]{' + colWidth + '}%\n' +
-    '      {\\Huge \\textbf{#1}}\\\\[5mm]%\n' +
-    '      {\\large #2}\\\\[5mm]%\n' +
-    '    {\\Large \\textbf{#3}}\\\\[' + bottomMargin + ']%\n' +
-    ' \\end{minipage}}}%\n' +
-    '\n' +
-    '\n' +
-    '\\begin{document}\n';
-}
-
-function _nametagFor(member) {
-  const twitter = member.twitter() ? '@' + member.twitter().replace(/_/g, '\\_') : '';
-  return '\\nametag{' + member.firstname() + '}{' + member.lastname() + '}{' + twitter + '}';
-}
-
-function _lineFor(members) {
-  if (members.length > 3) {
-    return 'ERROR! Passed more than 3 members to lineFor()';
-  }
-  return members.map(member => _nametagFor(member)).join(' & ') + '\\\\ \\hline \n';
-}
-
-function _tableFor(members) {
-  if (members.length > 12) {
-    return 'ERROR! Passed more than 12 members to tableFor()';
-  }
-  return '\\begin{tabular}{|p{' + colWidth + '}|p{' + colWidth + '}|p{' + colWidth + '}|} \n' +
-    '\\hline \n' +
-    R.splitEvery(3, members).map(_lineFor).join('') +
-    '\n\\end{tabular}\n\n\n';
-}
-
-function _tablesFor(members) {
-  return R.splitEvery(12, members).map(_tableFor).join('');
-}
-
-function _postfix() {
-  return '\n' +
-    '\\end{document}\n';
-}
-
-function nametagsFor(members) {
-  return _prefix() + _tablesFor(members) + _postfix();
-}
 
 module.exports = {
-  nametagsFor: nametagsFor,
+  nametagsFor: function nametagsFor(members) {
+    return this._prefix() + this._tablesFor.bind(this)(members) + this._postfix();
+  },
 
-  _prefix: _prefix,
+  _prefix: function _prefix() {
+    return '\\documentclass[12pt,a4paper]{scrbook}\n' +
+      '\n' +
+      '\\usepackage{ngerman}\n' +
+      '\\usepackage[default,osfigures,scale=0.95]{opensans}\n' +
+      '\\usepackage[utf8]{inputenc}\n' +
+      '\\usepackage[T1]{fontenc}\n' +
+      '\\usepackage[a4paper, ' + paperMargins + ', landscape]{geometry}\n' +
+      '\n' +
+      '\\pagestyle{empty}\n' +
+      '\n' +
+      '\n' +
+      '\\newcommand{\\nametag}[3]{%\n' +
+      '  \\parbox[t]{' + colWidth + '}{\\rule[0mm]{0mm}{' + tagHeight + '}%\n' +
+      '    \\begin{minipage}[b]{' + colWidth + '}%\n' +
+      '      {\\Huge \\textbf{#1}}\\\\[5mm]%\n' +
+      '      {\\large #2}\\\\[5mm]%\n' +
+      '    {\\Large \\textbf{#3}}\\\\[' + bottomMargin + ']%\n' +
+      ' \\end{minipage}}}%\n' +
+      '\n' +
+      '\n' +
+      '\\begin{document}\n';
+  },
 
-  _nametagFor: _nametagFor,
+  _nametagFor: function _nametagFor(member) {
+    const twitter = member.twitter() ? '@' + member.twitter().replace(/_/g, '\\_') : '';
+    return '\\nametag{' + member.firstname() + '}{' + member.lastname() + '}{' + twitter + '}';
+  },
 
-  _lineFor: _lineFor,
+  _lineFor: function _lineFor(members) {
+    if (members.length > 3) {
+      return 'ERROR! Passed more than 3 members to lineFor()';
+    }
+    return members.map(this._nametagFor).join(' & ') + '\\\\ \\hline \n';
+  },
 
-  _tableFor: _tableFor,
+  _tableFor: function _tableFor(members) {
+    if (members.length > 12) {
+      return 'ERROR! Passed more than 12 members to tableFor()';
+    }
+    return '\\begin{tabular}{|p{' + colWidth + '}|p{' + colWidth + '}|p{' + colWidth + '}|} \n' +
+      '\\hline \n' +
+      R.splitEvery(3, members).map(this._lineFor.bind(this)).join('') +
+      '\n\\end{tabular}\n\n\n';
+  },
 
-  _tablesFor: _tablesFor,
+  _tablesFor: function _tablesFor(members) {
+    return R.splitEvery(12, members).map(this._tableFor.bind(this)).join('');
+  },
 
-  _postfix: _postfix
-
+  _postfix: function _postfix() {
+    return '\n' +
+      '\\end{document}\n';
+  }
 };
