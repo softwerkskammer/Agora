cb381587bbebac3a8f92d141d369959f2b4decdb
leider <derleider@web.de>
2018-05-27 18:30:14 +0200

    fixes #1356 without breaking legacy data

--- softwerkskammer/frontendtests/surroundWithLink-tests.js
+++ softwerkskammer/frontendtests/surroundWithLink-tests.js
@@ -10,29 +10,29 @@
     describe('surround with link', function () {
       it('surrounds a text starting with "http" with a link consisting of the text', function () {
         var result = surroundWithLink('http://my.link');
-        expect(result).to.equal('<a href="http://my.link" target="_blank"><i class="fa fa-external-link"/> http://my.link</a>');
+        expect(result).to.equal('<a href="http://my.link" target="_blank"><i class="fa fa-external-link"/> http://my.link </a>');
       });
 
       it('surrounds each link in a text with two "http" links', function () {
         var result = surroundWithLink('http://my.link, http://your.link');
-        expect(result).to.equal('<a href="http://my.link" target="_blank"><i class="fa fa-external-link"/> http://my.link</a>, <a href="http://your.link" target="_blank"><i class="fa fa-external-link"/> http://your.link</a>');
+        expect(result).to.equal('<a href="http://my.link" target="_blank"><i class="fa fa-external-link"/> http://my.link </a>, <a href="http://your.link" target="_blank"><i class="fa fa-external-link"/> http://your.link </a>');
       });
 
       it('surrounds only links in a text', function () {
         var result = surroundWithLink('http://my.link, your.link');
-        expect(result).to.equal('<a href="http://my.link" target="_blank"><i class="fa fa-external-link"/> http://my.link</a>, your.link');
+        expect(result).to.equal('<a href="http://my.link" target="_blank"><i class="fa fa-external-link"/> http://my.link </a>, your.link');
       });
 
       it('links an element inside class "urlify"', function () {
-        expect($('#first').html()).to.equal('<a href="http://my.first.link" target="_blank"><i class="fa fa-external-link"></i> http://my.first.link</a>');
+        expect($('#first').html()).to.equal('<a href="http://my.first.link" target="_blank"><i class="fa fa-external-link"></i> http://my.first.link </a>');
       });
 
       it('links two elements inside class "urlify"', function () {
-        expect($('#second').html()).to.equal('<a href="http://my.first.link" target="_blank"><i class="fa fa-external-link"></i> http://my.first.link</a>, <a href="http://my.first.link.again" target="_blank"><i class="fa fa-external-link"></i> http://my.first.link.again</a>');
+        expect($('#second').html()).to.equal('<a href="http://my.first.link" target="_blank"><i class="fa fa-external-link"></i> http://my.first.link </a>, <a href="http://my.first.link.again" target="_blank"><i class="fa fa-external-link"></i> http://my.first.link.again </a>');
       });
 
       it('links one of two elements inside class "urlify"', function () {
-        expect($('#third').html()).to.equal('<a href="http://my.first.link" target="_blank"><i class="fa fa-external-link"></i> http://my.first.link</a>, my.first.link.again');
+        expect($('#third').html()).to.equal('<a href="http://my.first.link" target="_blank"><i class="fa fa-external-link"></i> http://my.first.link </a>, my.first.link.again');
       });
     });
 
