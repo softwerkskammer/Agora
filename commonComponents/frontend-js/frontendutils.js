472c7776a89cf53e52e0d369d45efe43e11e4db3
leider <derleider@web.de>
2018-09-26 14:15:57 +0200

    fix regex (thanks to eslint)

--- commonComponents/frontend-js/frontendutils.js
+++ commonComponents/frontend-js/frontendutils.js
@@ -4,7 +4,7 @@ function surroundWithLink(text) {
   'use strict';
 
   // shamelessly stolen from http://stackoverflow.com/questions/1500260/detect-urls-in-text-with-javascript
-  var urlRegex = /(\b(https?|ftp|file):\/\/[\-A-Z0-9+&@#\/%?=~_|!:,.;]*[\-A-Z0-9+&@#\/%=~_|])/ig;
+  var urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/ig;
   return text.replace(urlRegex, function (url) {
     return '<a href="' + url + '" target="_blank">' + '<i class="fa fa-external-link"/> ' + url + ' </a>';
   });
