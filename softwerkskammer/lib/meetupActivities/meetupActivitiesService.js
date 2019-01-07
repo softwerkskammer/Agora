7ee8d6c2c4d88fb28ca113bc5721d9765d991d71
Nicole Rauch <info@nicole-rauch.de>
2019-01-07 21:27:17 +0100

    Repeat after me: Only access an object when you're really really sure it's there...

--- softwerkskammer/lib/meetupActivities/meetupActivitiesService.js
+++ softwerkskammer/lib/meetupActivities/meetupActivitiesService.js
@@ -9,6 +9,7 @@ const Activity = beans.get('activity');
 const fieldHelpers = beans.get('fieldHelpers');
 
 function meetupFetchActivitiesURLFor(urlname) {
+  // v3 API https://www.meetup.com/de-DE/meetup_api/docs/:urlname/events/
   return 'https://api.meetup.com/' + urlname + '/events';
 }
 
@@ -30,7 +31,7 @@ module.exports = {
             title: meetup.name,
             description: meetup.description,
             assignedGroup: group.id,
-            location: meetup.venue.name + ', ' + meetup.venue.address_1 + ', ' + meetup.venue.city,
+            location: meetup.venue ? meetup.venue.name + ', ' + meetup.venue.address_1 + ', ' + meetup.venue.city : '',
             direction: '',
             startDate: meetupDate.startDate,
             startTime: meetupDate.startTime,
