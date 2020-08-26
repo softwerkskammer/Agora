f2ec421d603eb917d1a1b808aab6bef488ed4f68
leider <derleider@web.de>
2020-08-26 18:02:44 +0200

    tuning migration-script for much more elegance!

--- softwerkskammer/moveUsersFromEzmlmToGroups.js
+++ softwerkskammer/moveUsersFromEzmlmToGroups.js
@@ -3,6 +3,7 @@
 'use strict';
 
 const async = require('async');
+const R = require('ramda');
 
 const conf = require('./configure'); // initializing parameters
 const beans = require('simple-configure').get('beans');
@@ -26,9 +27,7 @@ memberstore.allMembers((err, members) => {
 
   function listsWithEmails(callback) {
     function createListUsersObject(list, cb) {
-      listAdapter.getUsersOfList(list, (errX, users) => {
-        cb(errX, {listname: list, users});
-      });
+      listAdapter.getUsersOfList(list, (errX, users) => cb(errX, {listname: list, users}));
     }
 
     listAdapter.getAllAvailableLists((errList, lists) => {
@@ -36,32 +35,38 @@ memberstore.allMembers((err, members) => {
         console.log(errList);
         process.exit();
       }
-      async.map(lists, createListUsersObject, (finalErr, results) => {
+      async.map(lists, createListUsersObject, (finalErr, allListsWithTheirUsers) => {
         if (finalErr) {
           console.log(finalErr);
           process.exit();
         }
-        callback(finalErr, results);
+        callback(finalErr, allListsWithTheirUsers);
       });
     });
   }
 
-  listsWithEmails((err1, result) => {
+  listsWithEmails((err1, allListsWithTheirUsers) => {
     if (err1) {
       console.log(err1);
       process.exit();
     }
 
-    result.forEach(r => console.log('list: ' + r.listname + ' users: ' + r.users.length));
     groupstore.allGroups((errGroups, groups) => {
+      if (errGroups || !groups) {
+        console.log('ERROR ON LOADING ALL GROUPS: ' + errGroups + ' no of groups: ' + groups ? groups.length : 'no groups');
+        process.exit();
+      }
+
       groups.forEach(g => {
-        const currentList = result.find(r => r.listname === g.id);
+        const currentList = allListsWithTheirUsers.find(r => r.listname === g.id);
         if (!currentList) {
           console.log('Liste für ' + g.id + ' nicht gefunden!');
           return;
         }
         g.subscribedMembers = members.filter(m => currentList.users.includes(m.email())).map(m => m.id());
-        console.log('Group: ' + g.id + ', members: ' + g.subscribedMembers.join(','));
+        g.subscribedMembers = R.uniq(g.subscribedMembers);
+        console.log('The original list: ' + currentList.listname + ' has no of users: ' + currentList.users.length);
+        console.log('The Group: ' + g.id + ', now has no of members: ' + g.subscribedMembers.length);
       });
       async.each(groups, groupstore.saveGroup, fatal => {
         if (fatal) {console.log('ERROR: ' + fatal);}
