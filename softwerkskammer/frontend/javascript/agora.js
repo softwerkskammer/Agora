b1490ca2c7582834ac383fbb915982a61f92ca02
leider <derleider@web.de>
2020-08-27 14:18:03 +0200

    dependencies bump (except fullcalendar)

--- softwerkskammer/frontend/javascript/agora.js
+++ softwerkskammer/frontend/javascript/agora.js
@@ -87,7 +87,7 @@ function surroundWithLink(text) {
   // shamelessly stolen from http://stackoverflow.com/questions/1500260/detect-urls-in-text-with-javascript
   var urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/ig;
   return text.replace(urlRegex, function (url) {
-    return '<a href="' + url + '" target="_blank">' + '<i class="fa fa-external-link"/> ' + url + ' </a>';
+    return '<a href="' + url + '" target="_blank">' + '<i class="fa fa-external-link"></i> ' + url + ' </a>';
   });
 }
 
