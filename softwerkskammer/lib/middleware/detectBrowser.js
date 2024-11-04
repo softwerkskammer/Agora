3b60e3ece2087c62e95ec6f60e9743291abe9efe
Andreas Leidig <derleider@web.de>
2024-11-04 21:42:40 +0100

    Bump deps - more mail headers in message.js (#1508)
    * cleanup legacy IE 8 handling and minor improvement for bad git requests (404 not 500)
    
    * bump deps and add more info to message.js

--- softwerkskammer/lib/middleware/detectBrowser.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const useragent = require("useragent");
-
-module.exports = function detectBrowser(req, res, next) {
-  const agent = useragent.parse(req.headers["user-agent"]);
-  res.locals.browserIsTooOld = agent.family === "IE" && agent.major < 9;
-  next();
-};
